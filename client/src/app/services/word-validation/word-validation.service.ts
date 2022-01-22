1be2d2e12666eca7529e71c98fa70d9b7b73c453
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-22 13:24:18 -0500

    RS: Changer les mauvais nom oublies

--- client/src/app/services/word-validation/word-validation.service.ts
+++ client/src/app/services/word-validation/word-validation.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Square } from '@app/classes/square';
-
 @Injectable({
     providedIn: 'root',
 })
 export class WordValidationService {
     // TODO: Potentially make an interface of a candidate word to make it clearer
-    calculate(newWords: string[]): string[] {
+    validateWords(newWords: string[]): string[] {
         return Array(0);
     }
 }
