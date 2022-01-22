ef73346613391e8097fe4e3575b69be35621bb97
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-01-22 14:07:29 -0500

    Add comment to startRound()

--- client/src/app/classes/player/opponent.ts
+++ client/src/app/classes/player/opponent.ts
@@ -4,5 +4,10 @@ import IPlayer from './iplayer';
 export default abstract class Opponent extends IPlayer {
     events: EventEmitter;
 
+    /**
+     * Indicate to the opponent that it is his turn to play.
+     * @Param none
+     * @Return void, the event emitter will handle the completion
+     */
     abstract startRound(): void;
 }
