b3272e5ae0c1619e159b26192eecdd9339c6fba2
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-21 23:07:49 -0500

    RS : Signature des couleurs des messages envoye apres une action

---
+++ client/src/app/classes/message-color.ts
@@ -0,0 +1,7 @@
+import { COLOR_BLUE, COLOR_RED, COLOR_GRAY } from '@app/classes/message-color-constants';
+
+export enum MessageColor {
+    LocalPlayer = COLOR_BLUE,
+    Opponent = COLOR_RED,
+    System = COLOR_GRAY,
+}
