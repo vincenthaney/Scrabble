d6d54cbee4f75eb305896350bf4f516d5168330b
Thomas <thomtrepanier@gmail.com>
2022-01-24 15:52:33 -0500

    Les erreurs de lint sont en commentaire maintenant

--- client/src/app/classes/tile-reserve.ts
+++ client/src/app/classes/tile-reserve.ts
@@ -2,4 +2,20 @@ import { Tile } from '@app/classes/tile';
 
 export class TileReserve {
     tiles: Tile[];
+
+    // getTiles(amount: number): Tile[] {
+    //     throw new Error('Method not implemented.');
+    // }
+
+    // swapTiles(tiles: Tile[]): Tile[] {
+    //     throw new Error('Method not implemented.');
+    // }
+
+    getTilesLeft(): number {
+        throw new Error('Method not implemented.');
+    }
+
+    getTilesLeftPerLetter(): Map<string, number> {
+        throw new Error('Method not implemented.');
+    }
 }
--- client/src/app/services/score-computer/score-computer.service.ts
+++ client/src/app/services/score-computer/score-computer.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+// import { Square } from '@app/classes/square';
 import { Board } from '@app/classes/board';
 
 @Injectable({
@@ -6,4 +7,9 @@ import { Board } from '@app/classes/board';
 })
 export default class ScoreComputerService {
     board: Board;
+
+    // TODO: Potentially make an interface of a candidate word to make it clearer
+    // computeScore(words: Square[][]): number {
+    //     throw new Error('Method not implemented.');
+    // }
 }
