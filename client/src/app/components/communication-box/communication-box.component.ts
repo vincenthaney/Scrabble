b1c14afbe9008c7838e2739a1bcd121884f99af3
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-23 23:41:21 -0500

    Creation du communication component vide

---
+++ client/src/app/components/communication-box/communication-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-communication-box',
+  templateUrl: './communication-box.component.html',
+  styleUrls: ['./communication-box.component.scss']
+})
+export class CommunicationBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
