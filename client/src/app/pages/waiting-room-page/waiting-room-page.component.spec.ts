4ba141e3893765c06e7d789432b78a90bcc2a347
Vincent Haney <vincent.7aney@gmail.com>
2022-01-25 17:00:38 -0500

    mise en page et ajout image

--- client/src/app/pages/waiting-room-page/waiting-room-page.component.spec.ts
+++ client/src/app/pages/waiting-room-page/waiting-room-page.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WaitingRoomPageComponent } from './waiting-room-page.component';
 
 describe('WaitingRoomPageComponent', () => {
-  let component: WaitingRoomPageComponent;
-  let fixture: ComponentFixture<WaitingRoomPageComponent>;
+    let component: WaitingRoomPageComponent;
+    let fixture: ComponentFixture<WaitingRoomPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ WaitingRoomPageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [WaitingRoomPageComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WaitingRoomPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WaitingRoomPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
