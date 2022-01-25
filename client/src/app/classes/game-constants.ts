071d4822323c6a5f114c2cd747e0c7d16140eea9
Thomas <thomtrepanier@gmail.com>
2022-01-25 12:12:00 -0500

    Le board est fini!

--- client/src/app/classes/game-constants.ts
+++ client/src/app/classes/game-constants.ts
@@ -2,3 +2,5 @@ import { Vec2 } from '@app/classes/vec2';
 
 export const BOARD_SIZE: Vec2 = { x: 15, y: 15 };
 export const SQUARE_SIZE: Vec2 = { x: 1, y: 1 };
+// REMOVE WHEN CHARLES MERGES TILE_RESERVED
+export const GRID_MARGIN_LETTER = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'];
--- client/src/app/components/board/board.component.ts
+++ client/src/app/components/board/board.component.ts
@@ -1,23 +1,24 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Board } from '@app/classes/board';
 import { COLORS } from '@app/classes/color-constants';
-import { BOARD_SIZE, SQUARE_SIZE } from '@app/classes/game-constants';
+import { BOARD_SIZE, GRID_MARGIN_LETTER, SQUARE_SIZE } from '@app/classes/game-constants';
 import { SquareView } from '@app/components/square/square-view';
+
+const MARGIN_COLUMN_SIZE = 1;
+const ID_MULTIPLIER = 10000;
 
 @Component({
     selector: 'app-board',
     templateUrl: './board.component.html',
     styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent implements OnInit, AfterViewInit {
+export class BoardComponent implements OnInit /* , AfterViewInit*/ {
     @ViewChild('board') componentIdentifier: ElementRef;
-
-    readonly marginColumnSize: number = 0;
+    readonly marginLetters = GRID_MARGIN_LETTER;
+    readonly marginColumnSize: number = MARGIN_COLUMN_SIZE;
     squares: SquareView[][];
-    colAmount: number = 0;
-    boardStyle: BoardStyle = {
-        width: '100%',
-    };
+    colAmount: number;
+    boardStyle: CSSStyleDeclaration;
     ngOnInit() {
         this.squares = [];
         for (let i = 0; i < Board.size.x; i++) {
@@ -28,38 +29,33 @@ export class BoardComponent implements OnInit, AfterViewInit {
                     squareSize: SQUARE_SIZE,
                     squarePosition: { x: i, y: j },
                     color: COLORS.Beige,
-                    id: Math.floor(Math.random() * 1000),
+                    id: Math.floor(Math.random() * ID_MULTIPLIER),
                 };
                 this.squares[i][j] = square;
             }
         }
-        this.colAmount = BOARD_SIZE.x + 2 * this.marginColumnSize;
-        this.boardStyle.width = '50%';
+        this.colAmount = BOARD_SIZE.x + this.marginColumnSize;
     }
 
-    ngAfterViewInit(): void {
-        // this.resizeToSquare();
-        const board = document.getElementById('board');
-        if (board) {
-            const boardHeight = board.offsetHeight;
-            this.boardStyle.width = String(boardHeight + 'px');
-        }
-    }
+    // ngAfterViewInit(): void {
+    // this.resizeToSquare();
+    // const board = document.getElementById('board');
+    // if (board) {
+    // const boardHeight = board.offsetHeight;
+    // this.boardStyle.offsetWidth = String(boardHeight + 'px');
+    // }
+    // }
 
-    resizeToSquare(): void {
-        const board: HTMLElement = this.componentIdentifier.nativeElement;
+    // resizeToSquare(): void {
+    // const board: HTMLElement = this.componentIdentifier.nativeElement;
 
-        const boardHeight = board.offsetHeight;
-        this.boardStyle.width = String(boardHeight + 'px');
-        console.log(board.style.width);
-        // if (board) {
+    // const boardHeight = board.offsetHeight;
+    // this.boardStyle.width = String(boardHeight + 'px');
+    // console.log(board.style.width);
+    // if (board) {
 
-        //     // eslint-disable-next-line no-console
-        //     console.log(this.boardStyle.width);
-        // }
-    }
-}
-
-interface BoardStyle {
-    width: string;
+    //     // eslint-disable-next-line no-console
+    //     console.log(this.boardStyle.width);
+    // }
+    // }
 }
