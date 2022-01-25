21169be111930352d16ca8a822778afa59665beb
Vincent Haney <vincent.7aney@gmail.com>
2022-01-25 13:22:05 -0500

    creation page et bouton home

---
+++ client/src/app/pages/waiting-room-page/waiting-room-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WaitingRoomPageComponent } from './waiting-room-page.component';
+
+describe('WaitingRoomPageComponent', () => {
+  let component: WaitingRoomPageComponent;
+  let fixture: ComponentFixture<WaitingRoomPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WaitingRoomPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WaitingRoomPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
