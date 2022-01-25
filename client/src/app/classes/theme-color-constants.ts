a1dbe4e192c1debea5783cffeccac5fad1f3e163
Vincent Haney <vincent.7aney@gmail.com>
2022-01-25 18:26:57 -0500

    ajout fichier theme-color et theme-color-constants

---
+++ client/src/app/classes/theme-color-constants.ts
@@ -0,0 +1,4 @@
+export enum COLORS {
+    Green = 0x0e520e,
+    LightGreen = 0x128012,
+}
---
+++ client/src/app/classes/theme-color.ts
@@ -0,0 +1,6 @@
+import { COLORS } from '@app/classes/theme-color-constants';
+
+export enum MessageColor {
+    Primary = COLORS.Green,
+    Secondary = COLORS.LightGreen,
+}
