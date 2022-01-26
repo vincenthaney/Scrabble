02865343e0c49706e55202610ad73666096f3d57
matbro-inextenso <matbro@inextenso.net>
2022-01-25 21:55:38 -0500

    Create page and style for Game Creation Page /game-creation

--- client/src/app/pages/game-creation-page/game-creation-page.component.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { GameMode } from '@app/classes/game-mode';
+import { GameType } from '@app/classes/game-type';
 //import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
@@ -7,19 +12,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-creation-page.component.scss']
 })
 export class GameCreationPageComponent implements OnInit {
-  //gameParameters : FormGroup;
+  GameType = GameType;
+  GameMode = GameMode;
 
-  constructor() { }
+  gameType: GameType = GameType.Classic;
+  gameMode: GameMode = GameMode.Solo;
+  timer: number = 60;
+  playerName: string;
+  //Dictionary;
+
+  playerNameController = new FormControl('', [
+    Validators.pattern("^([a-zA-Z0-9]+[ '\\-]{0,1})*$"),
+    Validators.minLength(2),
+    Validators.maxLength(20)]);
+
+  constructor(public dialog: MatDialog, private router: Router) { }
 
   ngOnInit(): void {
+  
   }
 
-  // private initForm() {
-  //   this.gameParameters = new FormGroup({
-  //     'playerName': new FormControl(null, Validators.required),
-  //     'gameMode': new FormControl(null),
-  //     'gameStyle': new FormControl(null)
-  //   });
-  // }
+  createGame() {
+    if (this.playerNameController.invalid) {
+        // open dialog "please correct username"
+    }
+    else {
+      // send new game request to server (?)
+      // route to waiting room
+      this.router.navigateByUrl('/waiting');
+    }
+    
+  }
 
 }
