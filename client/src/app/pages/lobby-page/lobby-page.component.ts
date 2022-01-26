5cd43dc80fae927896b2469530a3cb4b74231cf9
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-25 23:55:28 -0500

    RS: Add Lobby page

---
+++ client/src/app/pages/lobby-page/lobby-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-lobby-page',
+    templateUrl: './lobby-page.component.html',
+    styleUrls: ['./lobby-page.component.scss'],
+})
+export class LobbyPageComponent {}
