75de1705644e7fd69c81ac1744bbe194509b5061
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-27 00:42:49 -0500

    RS: Add lobby info component

--- client/src/app/components/lobby-info/lobby-info.component.spec.ts
+++ client/src/app/components/lobby-info/lobby-info.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LobbyInfoComponent } from './lobby-info.component';
 
 describe('LobbyInfoComponent', () => {
-  let component: LobbyInfoComponent;
-  let fixture: ComponentFixture<LobbyInfoComponent>;
+    let component: LobbyInfoComponent;
+    let fixture: ComponentFixture<LobbyInfoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LobbyInfoComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LobbyInfoComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LobbyInfoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LobbyInfoComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
