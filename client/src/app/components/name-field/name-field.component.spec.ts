26aed4723460636cb24b321b0e6105f06cb02761
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-27 16:44:12 -0500

    RS: Add tests for name-field

--- client/src/app/components/name-field/name-field.component.spec.ts
+++ client/src/app/components/name-field/name-field.component.spec.ts
@@ -27,4 +27,20 @@ describe('NameFieldComponent', () => {
         expect(component).toBeTruthy();
     });
 
+    it('onNameFieldChange should change the playerName', () => {
+        const expected = 'ThOm';
+        component.playerName = 'Charles';
+        component.onNameFieldChange(expected);
+        expect(component.playerName).toBe(expected);
+    });
+
+    it('onNameFieldChange should emit nameChange', () => {
+        const fakeNameChange = () => {
+            return false;
+        };
+        const spy = spyOn(component.nameChange, 'emit').and.callFake(fakeNameChange);
+        component.onNameFieldChange('Charles');
+        expect(spy).toHaveBeenCalled();
+        expect(component.nameChange.emit).toHaveBeenCalledWith(true);
+    });
 });
