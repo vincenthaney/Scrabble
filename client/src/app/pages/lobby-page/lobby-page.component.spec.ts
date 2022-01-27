4805244cc91d4f9b347f23d2ba29152676d8185d
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-27 13:26:59 -0500

    RS: Add test dependencies

--- client/src/app/pages/lobby-page/lobby-page.component.spec.ts
+++ client/src/app/pages/lobby-page/lobby-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LobbyPageComponent } from './lobby-page.component';
+import { FormControl, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldControl, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { LobbyInfoComponent } from '@app/components/lobby-info/lobby-info.component';
 
 describe('LobbyPageComponent', () => {
     let component: LobbyPageComponent;
@@ -8,6 +12,17 @@ describe('LobbyPageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [
+                FormControl,
+                Validators,
+                MatInputModule,
+                MatFormFieldControl,
+                MatFormFieldModule,
+                MatDividerModule,
+                LobbyInfoComponent,
+                MatLabel,
+            ],
+
             declarations: [LobbyPageComponent],
         }).compileComponents();
     });
