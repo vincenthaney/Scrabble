e03cbf8fbdb5005517b325c6fb6aeeb8787e0ac7
matbro-inextenso <matbro@inextenso.net>
2022-01-27 02:58:47 -0500

    Added scrollable view, chat input and objectives/tilesLeft collapsable sections

--- client/src/app/components/communication-box/communication-box.component.ts
+++ client/src/app/components/communication-box/communication-box.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-communication-box',
     templateUrl: './communication-box.component.html',
     styleUrls: ['./communication-box.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CommunicationBoxComponent {}
+export class CommunicationBoxComponent {
+    message: string;
+    messages: Array<string> = ["message 1", "message 2", "message 3", "message 4", "message 5", "message 6", "message 7", "message 8"]
+
+    objectives: Array<string> = ["Objectif 1", "Objectif 2", "Objectif 3", "Objectif 4"];
+    
+    lettersLeft: Array<string> = [
+        'A : 2 ',
+        'B : 1 ',
+        'C : 2 ',
+        'D : 0 ',
+        'E : 4 ',
+    ]
+    
+    messageForm = new FormGroup({
+        content: new FormControl(''),
+    })
+
+    sendMessage() {
+        if (this.message) {
+            this.messages = [...this.messages, this.message]
+            console.log("sent!! - " + this.message);
+            this.messageForm.reset();
+            console.log(this.messages);
+        }
+    }
+}
