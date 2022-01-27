4f63317a715055c671aba3e852f8ca258a1b4ee5
matbro-inextenso <matbro@inextenso.net>
2022-01-26 21:59:04 -0500

    finished component game-creation-page, missing tests

--- client/src/app/pages/game-creation-page/game-creation-page.component.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { GameMode } from '@app/classes/game-mode';
 import { GameType } from '@app/classes/game-type';
-//import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-game-creation-page',
@@ -26,7 +24,7 @@ export class GameCreationPageComponent implements OnInit {
     Validators.minLength(2),
     Validators.maxLength(20)]);
 
-  constructor(public dialog: MatDialog, private router: Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   
@@ -39,9 +37,8 @@ export class GameCreationPageComponent implements OnInit {
     else {
       // send new game request to server (?)
       // route to waiting room
-      this.router.navigateByUrl('/waiting');
+      this.router.navigateByUrl('waiting');
     }
-    
   }
 
 }
