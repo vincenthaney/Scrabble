1f4160db99a34fd80d6993aabb694a926f39e821
Vincent Haney <vincent.7aney@gmail.com>
2022-01-27 11:53:12 -0500

    Ajouts tests et modifs structure

--- client/src/app/pages/waiting-room-page/waiting-room-page.component.spec.ts
+++ client/src/app/pages/waiting-room-page/waiting-room-page.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { OnlinePlayer } from '@app/classes/player';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 
 import { WaitingRoomPageComponent } from './waiting-room-page.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { WaitingRoomMessages } from './waiting-room-page.component.const';
+
+let loader: HarnessLoader;
 
 describe('WaitingRoomPageComponent', () => {
     let component: WaitingRoomPageComponent;
@@ -9,7 +17,10 @@ describe('WaitingRoomPageComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [WaitingRoomPageComponent],
+            imports: [MatProgressSpinnerModule],
         }).compileComponents();
+        fixture = TestBed.createComponent(WaitingRoomPageComponent);
+        loader = TestbedHarnessEnvironment.loader(fixture);
     });
 
     beforeEach(() => {
@@ -20,5 +31,50 @@ describe('WaitingRoomPageComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should throw error when opponent connects and message doesn't change", async () => {
+        const testOpponent = new OnlinePlayer();
+        testOpponent.name = 'testName';
+        component.setOpponent(testOpponent);
+        expect(component.messageWaitingRoom).toEqual(testOpponent.name + WaitingRoomMessages.OpponentFoundMessage);
+    });
+
+    it("should throw error when opponent disconnects and message doesn't change", async () => {
+        const testOpponent = new OnlinePlayer();
+        testOpponent.name = 'testName';
+        component.setOpponent(testOpponent);
+        component.disconnectOpponent();
+        expect(component.messageWaitingRoom).toEqual(WaitingRoomMessages.HostWaitingMessage);
+    });
+
+    it('should throw error when opponent connects and start-button stays disabled', async () => {
+        const testOpponent = new OnlinePlayer();
+        component.setOpponent(testOpponent);
+        const startButton = await loader.getHarness(MatButtonHarness.with({ selector: '#start-game-button' })); // === buttons[0]
+        expect(await startButton.isDisabled()).toBe(true);
+    });
+
+    it('should throw error when opponent connects and reject-button stays disabled', async () => {
+        const testOpponent = new OnlinePlayer();
+        component.opponent = testOpponent;
+        const rejectButton = await loader.getHarness(MatButtonHarness.with({ selector: '#reject-button' })); // === buttons[0]
+        expect(await rejectButton.isDisabled()).toBe(true);
+    });
+
+    it('should throw error when opponent disconnects and start-button stays enabled', async () => {
+        const testOpponent = new OnlinePlayer();
+        component.opponent = testOpponent;
+        component.disconnectOpponent();
+        const startButton = await loader.getHarness(MatButtonHarness.with({ selector: '#start-game-button' })); // === buttons[0]
+        expect(await startButton.isDisabled()).toBe(true);
+    });
+
+    it('should throw error when opponent disconnects and reject-button stays enabled', async () => {
+        const testOpponent = new OnlinePlayer();
+        component.opponent = testOpponent;
+        component.disconnectOpponent();
+        const rejectButton = await loader.getHarness(MatButtonHarness.with({ selector: '#reject-button' })); // === buttons[0]
+        expect(await rejectButton.isDisabled()).toBe(true);
     });
 });
