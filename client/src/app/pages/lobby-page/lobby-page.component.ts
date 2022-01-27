3c26903799ad7c4a50fcd4a18e30dfead2954c58
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-27 13:27:21 -0500

    RS: addNamevalidentaiotn

--- client/src/app/pages/lobby-page/lobby-page.component.ts
+++ client/src/app/pages/lobby-page/lobby-page.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-lobby-page',
     templateUrl: './lobby-page.component.html',
     styleUrls: ['./lobby-page.component.scss'],
 })
-export class LobbyPageComponent {}
+export class LobbyPageComponent {
+    playerName: string;
+
+    playerNameController = new FormControl('', [
+        // Validators.pattern(NameValidation.rule),
+        // Validators.minLength(NameValidation.minLength),
+        // Validators.maxLength(NameValidation.maxLength),
+        Validators.minLength(2),
+        // Validators.maxLength(20),
+    ]);
+    onNameFieldChange(name: string) {
+        this.playerName = name;
+    }
+}
