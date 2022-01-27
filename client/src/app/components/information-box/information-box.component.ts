7c51b0718b4cf3ff7a80b216184b0710ac86d0ac
matbro-inextenso <matbro@inextenso.net>
2022-01-27 11:05:17 -0500

    Info-box and communication-box progress

--- client/src/app/components/information-box/information-box.component.ts
+++ client/src/app/components/information-box/information-box.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { OnlinePlayer, Opponent, Player } from '@app/classes/player';
 
 @Component({
     selector: 'app-information-box',
     templateUrl: './information-box.component.html',
     styleUrls: ['./information-box.component.scss'],
 })
-export class InformationBoxComponent {}
+export class InformationBoxComponent {
+    player1: Player = new Player("Mathilde");
+    player2: Opponent = new OnlinePlayer("Raphael");
+
+    
+}
