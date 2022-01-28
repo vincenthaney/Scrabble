c9750d341c3c3ae2644e7e3cea6c731f8d700486
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-01-28 14:40:33 -0500

    remove array

--- client/src/app/utils/array.ts
+++
@@ -1,7 +0,0 @@
-export const reduce = <T, S>(array: T[], start: S, callback: (previous: S, value: T, index: number, array: T[]) => S): S => {
-    let o = start;
-    array.forEach((v, i, a) => {
-        o = callback(o, v, i, a);
-    });
-    return o;
-};
