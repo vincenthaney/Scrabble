b6e3732d7452a638922edd53ee3144bec5cefde2
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-01-28 11:55:25 -0500

    Uncomment tests to have real coverage

--- server/app/controllers/game-dispatcher.controller.ts
+++ server/app/controllers/game-dispatcher.controller.ts
@@ -1,24 +1,17 @@
-import { GameDispatcherService } from '@app/services/game-dispatcher.service';
-import { Request, Response, Router } from 'express';
+// import { GameDispatcherService } from '@app/services/game-dispatcher.service';
+import { /* Request, Response,*/ Router } from 'express';
 import { Service } from 'typedi';
-import * as HttpStatus from '@app/constants/http-status';
+// import * as HttpStatus from '@app/constants/http-status';
 
 @Service()
 export class GameDispatcherController {
     router: Router;
 
-    constructor(private readonly gameDispatcherService: GameDispatcherService) {
+    constructor(/* private readonly gameDispatcherService: GameDispatcherService*/) {
         this.configureRouter();
     }
 
     configureRouter(): void {
         this.router = Router();
-
-        this.router.get('/new', async (req: Request, res: Response) => {
-            this.gameDispatcherService
-                .createNewGame()
-                .then((d) => res.status(HttpStatus.ACCEPTED).send(d))
-                .catch((e) => res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(e));
-        });
     }
 }
--- server/app/services/game-dispatcher.service.ts
+++ server/app/services/game-dispatcher.service.ts
@@ -1,8 +1,4 @@
 import { Service } from 'typedi';
 
 @Service()
-export class GameDispatcherService {
-    async createNewGame(): Promise<string> {
-        return `New game at ${new Date()}`;
-    }
-}
+export class GameDispatcherService {}
