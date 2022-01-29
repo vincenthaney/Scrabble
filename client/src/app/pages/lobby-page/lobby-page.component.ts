0220b812585e44af194797e303164e30e8917fd5
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-28 21:50:43 -0500

    RS: Integrate NameFieldCOmponent and LobbyInfo interface

--- client/src/app/pages/lobby-page/lobby-page.component.ts
+++ client/src/app/pages/lobby-page/lobby-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { GameType } from '@app/classes/game-type';
+import { LobbyInfo } from '@app/classes/lobby-info';
+import { NameFieldComponent } from '@app/components/name-field/name-field.component';
 
 @Component({
     selector: 'app-lobby-page',
@@ -7,17 +9,44 @@ import { FormControl, Validators } from '@angular/forms';
     styleUrls: ['./lobby-page.component.scss'],
 })
 export class LobbyPageComponent {
-    playerName: string;
+    @ViewChild(NameFieldComponent) nameField: NameFieldComponent;
 
-    playerNameController = new FormControl('', [
-        // Validators.pattern(NameValidation.rule),
-        // Validators.minLength(NameValidation.minLength),
-        // Validators.maxLength(NameValidation.maxLength),
-        Validators.minLength(2),
-        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-        Validators.maxLength(20),
-    ]);
-    onNameFieldChange(name: string) {
-        this.playerName = name;
+    isInputNameValid: boolean;
+    // TODO: Receive LobbyInfo from server
+    lobbies: LobbyInfo[] = [
+        { lobbyID: 1, playerName: 'Nom1', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 2, playerName: 'Nom2', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 3, playerName: 'Nom3', gameType: GameType.LOG2990, timer: 90, canJoin: false },
+        { lobbyID: 4, playerName: 'Nom4', gameType: GameType.Classic, timer: 300, canJoin: false },
+        { lobbyID: 5, playerName: 'Nom5', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 6, playerName: 'Nom6', gameType: GameType.LOG2990, timer: 60, canJoin: false },
+        { lobbyID: 7, playerName: 'Nom7', gameType: GameType.LOG2990, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+        { lobbyID: 8, playerName: 'Nom8', gameType: GameType.Classic, timer: 60, canJoin: false },
+    ];
+
+    validateName(): void {
+        for (const lobby of this.lobbies) {
+            lobby.canJoin = this.isInputNameValid && this.nameField.formParameters.get('inputName')?.value !== lobby.playerName;
+        }
+    }
+
+    onNameChange(isInputNameValid: boolean) {
+        this.isInputNameValid = isInputNameValid;
+        this.validateName();
     }
 }
