c2640a7f32850cafb7e89d9a91a4a62eda9b72e4
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-28 23:41:18 -0500

    RS: Add lobby page tests

--- client/src/app/pages/lobby-page/lobby-page.component.spec.ts
+++ client/src/app/pages/lobby-page/lobby-page.component.spec.ts
@@ -3,6 +3,8 @@ import { LobbyPageComponent } from './lobby-page.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDividerModule } from '@angular/material/divider';
+import { GameType } from '@app/classes/game-type';
+import { NameFieldComponent } from '@app/components/name-field/name-field.component';
 
 describe('LobbyPageComponent', () => {
     let component: LobbyPageComponent;
@@ -22,7 +24,62 @@ describe('LobbyPageComponent', () => {
         fixture.detectChanges();
     });
 
+    beforeEach(() => {
+        component.lobbies = [
+            { lobbyID: 1, playerName: 'Name1', gameType: GameType.Classic, timer: 60, canJoin: false },
+            { lobbyID: 2, playerName: 'Name2', gameType: GameType.Classic, timer: 60, canJoin: true },
+            { lobbyID: 3, playerName: 'Name3', gameType: GameType.LOG2990, timer: 90, canJoin: false },
+        ];
+        component.nameField = new NameFieldComponent();
+    });
+
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('validateName should update canJoin attribute of the lobbies (use #1)', () => {
+        component.isInputNameValid = true;
+        component.nameField.formParameters.patchValue({ inputName: 'differentName' });
+        component.validateName();
+        for (const lobby of component.lobbies) {
+            expect(lobby.canJoin).toBeTruthy();
+        }
+    });
+
+    it('validateName should update canJoin attribute of the lobbies (use #2)', () => {
+        component.isInputNameValid = false;
+        component.nameField.formParameters.patchValue({ inputName: 'differentName' });
+        component.validateName();
+        for (const lobby of component.lobbies) {
+            expect(lobby.canJoin).toBeFalsy();
+        }
+    });
+
+    it('validateName should update canJoin attribute of the lobbies ( use #3)', () => {
+        component.isInputNameValid = true;
+        component.nameField.formParameters.patchValue({ inputName: 'Name1' });
+        const expected = [false, true, true];
+        component.validateName();
+        expect(component.lobbies);
+        for (let i = 0; i++; i < component.lobbies.length) {
+            expect(component.lobbies[i].canJoin).toEqual(expected[i]);
+        }
+    });
+
+    it('onNameChange should call validateName', () => {
+        const fakeValidateName = () => {
+            return false;
+        };
+        const spy = spyOn(component, 'validateName').and.callFake(fakeValidateName);
+        const expected = true;
+        component.onNameChange(expected);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('onNameChange should update isInputNameValid', () => {
+        const expected = false;
+        component.isInputNameValid = true;
+        component.onNameChange(expected);
+        expect(component.isInputNameValid).toEqual(expected);
     });
 });
