525a381080ca47129a619ebbe51a1f0e304c9648
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-29 17:23:49 -0500

    RS: Add correct time display

--- client/src/app/components/lobby-info/lobby-info.component.ts
+++ client/src/app/components/lobby-info/lobby-info.component.ts
@@ -25,4 +25,15 @@ export class LobbyInfoComponent {
     async joinLobby() {
         await this.router.navigateByUrl('waiting');
     }
+
+    convertTime(): string {
+        const SECONDS_IN_MINUTE = 60;
+        const time = this.lobby.timer;
+        const minutes = Math.floor(time / SECONDS_IN_MINUTE);
+        const seconds = Math.floor(time % SECONDS_IN_MINUTE);
+        const minuteDisplay = minutes > 0 ? minutes + (minutes === 1 ? ' minute' : ' minutes') : '';
+        const textBetween = minutes > 0 && seconds > 0 ? ' et ' : '';
+        const secondsDisplay = seconds > 0 ? seconds + (seconds === 1 ? ' seconde' : ' secondes') : '';
+        return minuteDisplay + textBetween + secondsDisplay;
+    }
 }
