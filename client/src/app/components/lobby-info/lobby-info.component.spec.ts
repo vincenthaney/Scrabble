2aad5306907195ba4b27cb76c127a6c545ca319a
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-29 17:36:18 -0500

    RS: Add testing to converTimer

--- client/src/app/components/lobby-info/lobby-info.component.spec.ts
+++ client/src/app/components/lobby-info/lobby-info.component.spec.ts
@@ -1,5 +1,4 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,15 +7,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppMaterialModule } from '@app/modules/material.module';
 
 import { LobbyInfoComponent } from './lobby-info.component';
-
-@Component({
-    template: '',
-})
-class TestComponent {}
 
 describe('LobbyInfoComponent', () => {
     let component: LobbyInfoComponent;
@@ -36,10 +29,6 @@ describe('LobbyInfoComponent', () => {
                 AppMaterialModule,
                 MatFormFieldModule,
                 FormsModule,
-                RouterTestingModule.withRoutes([
-                    { path: 'waiting-room', component: TestComponent },
-                    { path: 'home', component: TestComponent },
-                ]),
             ],
             providers: [
                 {
@@ -64,5 +53,27 @@ describe('LobbyInfoComponent', () => {
     it('joinLobby should redirect to waiting page', () => {
         component.joinLobby();
         expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('waiting');
+    });
+
+    it('convertTime should output the correct string', () => {
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        const TIMES = [0, 1, 2, 30, 60, 61, 90, 120, 121, 150, 243];
+        const EXPECTED_STRINGS = [
+            '',
+            '1 seconde',
+            '2 secondes',
+            '30 secondes',
+            '1 minute',
+            '1 minute et 1 seconde',
+            '1 minute et 30 secondes',
+            '2 minutes',
+            '2 minutes et 1 seconde',
+            '2 minutes et 30 secondes',
+            '4 minutes et 3 secondes',
+        ];
+        for (let i = 0; i < TIMES.length; i++) {
+            component.lobby.timer = TIMES[i];
+            expect(component.convertTime()).toEqual(EXPECTED_STRINGS[i]);
+        }
     });
 });
