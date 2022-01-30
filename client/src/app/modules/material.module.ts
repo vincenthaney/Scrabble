99877d4fab6a8004cc9b4e44d343bd6ddab4847a
Vincent Haney <vincent.7aney@gmail.com>
2022-01-30 16:10:21 -0500

    corrections imports

--- client/src/app/modules/material.module.ts
+++ client/src/app/modules/material.module.ts
@@ -16,7 +16,6 @@ import { MatInputModule } from '@angular/material/input';
 // import { MatMenuModule } from '@angular/material/menu';
 // import { MatPaginatorModule } from '@angular/material/paginator';
 // import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 // import { MatSidenavModule } from '@angular/material/sidenav';
@@ -49,7 +48,6 @@ const modules = [
     // MatMenuModule,
     // MatPaginatorModule,
     // MatProgressBarModule,
-    MatProgressSpinnerModule,
     MatRadioModule,
     MatSelectModule,
     // MatSidenavModule,
