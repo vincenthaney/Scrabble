b24e54584f5b5cbab2f64cda30f875b3a27df4ce
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-01-30 17:03:25 -0500

    Removed unused type

--- server/app/classes/game/game.type.ts
+++ server/app/classes/game/game.type.ts
@@ -2,7 +2,3 @@ export enum GameType {
     Classic = 'Classique',
     LOG2990 = 'LOG2990',
 }
-
-export type GameModeMultiplayer = 'multiplayer';
-export type GameModeSolo = 'solo';
-export type GameMode = GameModeMultiplayer | GameModeSolo;
