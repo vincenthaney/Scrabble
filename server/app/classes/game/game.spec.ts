cd60ae9167b5154be34a984d6d59bc1197ad68f8
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-01-29 22:17:18 -0500

    Tested game

--- server/app/classes/game/game.spec.ts
+++ server/app/classes/game/game.spec.ts
@@ -9,7 +9,8 @@ import { GameType } from './game.type';
 import TileReserve from '@app/classes/tile/tile-reserve';
 import Player from '@app/classes/player/player';
 import { MultiplayerGameConfig } from './game-config';
-// import { Tile } from '@app/classes/tile';
+import { Tile } from '@app/classes/tile';
+import * as Errors from '@app/constants/errors';
 
 const expect = chai.expect;
 
@@ -25,19 +26,22 @@ const DEFAULT_MULTIPLAYER_CONFIG: MultiplayerGameConfig = {
     maxRoundTime: 1,
     dictionary: 'francais',
 };
-// const DEFAULT_TILE: Tile = { }
+const DEFAULT_TILE: Tile = { letter: 'A', value: 1 };
+const DEFAULT_AMOUNT_OF_TILES = 25;
 
 describe('Game', () => {
     let defaultInit: () => Promise<void>;
-    let spy: unknown;
 
     beforeEach(() => {
         defaultInit = TileReserve.prototype.init;
         TileReserve.prototype.init = async function () {
             this['initialized'] = true;
+            for (let i = 0; i < DEFAULT_AMOUNT_OF_TILES; ++i) {
+                this['tiles'].push({ ...DEFAULT_TILE });
+            }
+            this['referenceTiles'] = [...this['tiles']];
             return Promise.resolve();
         };
-        spy = chai.spy(TileReserve.prototype.init);
     });
 
     afterEach(() => {
@@ -51,13 +55,79 @@ describe('Game', () => {
             game = await Game.createMultiplayerGame(DEFAULT_MULTIPLAYER_CONFIG);
         });
 
-        it('should create', async () => {
-            console.log(game);
+        it('should create', () => {
             expect(game).to.exist;
         });
 
-        it('should call TileReserve init', () => {
-            expect(spy).to.be.called();
+        it('should instantiate attributes', () => {
+            expect(game.player1).to.exist;
+            expect(game.player2).to.exist;
+            expect(game.roundManager).to.exist;
+            expect(game.wordsPlayed).to.exist;
+            expect(game.gameType).to.exist;
+            expect(game.tileReserve).to.exist;
+            expect(game.board).to.exist;
+        });
+
+        it('should init TileReserve', () => {
+            expect(game.tileReserve.isInitialized()).to.be.true;
+        });
+
+        it('should give players their tiles', () => {
+            expect(game.player1.tiles).to.not.be.empty;
+            expect(game.player2.tiles).to.not.be.empty;
+        });
+    });
+
+    describe('createSoloGame', () => {
+        it('is not implemented', () => {
+            return expect(Game.createSoloGame()).to.be.rejectedWith('Solo mode not implemented');
+        });
+    });
+
+    describe('General', () => {
+        let game: Game;
+
+        beforeEach(async () => {
+            game = await Game.createMultiplayerGame(DEFAULT_MULTIPLAYER_CONFIG);
+        });
+
+        it('should retturn an id', () => {
+            expect(game.getId()).to.exist;
+        });
+
+        describe('getActivePlayer', () => {
+            it('should return player with same id (player 1)', () => {
+                const player = game.getActivePlayer(DEFAULT_PLAYER_1.getId());
+                expect(player).to.equal(DEFAULT_PLAYER_1);
+            });
+
+            it('should return player with same id (player 2)', () => {
+                const player = game.getActivePlayer(DEFAULT_PLAYER_2.getId());
+                expect(player).to.equal(DEFAULT_PLAYER_2);
+            });
+
+            it('should throw error if invalid id', () => {
+                const invalidId = 'invalidId';
+                expect(() => game.getActivePlayer(invalidId)).to.throw(Errors.INVALID_PLAYER_ID_FOR_GAME);
+            });
+        });
+
+        describe('getOpponentPlayer', () => {
+            it('should return player with other id (player 1)', () => {
+                const player = game.getOpponentPlayer(DEFAULT_PLAYER_1.getId());
+                expect(player).to.equal(DEFAULT_PLAYER_2);
+            });
+
+            it('should return player with other id (player 2)', () => {
+                const player = game.getOpponentPlayer(DEFAULT_PLAYER_2.getId());
+                expect(player).to.equal(DEFAULT_PLAYER_1);
+            });
+
+            it('should throw error if invalid id', () => {
+                const invalidId = 'invalidId';
+                expect(() => game.getOpponentPlayer(invalidId)).to.throw(Errors.INVALID_PLAYER_ID_FOR_GAME);
+            });
         });
     });
 });
