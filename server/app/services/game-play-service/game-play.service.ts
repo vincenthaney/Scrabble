22841d28dbee364c34d5971654cfdb0d3d249b53
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-01-29 22:17:06 -0500

    Tested game-play service

--- server/app/services/game-play-service/game-play.service.ts
+++ server/app/services/game-play-service/game-play.service.ts
@@ -1,19 +1,18 @@
 import { Action } from '@app/classes/actions';
 import { Service } from 'typedi';
-import { ActiveGameService } from '@app/services/active-game-service/active-game.service';
-import { SocketService } from '@app/services/socket-service/socket.service';
+// import { ActiveGameService } from '@app/services/active-game-service/active-game.service';
+// import { SocketService } from '@app/services/socket-service/socket.service';
 
 @Service()
 export class GamePlayService {
-    constructor(private readonly activeGameService: ActiveGameService, private readonly socketService: SocketService) {}
+    // constructor(private readonly activeGameService: ActiveGameService, private readonly socketService: SocketService) {}
 
     // eslint-disable-next-line no-unused-vars
     playAction(gameId: string, playerId: string, action: Action) {
-        // eslint-disable-next-line no-unused-vars
-        const game = this.activeGameService.getGame(gameId, playerId);
-        const opponentId = game.getOpponentPlayer(playerId).getId();
-
-        this.socketService.getSocket(playerId).emit('event', 'args');
-        this.socketService.getSocket(opponentId).emit('event', 'args');
+        throw new Error('Method not implemented');
+        // const game = this.activeGameService.getGame(gameId, playerId);
+        // const opponentId = game.getOpponentPlayer(playerId).getId();
+        // this.socketService.getSocket(playerId).emit('event', 'args');
+        // this.socketService.getSocket(opponentId).emit('event', 'args');
     }
 }
