1ea629f6afa6ffeab690cf54d03031a01c6d2d63
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-01-30 00:27:13 -0500

    RS: Add Multi9plier Type to index

--- server/app/classes/board/index.ts
+++ server/app/classes/board/index.ts
@@ -2,3 +2,4 @@ export { default as Board } from './board';
 export { default as Orientation } from './orientation';
 export { default as Position } from './position';
 export { default as Square } from './square';
+export { MultiplierType as MultiplierType } from './multiplier-type';
