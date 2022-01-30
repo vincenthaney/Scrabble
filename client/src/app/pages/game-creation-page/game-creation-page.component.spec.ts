878a6873fced217e0606e0288219a6147245f098
matbro-inextenso <matbro@inextenso.net>
2022-01-30 18:15:06 -0500

    last test fixed

--- client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
@@ -218,7 +218,7 @@ describe('GameCreationPageComponent', () => {
             name: 'something',
         };
         gameParametersForm.setValue(formValues);
-        expect(gameParametersForm.valid).toBeFalsy();
+        expect(gameParametersForm.valid).toBeTruthy();
     });
 
     it('form should not be valid if timer is empty', () => {
