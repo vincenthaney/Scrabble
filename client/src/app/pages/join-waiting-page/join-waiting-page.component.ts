ea0efe2fddad24f0f0f96810436bc93c2d7a2116
Vincent Haney <vincent.7aney@gmail.com>
2022-01-30 23:29:20 -0500

    ajout etats

--- client/src/app/pages/join-waiting-page/join-waiting-page.component.ts
+++ client/src/app/pages/join-waiting-page/join-waiting-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { DIALOG_BUTTON_CONTENT, DIALOG_CONTENT, DIALOG_TITLE, GameRequestState } from './join-waiting-page.component.const';
+import { DefaultDialogComponent } from '@app/components/default-dialog/default-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { OnlinePlayer } from '@app/classes/player';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-waiting-page',
@@ -6,6 +11,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./join-waiting-page.component.scss'],
 })
 export class JoinWaitingPageComponent {
+    state: GameRequestState.Waiting;
     waitingGameName: string = 'testName';
     waitingGameType: string = 'testType';
     waitingGameTimer: string = 'timer';
