77ba6e2b709a5bd0744dd8d6a1b537cab99362c5
Vincent Haney <vincent.7aney@gmail.com>
2022-02-02 00:26:29 -0500

    words verification with constants

--- server/app/constants/errors.ts
+++ server/app/constants/errors.ts
@@ -1,2 +1,3 @@
 export const NO_GAME_FOUND_WITH_ID = 'No game could be found with id';
 export const INVALID_PLAYER_ID_FOR_GAME = 'Invalid player id for game';
+export const EMPTY_WORD = 'Words should not be empty';
--- server/app/services/words-verification-service/words-verification.service.ts
+++ server/app/services/words-verification-service/words-verification.service.ts
@@ -1,4 +1,23 @@
+import { EMPTY_WORD } from '@app/constants/errors';
 import { Service } from 'typedi';
 
 @Service()
-export class WordsVerificationService {}
+export class WordsVerificationService {
+    activeDictionary: string = 'log2990_dictionary';
+
+    async verifyWords(words: string[][]) {
+        for (const word in words) {
+            if (word.length > 0) {
+                const letterDictionary = await import(`../assets/${this.activeDictionary}/${this.activeDictionary}_${word[0]}.json`);
+                if (word in letterDictionary) {
+                    continue;
+                } else {
+                    return word;
+                }
+            } else {
+                throw new Error(EMPTY_WORD);
+            }
+        }
+        return null;
+    }
+}
