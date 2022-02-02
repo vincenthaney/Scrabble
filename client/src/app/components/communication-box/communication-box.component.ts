e3ec445c99f3c52be3af6228395c1cb55d2b251d
matbro-inextenso <matbro@inextenso.net>
2022-02-01 23:53:46 -0500

    MB: Fixed layout of communication component

--- client/src/app/components/communication-box/communication-box.component.ts
+++ client/src/app/components/communication-box/communication-box.component.ts
@@ -9,28 +9,20 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class CommunicationBoxComponent {
     message: string;
-    messages: Array<string> = ["message 1", "message 2", "message 3", "message 4", "message 5", "message 6", "message 7", "message 8"]
+    messages: string[] = ['message 1', 'message 2', 'message 3', 'message 4', 'message 5', 'message 6', 'message 7', 'message 8'];
 
-    objectives: Array<string> = ["Objectif 1", "Objectif 2", "Objectif 3", "Objectif 4"];
-    
-    lettersLeft: Array<string> = [
-        'A : 2 ',
-        'B : 1 ',
-        'C : 2 ',
-        'D : 0 ',
-        'E : 4 ',
-    ]
-    
+    objectives: string[] = ['Objectif 1', 'Objectif 2', 'Objectif 3', 'Objectif 4'];
+
+    lettersLeft: string[] = ['A : 2 ', 'B : 1 ', 'C : 2 ', 'D : 0 ', 'E : 4 '];
+
     messageForm = new FormGroup({
         content: new FormControl(''),
-    })
+    });
 
     sendMessage() {
         if (this.message) {
-            this.messages = [...this.messages, this.message]
-            console.log("sent!! - " + this.message);
+            this.messages = [...this.messages, this.message];
             this.messageForm.reset();
-            console.log(this.messages);
         }
     }
 }
