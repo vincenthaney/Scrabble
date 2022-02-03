70832f1d402eedd70b28e77a51ffc0b6427cf3e3
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-02-02 23:56:43 -0500

    RS: Add action exchange implementation

--- server/app/classes/actions/action-exchange.ts
+++ server/app/classes/actions/action-exchange.ts
@@ -1,6 +1,11 @@
 import ActionPlay from './action-play';
 import { Tile } from '@app/classes/tile';
+// import { Game } from '@app/classes/game/game';
+import Player from '../player/player';
+import Game from '@app/classes/game/game';
+import { GameUpdateData, PlayerData } from '../game/game-update-data';
 
+const INVALID_COMMAND = 'a';
 export default class ActionExchange extends ActionPlay {
     tilesToExchange: Tile[];
 
@@ -9,8 +14,20 @@ export default class ActionExchange extends ActionPlay {
         this.tilesToExchange = tilesToExchange;
     }
 
-    execute(): void {
-        throw new Error('Method not implemented.');
+    // eslint-disable-next-line no-unused-vars
+    execute(game: Game, player: Player): GameUpdateData {
+        const containsAll = this.tilesToExchange.every((tile) => {
+            return player.tiles.includes(tile);
+        });
+        if (!containsAll) throw new Error(INVALID_COMMAND);
+
+        const newTiles = player.tiles.filter((tile) => {
+            return !this.tilesToExchange.includes(tile);
+        });
+        newTiles.concat(game.tileReserve.swapTiles(this.tilesToExchange));
+
+        const response: GameUpdateData = { playerId: player.getId(), player: { tiles: newTiles } };
+        return response;
     }
 
     getMessage(): string {
