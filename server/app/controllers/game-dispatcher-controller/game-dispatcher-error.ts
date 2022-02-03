e1f2d87caa9f070abc1582635d60d97fe00b54cd
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-03 17:16:46 -0500

    error messages more explicit

--- server/app/controllers/game-dispatcher-controller/game-dispatcher-error.ts
+++ server/app/controllers/game-dispatcher-controller/game-dispatcher-error.ts
@@ -1,4 +1,4 @@
-export const PLAYER_NAME_REQUIRED = 'playerName required';
-export const GAME_TYPE_REQUIRED = 'gameType required';
-export const MAX_ROUND_TIME_REQUIRED = 'maxRoundTime required';
-export const DICTIONARY_REQUIRED = 'dictionary required';
+export const PLAYER_NAME_REQUIRED = 'request body must include playerName';
+export const GAME_TYPE_REQUIRED = 'request body must include gameType';
+export const MAX_ROUND_TIME_REQUIRED = 'request body must include maxRoundTime';
+export const DICTIONARY_REQUIRED = 'request body must include dictionary';
