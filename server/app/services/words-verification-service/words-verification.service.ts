b2bb1cfb0d5c1167b21d1fa73830c65eff59f355
Vincent Haney <vincent.7aney@gmail.com>
2022-02-03 00:35:27 -0500

    verifyWords method

--- server/app/services/words-verification-service/words-verification.service.ts
+++ server/app/services/words-verification-service/words-verification.service.ts
@@ -4,6 +4,27 @@ import { Service } from 'typedi';
 @Service()
 export class WordsVerificationService {
 
+
+    verifyWords(words: string[][], dictionary: string) {
+        for (const word in words) {
+            if (word != null) {
+                this.removeAccents(word);
+                if (words.length > MINIMUM_WORD_LEN) {
+                    throw new Error(INVALID_WORD);
+                }
+                if (word.includes('-')) {
+                    throw new Error(INVALID_WORD);
+                }
+                if (word.includes("'")) {
+                    throw new Error(INVALID_WORD);
+                }
+                if (this.activeDictionaries.get(dictionary)?.has(word)) {
+                    throw new Error(INVALID_WORD);
+                }
+            }
+        }
+    }
+
     removeAccents(word: string) {
         return word.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
     }
