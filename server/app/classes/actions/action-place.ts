dfe27533029d13ad155ae101a847525d5cf814a0
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-02-03 09:51:06 -0500

    RS: First version of action place

--- server/app/classes/actions/action-place.ts
+++ server/app/classes/actions/action-place.ts
@@ -1,20 +1,29 @@
 import ActionPlay from './action-play';
 import { Tile } from '@app/classes/tile';
-import { Orientation, Square } from '@app/classes/board';
+import { Orientation, Position, Square } from '@app/classes/board';
+import Game from '../game/game';
+import { GameUpdateData } from '../game/game-update-data';
+import Player from '../player/player';
 
 export default class ActionPlace extends ActionPlay {
     tilesToPlace: Tile[];
-    startingSquare: Square;
+    startPosition: Position;
     orientation: Orientation;
 
-    constructor(tilesToPlace: Tile[], startingSquare: Square, orientation: Orientation) {
+    constructor(tilesToPlace: Tile[], startPosition: Position, orientation: Orientation) {
         super();
         this.tilesToPlace = tilesToPlace;
-        this.startingSquare = startingSquare;
+        this.startPosition = startPosition;
         this.orientation = orientation;
     }
 
-    execute(): void {
+    execute(game: Game, player: Player): GameUpdateData {
+        const containsAll = this.tilesToPlace.every((tile) => {
+            return player.tiles.includes(tile);
+        });
+        if (!containsAll) throw new Error(INVALID_COMMAND);
+        this.wordExtraction.extract(game.board, this.tilesToPlace, this.startPosition, this.orientation)
+
         throw new Error('Method not implemented.');
     }
 
