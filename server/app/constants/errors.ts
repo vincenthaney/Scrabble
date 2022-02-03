a16614891a0a2bfab22f35ad62fe11ee498a6e1e
Vincent Haney <vincent.7aney@gmail.com>
2022-02-03 00:35:10 -0500

    removeAccents method

--- server/app/constants/errors.ts
+++ server/app/constants/errors.ts
@@ -1,5 +1,4 @@
 export const NO_GAME_FOUND_WITH_ID = 'No game could be found with id';
 export const INVALID_PLAYER_ID_FOR_GAME = 'Invalid player id for game';
-export const EMPTY_WORD = 'Words should not be empty';
 export const MINIMUM_WORD_LEN = 2;
 export const INVALID_WORD = 'Invalid Word';
--- server/app/services/words-verification-service/words-verification.service.ts
+++ server/app/services/words-verification-service/words-verification.service.ts
@@ -1,9 +1,10 @@
-import { EMPTY_WORD, INVALID_WORD, MINIMUM_WORD_LEN } from '@app/constants/errors';
+import { INVALID_WORD, MINIMUM_WORD_LEN } from '@app/constants/errors';
 import { Service } from 'typedi';
 
 @Service()
 export class WordsVerificationService {
-    activeDictionaries: Map<string, Set<string>>;
 
-
+    removeAccents(word: string) {
+        return word.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    }
 }
