095036ac53ed08e74eb7c1c7b8fc2a48a50591f9
Vincent Haney <vincent.7aney@gmail.com>
2022-02-03 00:35:43 -0500

    dictionary structure

--- server/app/services/words-verification-service/words-verification.service.ts
+++ server/app/services/words-verification-service/words-verification.service.ts
@@ -3,7 +3,24 @@ import { Service } from 'typedi';
 
 @Service()
 export class WordsVerificationService {
+    activeDictionaries: Map<string, Set<string>>;
 
+    constructor() {
+        this.openAllDictionaries();
+    }
+
+    openAllDictionaries() {
+        this.addDictionary();
+    }
+
+    // À mettre dans un service à part
+    addDictionary() {
+        const name = 'dictionary_log2990';
+        const fs = require('fs');
+        const rawData = fs.readFileSync('assets/dictionaries/dictionnary.json');
+        const dictionary = JSON.parse(rawData);
+        this.activeDictionaries[name] = new Set(dictionary.words);
+    }
 
     verifyWords(words: string[][], dictionary: string) {
         for (const word in words) {
