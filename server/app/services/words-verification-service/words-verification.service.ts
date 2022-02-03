da02563b4922401022c47f9da2efa7d5b93b987b
Vincent Haney <vincent.7aney@gmail.com>
2022-02-03 01:22:04 -0500

    corrections

--- server/app/services/words-verification-service/words-verification.service.ts
+++ server/app/services/words-verification-service/words-verification.service.ts
@@ -31,9 +31,9 @@ export class WordsVerificationService {
 
     verifyWords(words: string[][], dictionary: string) {
         for (const word in words) {
-            if (word != null) {
+            if (!word) {
                 this.removeAccents(word);
-                if (words.length > MINIMUM_WORD_LEN) {
+                if (words.length < MINIMUM_WORD_LEN) {
                     throw new Error(INVALID_WORD);
                 }
                 if (word.includes('-')) {
@@ -42,7 +42,7 @@ export class WordsVerificationService {
                 if (word.includes("'")) {
                     throw new Error(INVALID_WORD);
                 }
-                if (this.activeDictionaries.get(dictionary)?.has(word)) {
+                if (!this.activeDictionaries.get(dictionary)?.has(word)) {
                     throw new Error(INVALID_WORD);
                 }
             }
