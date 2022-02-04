87dac0532f652abdbb851c1c12997357b4e08285
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-03 21:54:42 -0500

    Style communication box

--- client/src/app/components/communication-box/communication-box.component.ts
+++ client/src/app/components/communication-box/communication-box.component.ts
@@ -2,6 +2,8 @@ import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+type Message = { text: string; sender: string; date: Date; class: string };
+
 @Component({
     selector: 'app-communication-box',
     templateUrl: './communication-box.component.html',
@@ -12,17 +14,33 @@ export class CommunicationBoxComponent {
     @ViewChild(CdkVirtualScrollViewport, { static: false }) scrollViewport: CdkVirtualScrollViewport;
 
     message: string;
-    messages: string[] = [
-        'message 1',
-        'message 2',
-        'message 3',
-        'message 4',
-        'message 5',
-        'message 6',
-        'message 7',
-        'message 8',
-        "je suis un message très long qui va sûrement prendre plus qu'une ligne à afficher parce qu'il faut tester le wrap sur plusieurs lignes",
+    messages: Message[] = [
+        { text: 'message 1', sender: 'Mathilde', date: new Date(), class: 'me' },
+        { text: 'message 2', sender: 'Mathilde', date: new Date(), class: 'me' },
+        { text: 'message 3', sender: 'Raph', date: new Date(), class: 'opponent' },
+        { text: 'message 4', sender: 'Mathilde', date: new Date(), class: 'me' },
+        { text: 'message 5', sender: 'Raph', date: new Date(), class: 'opponent' },
+        { text: 'message 6', sender: 'Mathilde', date: new Date(), class: 'me' },
+        {
+            // eslint-disable-next-line max-len
+            text: "je suis un message très long qui va sûrement prendre plus qu'une ligne à afficher parce qu'il faut tester le wrap sur plusieurs lignes",
+            sender: 'Raph',
+            date: new Date(),
+            class: 'opponent',
+        },
     ];
+    // message: string;
+    // messages: string[] = [
+    //     'message 1',
+    //     'message 2',
+    //     'message 3',
+    //     'message 4',
+    //     'message 5',
+    //     'message 6',
+    //     'message 7',
+    //     'message 8',
+    //     "je suis un message très long qui va sûrement prendre plus qu'une ligne à afficher parce qu'il faut tester le wrap sur plusieurs lignes",
+    // ];
 
     objectives: string[] = ['Objectif 1', 'Objectif 2', 'Objectif 3', 'Objectif 4'];
 
@@ -34,7 +52,7 @@ export class CommunicationBoxComponent {
 
     sendMessage() {
         if (this.message) {
-            this.messages = [...this.messages, this.message];
+            this.messages = [...this.messages, { text: this.message, sender: 'Mathilde', date: new Date(), class: 'me' }];
             this.messageForm.reset();
             this.scrollToBottom();
         }
