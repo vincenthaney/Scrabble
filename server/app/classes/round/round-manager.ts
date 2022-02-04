648e68e1ad2098d0d8e8d23218a1e68562c2e9e9
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-03 19:25:28 -0500

    implement round-manager and use it

--- server/app/classes/round/round-manager.ts
+++ server/app/classes/round/round-manager.ts
@@ -1,21 +1,59 @@
+import Player from '@app/classes/player/player';
 import Round from './round';
+import { GAME_NOT_STARTED } from './round-manager-error';
+
+const SECONDS_TO_MILLISECONDS = 1000;
 
 export default class RoundManager {
-    // private currentRound: Round;
-    // private completedRounds: Round[];
-    // private maxRoundTime: number;
+    private player1: Player;
+    private player2: Player;
+    private currentRound: Round;
+    private completedRounds: Round[];
+    private maxRoundTime: number;
 
-    // constructor(maxRoundTime: number) {
-    // this.maxRoundTime = maxRoundTime;
-    // }
+    constructor(maxRoundTime: number, player1: Player, player2: Player) {
+        this.maxRoundTime = maxRoundTime;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.completedRounds = [];
+    }
 
     getStartGameTime(): Date {
-        throw new Error('Method not implemented.');
+        if (this.completedRounds.length === 0) {
+            if (this.currentRound) {
+                return this.currentRound.startTime;
+            } else {
+                throw new Error(GAME_NOT_STARTED);
+            }
+        } else {
+            return this.completedRounds[0].startTime;
+        }
     }
-    startRound(): Round {
-        throw new Error('Method not implemented.');
+
+    nextRound(): Round {
+        const player = this.getNextPlayer();
+        const now = new Date();
+        const limit = new Date(Date.now() + this.maxRoundTime * SECONDS_TO_MILLISECONDS);
+
+        if (this.currentRound !== undefined) {
+            this.completedRounds.push({ ...this.currentRound, completedTime: now });
+        }
+
+        return (this.currentRound = {
+            player,
+            startTime: now,
+            limitTime: limit,
+            completedTime: null,
+        });
     }
-    finishRound(): void {
-        throw new Error('Method not implemented.');
+
+    private getNextPlayer(): Player {
+        if (this.currentRound === undefined) {
+            // Randomly get a player
+            return Math.round(Math.random()) === 0 ? this.player1 : this.player2;
+        }
+
+        if (this.currentRound.player === this.player1) return this.player2;
+        else return this.player1;
     }
 }
