05f29408dce3b394467505e2b3f147befffa088c
matbro-inextenso <matbro@inextenso.net>
2022-02-04 00:42:36 -0500

    started tests on input-parser

--- client/src/app/services/input-parser/input-parser.service.spec.ts
+++ client/src/app/services/input-parser/input-parser.service.spec.ts
@@ -1,15 +1,76 @@
+/* eslint-disable dot-notation */
 import { TestBed } from '@angular/core/testing';
+import { Orientation } from '@app/classes/orientation';
+import { Position } from '@app/classes/position';
+import { InputControllerService } from '@app/controllers/input-controller/input-controller.service';
 import { InputParserService } from '@app/services';
+import { INVALID_COMMAND } from './command-errors';
 
 describe('InputParserService', () => {
     let service: InputParserService;
+    let inputControllerSpy: jasmine.SpyObj<InputControllerService>;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [{ provide: InputControllerService, useValue: inputControllerSpy }],
+        });
         service = TestBed.inject(InputParserService);
+        inputControllerSpy = jasmine.createSpyObj('InputControllerService', [
+            'sendPlaceAction',
+            'sendExchangeAction',
+            'sendPassAction',
+            'sendReserveAction',
+            'sendHintAction',
+            'sendHelpAction',
+            'sendMessage',
+        ]);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('getOrientation should throw invalid command if orientation string is invalid', () => {
+        const invalidOrientationStrings: string[] = ['vh', 'H', 'V', 'j', 'K', '*', '8'];
+
+        for (const invalidOrientationString of invalidOrientationStrings) {
+            expect(() => {
+                service['getOrientation'](invalidOrientationString);
+            }).toThrowError(INVALID_COMMAND);
+        }
+    });
+
+    it('getOrientation should return valid orientation if orientation string is valid', () => {
+        const validLocationStrings: string[] = ['v', 'h'];
+
+        expect(service['getOrientation'](validLocationStrings[0])).toBe(Orientation.Vertical);
+        expect(service['getOrientation'](validLocationStrings[1])).toBe(Orientation.Horizontal);
+    });
+
+    it('getStartPosition should throw invalid command if location string is invalid', () => {
+        const invalidLocationStrings: string[] = ['abcde', 'g', 'a143', 'A12', 'B3', '%4', 'a17', 'f0', 'o0', '14a'];
+
+        for (const invalidLocationString of invalidLocationStrings) {
+            expect(() => {
+                service['getStartPosition'](invalidLocationString);
+            }).toThrowError(INVALID_COMMAND);
+        }
+    });
+
+    it('getStartPosition should return valid Position if location string is valid', () => {
+        const validLocationStrings: string[] = ['a1', 'a15', 'g12', 'f1', 'm12', 'o15', 'o1'];
+        const expectedPositions: Position[] = [
+            { row: 0, column: 0 },
+            { row: 0, column: 14 },
+            { row: 6, column: 11 },
+            { row: 5, column: 0 },
+            { row: 12, column: 11 },
+            { row: 14, column: 14 },
+            { row: 14, column: 0 },
+        ];
+
+        for (let i = 0; i < validLocationStrings.length; i++) {
+            expect(service['getStartPosition'](validLocationStrings[i])).toEqual(expectedPositions[i]);
+        }
     });
 });
--- client/src/app/services/input-parser/input-parser.service.ts
+++ client/src/app/services/input-parser/input-parser.service.ts
@@ -1,28 +1,41 @@
 import { Injectable } from '@angular/core';
 import { ActionExchangePayload } from '@app/classes/actions/action-exchange';
 import { ActionPlacePayload } from '@app/classes/actions/action-place';
+import { Orientation } from '@app/classes/orientation';
+import { IPlayer } from '@app/classes/player';
 import { Position } from '@app/classes/position';
-import { InputControllerService } from '@app/input-controller.service';
+import { LetterValue, Tile } from '@app/classes/tile';
+import {
+    MAX_COL_NUMBER,
+    MAX_LOCATION_COMMAND_LENGTH,
+    MAX_ROW_NUMBER,
+    MIN_COL_NUMBER,
+    MIN_LOCATION_COMMAND_LENGTH,
+    MIN_ROW_NUMBER
+} from '@app/constants/game';
+import { InputControllerService } from '@app/controllers/input-controller/input-controller.service';
+import { GameService } from '..';
+import { INVALID_COMMAND } from './command-errors';
+
+const ASCII_VALUE_OF_LOWERCASE_A = 97;
 
 @Injectable({
     providedIn: 'root',
 })
 export default class InputParserService {
-    constructor(private controller: InputControllerService) {}
+    constructor(private controller: InputControllerService, private gameService: GameService) {}
 
     parseInput(input: string): void {
         if (input[0] === '!') {
+            // it is an action
             const inputWords: string[] = input.substring(1).split(' ');
             const actionName: string = inputWords[0];
-            console.log('action: ' + actionName);
 
             switch (actionName) {
                 case 'placer':
-                    // const placeActionPayload: ActionPlacePayload = this.createPlaceActionPayload(inputWords[1], inputWords[2]);
                     this.controller.sendPlaceAction(this.createPlaceActionPayload(inputWords[1], inputWords[2]));
                     break;
                 case 'échanger':
-                    // this.createExchangeActionPayload(inputWords[1]);
                     this.controller.sendExchangeAction(this.createExchangeActionPayload(inputWords[1]));
                     break;
                 case 'passer':
@@ -40,30 +53,96 @@ export default class InputParserService {
                 default:
             }
         } else {
-            console.log('ceci est un message');
+            this.controller.sendMessage(input);
         }
     }
 
-    createPlaceActionPayload(location: string, lettersToPlace: string) {
-        const inputStartPosition: Position = {
-            row: location[0].toLowerCase().charCodeAt(0) - 97,
-            column: +location.substring(1, location.length - 1) - 1,
-        };
-        console.log(inputStartPosition);
-
+    private createPlaceActionPayload(location: string, lettersToPlace: string) {
+        // try catch invalid command
         const placeActionPayload: ActionPlacePayload = {
-            letters: [...lettersToPlace],
-            startPosition: inputStartPosition,
-            orientation: location.charAt(location.length - 1),
+            tiles: this.parsePlaceLettersToTiles(lettersToPlace),
+            startPosition: this.getStartPosition(location.substring(0, location.length - 2)),
+            orientation: this.getOrientation(location.charAt(location.length - 1)),
         };
         console.log(placeActionPayload);
+
         return placeActionPayload;
     }
 
-    createExchangeActionPayload(letters: string) {
+    private createExchangeActionPayload(lettersToExchange: string) {
         const exchangeActionPayload: ActionExchangePayload = {
-            lettersToExchange: [...letters],
+            tiles: this.parseExchangeLettersToTiles(lettersToExchange),
         };
+        console.log(exchangeActionPayload);
+
         return exchangeActionPayload;
+    }
+
+    private parsePlaceLettersToTiles(lettersToPlace: string): Tile[] {
+        const player: IPlayer = this.gameService.getLocalPlayer();
+        const playerTiles: Tile[] = { ...player.tiles };
+        const tilesToPlace: Tile[] = [];
+
+        for (const letter of lettersToPlace) {
+            for (let i = Object.values(playerTiles).length - 1; i >= 0; i--) {
+                if (playerTiles[i].letter.toLowerCase() === letter) {
+                    tilesToPlace.push(playerTiles[i]);
+                    break;
+                } else if (playerTiles[i].letter.toLowerCase() === '*' && (letter.toLowerCase() as LetterValue)) {
+                    const tile = playerTiles[i];
+                    tile.letter = letter.toLowerCase() as LetterValue;
+                    tilesToPlace.push(tile);
+                    break;
+                }
+            }
+        }
+        if (tilesToPlace.length !== lettersToPlace.length) throw new Error(INVALID_COMMAND);
+
+        return tilesToPlace;
+    }
+
+    private parseExchangeLettersToTiles(lettersToExchange: string): Tile[] {
+        const player: IPlayer = this.gameService.getLocalPlayer();
+        const playerTiles: Tile[] = { ...player.tiles };
+        const tilesToExchange: Tile[] = [];
+
+        for (const letter of lettersToExchange) {
+            for (let i = Object.values(playerTiles).length - 1; i >= 0; i--) {
+                if (playerTiles[i].letter.toLowerCase() === letter) {
+                    const tile = playerTiles[i];
+                    tilesToExchange.push(tile);
+                    break;
+                }
+            }
+        }
+
+        if (tilesToExchange.length !== lettersToExchange.length) throw new Error(INVALID_COMMAND);
+
+        return tilesToExchange;
+    }
+
+    private getStartPosition(location: string): Position {
+        if (location.length > MAX_LOCATION_COMMAND_LENGTH || location.length < MIN_LOCATION_COMMAND_LENGTH) throw new Error(INVALID_COMMAND);
+
+        const inputRow: number = location[0].charCodeAt(0) - ASCII_VALUE_OF_LOWERCASE_A;
+        if (inputRow < MIN_ROW_NUMBER || inputRow > MAX_ROW_NUMBER) throw new Error(INVALID_COMMAND);
+
+        const inputCol: number = +location.substring(1) - 1;
+        if (inputCol < MIN_COL_NUMBER || inputCol > MAX_COL_NUMBER) throw new Error(INVALID_COMMAND);
+
+        const inputStartPosition: Position = {
+            row: inputRow,
+            column: inputCol,
+        };
+
+        return inputStartPosition;
+    }
+
+    private getOrientation(orientationString: string): Orientation {
+        if (orientationString.length !== 1) throw new Error(INVALID_COMMAND);
+
+        if (orientationString === 'h') return Orientation.Horizontal;
+        else if (orientationString === 'v') return Orientation.Vertical;
+        else throw new Error(INVALID_COMMAND);
     }
 }
