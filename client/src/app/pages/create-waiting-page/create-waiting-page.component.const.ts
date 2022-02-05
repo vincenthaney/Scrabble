a04f6982f6b59a9bfcc22b551e4253cf1835126f
Thomas <thomtrepanier@gmail.com>
2022-02-05 18:20:51 -0500

    Fix button routing

--- client/src/app/pages/create-waiting-page/create-waiting-page.component.const.ts
+++ client/src/app/pages/create-waiting-page/create-waiting-page.component.const.ts
@@ -1,4 +1,4 @@
-export const HOST_WAITING_MESSAGE = "En attender d'un adversative.";
+export const HOST_WAITING_MESSAGE = "En attender d'un adversaire.";
 export const OPPONENT_FOUND_MESSAGE = 'a rejoint votre partie.';
 export const DIALOG_TITLE = 'Attention!';
 export const DIALOG_CONTENT = "a rejoint le salon. Veuillez patientez le temps qu'un autre joueur veuille vous affronter.";
