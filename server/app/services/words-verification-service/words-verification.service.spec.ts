5893f82477f8796cbe5ab0ff476729f2a845b1f3
Vincent Haney <vincent.7aney@gmail.com>
2022-02-06 15:16:48 -0500

    verif tests

--- server/app/services/words-verification-service/words-verification.service.spec.ts
+++ server/app/services/words-verification-service/words-verification.service.spec.ts
@@ -5,6 +5,7 @@
 import { INVALID_WORD, WORD_CONTAINS_APOSTROPHE, WORD_CONTAINS_HYPHEN, WORD_TOO_SHORT } from '@app/constants/errors';
 import { expect } from 'chai';
 import { WordsVerificationService } from './words-verification.service';
+import { WordError } from './words-verification.service.classes';
 import { DICTIONARY_NAME } from './words-verification.service.const';
 
 describe('WordsVerificationService', () => {
@@ -28,22 +29,22 @@ describe('WordsVerificationService', () => {
 
     it('should return error because word too short', () => {
         const result = () => wordsVerificationService.verifyWords(['a'], DICTIONARY_NAME);
-        expect(result).to.Throw(WORD_TOO_SHORT);
+        expect(result).to.Throw(new WordError(WORD_TOO_SHORT, 'a'));
     });
 
     it('should return error because word contains hyphen', () => {
         const result = () => wordsVerificationService.verifyWords(['a-a'], DICTIONARY_NAME);
-        expect(result).to.Throw(WORD_CONTAINS_HYPHEN);
+        expect(result).to.Throw(new WordError(WORD_CONTAINS_HYPHEN, 'a-a'));
     });
 
     it('should return error because word contains apostrophe', () => {
         const result = () => wordsVerificationService.verifyWords(["aaaa'aaaa"], DICTIONARY_NAME);
-        expect(result).to.Throw(WORD_CONTAINS_APOSTROPHE);
+        expect(result).to.Throw(new WordError(WORD_CONTAINS_APOSTROPHE, "aaaa'aaaa"));
     });
 
     it('should return error if word is not in dictionary', () => {
         const result = () => wordsVerificationService.verifyWords(['ufdwihfewa'], DICTIONARY_NAME);
-        expect(result).to.Throw(INVALID_WORD);
+        expect(result).to.Throw(new WordError(INVALID_WORD, 'ufdwihfewa'));
     });
 
     it('should be true when word is in the dictionary', () => {
@@ -51,6 +52,8 @@ describe('WordsVerificationService', () => {
         const dictionary = wordsVerificationService.activeDictionaries.get(DICTIONARY_NAME);
         if (dictionary) {
             dictionary.forEach((word) => {
+                // eslint-disable-next-line no-console
+                console.log(words.length);
                 if (words.length < 1) {
                     words.join(word);
                 }
@@ -66,6 +69,8 @@ describe('WordsVerificationService', () => {
         const dictionary = wordsVerificationService.activeDictionaries.get(DICTIONARY_NAME);
         if (dictionary) {
             dictionary.forEach((word) => {
+                // eslint-disable-next-line no-console
+                console.log(words.length);
                 if (words.length < 3) {
                     words.join(word);
                 }
