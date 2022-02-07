ed743c1c1c46e00f3510c6f95b092610a7ca140a
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-02-06 22:28:20 -0500

    RS: Add dialog for opponent leaving

--- client/src/app/pages/join-waiting-page/join-waiting-page.component.const.ts
+++ client/src/app/pages/join-waiting-page/join-waiting-page.component.const.ts
@@ -4,5 +4,7 @@ export enum GameRequestState {
     Rejected = 'rejected',
 }
 export const DIALOG_BUTTON_CONTENT = 'Retourner à la sélection de parties.';
-export const DIALOG_TITLE = 'Partie rejetée';
-export const DIALOG_CONTENT = 'a refusé la partie.';
+export const DIALOG_REJECT_TITLE = 'Partie rejetée';
+export const DIALOG_REJECT_CONTENT = ' a refusé la partie.';
+export const DIALOG_CANCEL_TITLE = 'Partie annulée';
+export const DIALOG_CANCEL_CONTENT = ' a annulé la partie.';
