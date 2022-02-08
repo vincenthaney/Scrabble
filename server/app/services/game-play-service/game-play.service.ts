a0592c4309c3770384ea211bf23a44e0482acd8b
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-08 18:16:32 -0500

    moved switch statement to its own function

--- server/app/services/game-play-service/game-play.service.ts
+++ server/app/services/game-play-service/game-play.service.ts
@@ -1,53 +1,61 @@
+/* eslint-disable no-dupe-class-members */
 import { Action, ActionExchange, ActionPass, ActionPlace } from '@app/classes/actions';
-import { ActionData } from '@app/classes/communication/action-data';
+import { ActionData, ActionExchangePayload, ActionPlacePayload } from '@app/classes/communication/action-data';
 import { GameUpdateData } from '@app/classes/communication/game-update-data';
-// import { HttpException } from '@app/classes/http.exception';
 import { Service } from 'typedi';
 import { ActiveGameService } from '@app/services/active-game-service/active-game.service';
-// import { SocketService } from '@app/services/socket-service/socket.service';
-
-const INVALID_COMMAND = 'The command is not one of the recognised commands. type !help for a list of possible commands';
-const NOT_PLAYER_TURN = 'It is not the turn of requesting player';
+import Player from '@app/classes/player/player';
+import Game from '@app/classes/game/game';
+import { INVALID_COMMAND, INVALID_PAYLOAD, NOT_PLAYER_TURN } from './game-player-error';
 
 @Service()
 export class GamePlayService {
-    // constructor(private readonly activeGameService: ActiveGameService, private readonly socketService: SocketService) {}
     constructor(private readonly activeGameService: ActiveGameService) {}
 
-    // playAction(gameId: string, playerId: string, action: Action) {  //GameUpdateData {
-    playAction(gameId: string, playerId: string, actionData: ActionData): GameUpdateData {
+    playAction(gameId: string, playerId: string, actionData: ActionData): GameUpdateData | void {
         const game = this.activeGameService.getGame(gameId, playerId);
-        const currentPlayer = game.getCurrentPlayer();
-        if (currentPlayer.getId() !== playerId) throw Error(NOT_PLAYER_TURN);
+        const player = game.getRequestingPlayer(playerId);
+        if (player.getId() !== playerId) throw Error(NOT_PLAYER_TURN);
 
+        const action: Action = this.getAction(player, game, actionData.type, actionData.payload);
+        let updatedData: void | GameUpdateData = action.execute();
+        if (action.willEndTurn()) {
+            const nextRound = game.roundManager.nextRound(action);
+            if (updatedData) updatedData.round = nextRound;
+            else updatedData = { round: nextRound };
+        }
+        if (game.isGameOver()) {
+            if (updatedData) updatedData.isGameOver = true;
+            else updatedData = { isGameOver: true };
+        }
+        return updatedData;
+    }
+
+    getAction(player: Player, game: Game, type: string, payload: unknown): Action {
         let action: Action;
-        switch (actionData.type) {
+        switch (type) {
             case 'place': {
-                action = new ActionPlace(actionData.payload.tiles, actionData.payload.position, actionData.payload.orientation);
+                const content = payload as ActionPlacePayload;
+                if (content.tiles === undefined || !Array.isArray(content.tiles)) throw new Error(INVALID_PAYLOAD);
+                if (content.position === undefined) throw new Error(INVALID_PAYLOAD);
+                if (content.orientation === undefined) throw new Error(INVALID_PAYLOAD);
+                action = new ActionPlace(player, game, content.tiles, content.position, content.orientation);
                 break;
             }
             case 'exchange': {
-                action = new ActionExchange(actionData.payload.tiles);
+                const content = payload as ActionExchangePayload;
+                if (content.tiles === undefined || !Array.isArray(content.tiles)) throw new Error(INVALID_PAYLOAD);
+                action = new ActionExchange(player, game, content.tiles);
                 break;
             }
             case 'pass': {
-                action = new ActionPass();
+                action = new ActionPass(player, game);
                 break;
             }
             default: {
                 throw Error(INVALID_COMMAND);
             }
         }
-        const updatedData = action.execute(game, game.getRequestingPlayer(playerId));
-        if (action.willEndTurn()) {
-            game.roundManager.nextRound();
-        }
-        if (game.isGameOver()) {
-            //
-        }
-        return updatedData;
-        // const opponentId = game.getOpponentPlayer(playerId).getId();
-        // this.socketService.getSocket(playerId).emit('event', 'args');
-        // this.socketService.getSocket(opponentId).emit('event', 'args');
+        return action;
     }
 }
