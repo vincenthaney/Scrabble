a20f0ba1a38c7690d53f9a225224db7ee61b51a4
thomtrep <thomtrepanier@gmail.com>
2022-02-08 15:48:35 -0500

    Updated timer tests

--- client/src/app/classes/utils.spec.ts
+++ client/src/app/classes/utils.spec.ts
@@ -4,17 +4,17 @@ describe('Util functions', () => {
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     const TIMES = [0, 1, 2, 30, 60, 61, 90, 120, 121, 150, 243];
     const EXPECTED_STRINGS = [
-        '',
-        '1 seconde',
-        '2 secondes',
-        '30 secondes',
-        '1 minute',
-        '1 minute et 1 seconde',
-        '1 minute et 30 secondes',
-        '2 minutes',
-        '2 minutes et 1 seconde',
-        '2 minutes et 30 secondes',
-        '4 minutes et 3 secondes',
+        { minutes: 0, seconds: 0 },
+        { minutes: 0, seconds: 1 },
+        { minutes: 0, seconds: 2 },
+        { minutes: 0, seconds: 30 },
+        { minutes: 1, seconds: 0 },
+        { minutes: 1, seconds: 1 },
+        { minutes: 1, seconds: 30 },
+        { minutes: 2, seconds: 0 },
+        { minutes: 2, seconds: 1 },
+        { minutes: 2, seconds: 30 },
+        { minutes: 4, seconds: 3 },
     ];
 
     for (let i = 0; i < TIMES.length; i++) {
