126c8b4c69b4ed81d2f78a68d31bb65474e765b5
Thomas <thomtrepanier@gmail.com>
2022-02-07 22:07:22 -0500

    TileRack initialization fix

--- client/src/app/components/tile-rack/tile-rack.component.ts
+++ client/src/app/components/tile-rack/tile-rack.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IPlayer } from '@app/classes/player';
 import { Tile } from '@app/classes/tile';
 import { GameService } from '@app/services';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-tile-rack',
     templateUrl: './tile-rack.component.html',
     styleUrls: ['./tile-rack.component.scss'],
 })
-export class TileRackComponent implements OnInit {
+export class TileRackComponent {
     tiles: Tile[];
+    startGameSubscription: Subscription;
 
-    constructor(private gameService: GameService) {}
-    ngOnInit() {
-        this.initializeTileRack();
+    constructor(private gameService: GameService) {
+        this.startGameSubscription = this.gameService.startGameEvent.subscribe(() => this.initializeTileRack());
     }
 
     private initializeTileRack() {
@@ -22,7 +23,6 @@ export class TileRackComponent implements OnInit {
         if (!localPlayer || !localPlayer.getTiles()) {
             return;
         }
-
         localPlayer.getTiles().forEach((tile: Tile) => {
             this.tiles.push({ letter: tile.letter, value: tile.value });
         });
