5a0f0af152a13badd2954ab0e6c552e0a92e1484
Vincent Haney <vincent.7aney@gmail.com>
2022-02-08 15:14:10 -0500

    space before errors

--- server/app/constants/errors.ts
+++ server/app/constants/errors.ts
@@ -1,8 +1,8 @@
 export const NO_GAME_FOUND_WITH_ID = 'No game could be found with id';
 export const INVALID_PLAYER_ID_FOR_GAME = 'Invalid player id for game';
 export const MINIMUM_WORD_LENGTH = 2;
-export const INVALID_WORD = 'Word not in dictionary';
+export const INVALID_WORD = ' Word not in dictionary';
 export const WORD_TOO_SHORT = ' Word too short';
 export const WORD_CONTAINS_HYPHEN = ' Word cannot contain hyphen';
 export const WORD_CONTAINS_APOSTROPHE = ' Word cannot contain apostrophe';
-export const WORD_CONTAINS_ASTERISK = 'Word cannot contain asterisk';
+export const WORD_CONTAINS_ASTERISK = ' Word cannot contain asterisk';
