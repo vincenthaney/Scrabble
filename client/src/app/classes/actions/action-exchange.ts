30fbde679112a1e15478d68b7a123ec2e9624782
matbro-inextenso <matbro@inextenso.net>
2022-02-08 20:20:15 -0500

    deleted actions from client

---
+++ client/src/app/classes/actions/action-exchange.ts
@@ -0,0 +1,24 @@
+import { ActionPlay } from '@app/classes/actions';
+import { Tile } from '@app/classes/tile';
+
+export interface ActionExchangePayload {
+    tiles: Tile[];
+}
+
+export default class ActionExchange extends ActionPlay {
+    tilesToExchange: Tile[];
+
+    constructor(tilesToExchange: Tile[]) {
+        super();
+        this.tilesToExchange = tilesToExchange;
+        throw new Error('Method not implemented.');
+    }
+
+    execute(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    getMessage(): string {
+        throw new Error('Method not implemented.');
+    }
+}
