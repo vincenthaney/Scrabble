69570fe510dfb5d25ed795b6ab3a500367cca923
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-09 11:20:11 -0500

    use join instead of reduce

--- server/app/classes/actions/action-reserve/action-reserve.ts
+++ server/app/classes/actions/action-reserve/action-reserve.ts
@@ -4,8 +4,8 @@ import { LetterValue } from '@app/classes/tile';
 export default class ActionReserve extends ActionInfo {
     getMessage(): string | undefined {
         const map = this.game.tileReserve.getTilesLeftPerLetter();
-        const arr: [tiles: LetterValue, amount: number][] = Array.from(map, ([v, k]) => [v, k]);
-        const message = arr.reduce((prev, [letter, amount]) => (prev += `${letter.toUpperCase()}: ${amount}, `), '');
+        const arr: [letter: LetterValue, amount: number][] = Array.from(map, ([v, k]) => [v, k]);
+        const message = arr.map(([letter, amount]) => `${letter}: ${amount}`).join(', ');
         return message;
     }
     getOpponentMessage(): undefined {
