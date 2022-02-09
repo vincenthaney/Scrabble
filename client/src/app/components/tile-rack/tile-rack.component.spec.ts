2707c0352457c49fa5312dca9f25dd6e2982cc70
Thomas <thomtrepanier@gmail.com>
2022-02-09 16:42:51 -0500

    Added tests to TileRack

--- client/src/app/components/tile-rack/tile-rack.component.spec.ts
+++ client/src/app/components/tile-rack/tile-rack.component.spec.ts
@@ -1,4 +1,7 @@
+/* eslint-disable dot-notation */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CommonModule } from '@angular/common';
+import { EventEmitter } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -17,17 +20,23 @@ import { TileComponent } from '@app/components/tile/tile.component';
 import { AppMaterialModule } from '@app/modules/material.module';
 import { GameService } from '@app/services';
 import { TileRackComponent } from './tile-rack.component';
-import SpyObj = jasmine.SpyObj;
+
+class MockGameService {
+    startGameEvent: EventEmitter<void> = new EventEmitter();
+    getLocalPlayer(): AbstractPlayer | undefined {
+        return new Player('id', 'name', []);
+    }
+
+    isLocalPlayerPlaying(): boolean {
+        return true;
+    }
+}
 
 describe('TileRackComponent', () => {
     const EMPTY_TILE_RACK: Tile[] = [];
-    let gameServiceSpy: SpyObj<GameService>;
+    const mockGameService = new MockGameService();
     let component: TileRackComponent;
     let fixture: ComponentFixture<TileRackComponent>;
-
-    beforeEach(async () => {
-        gameServiceSpy = jasmine.createSpyObj('GameService', ['getLocalPlayer', 'isLocalPlayerPlaying']);
-    });
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -47,7 +56,7 @@ describe('TileRackComponent', () => {
                 MatDialogModule,
             ],
             declarations: [TileRackComponent, IconComponent, TileComponent],
-            providers: [{ provide: GameService, useValue: gameServiceSpy }],
+            providers: [{ provide: GameService, useValue: mockGameService }],
         }).compileComponents();
     });
 
@@ -61,29 +70,44 @@ describe('TileRackComponent', () => {
         expect(component).toBeTruthy();
     });
 
+    it('should call initializeTileRack when startGameEvent is received', () => {
+        const spy = spyOn<any>(component, 'initializeTileRack');
+        mockGameService.startGameEvent.emit();
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('ngOnDestroy should unsubscribe from startGameEvent', () => {
+        const spy = spyOn<any>(component.startGameSubscription, 'unsubscribe');
+        component.ngOnDestroy();
+        expect(spy).toHaveBeenCalled();
+    });
+
     it('Initializing TileRack with no Player in Game should return empty TileRack', () => {
-        // eslint-disable-next-line dot-notation
+        spyOn(mockGameService, 'getLocalPlayer').and.returnValue(undefined);
         component['initializeTileRack']();
         expect(component.tiles).toEqual(EMPTY_TILE_RACK);
     });
 
     it('Initializing TileRack with player with no tiles should return empty TileRack', () => {
         const localPlayer: AbstractPlayer = new Player('', 'Test', []);
-        gameServiceSpy.getLocalPlayer.and.returnValue(localPlayer);
+
+        spyOn(mockGameService, 'getLocalPlayer').and.returnValue(localPlayer);
         spyOn(localPlayer, 'getTiles').and.returnValue([]);
-        // eslint-disable-next-line dot-notation
+
         component['initializeTileRack']();
+
         expect(component.tiles).toEqual(EMPTY_TILE_RACK);
     });
 
     it('Initializing TileRack with player with tiles should return the player tiles', () => {
         const tiles: Tile[] = [{ letter: 'A', value: 10 }];
         const localPlayer: AbstractPlayer = new Player('', 'Test', []);
-        gameServiceSpy.getLocalPlayer.and.returnValue(localPlayer);
+
+        spyOn(mockGameService, 'getLocalPlayer').and.returnValue(localPlayer);
         spyOn(localPlayer, 'getTiles').and.returnValue(tiles);
-        // eslint-disable-next-line dot-notation
+
         component['initializeTileRack']();
+
         expect(component.tiles[0]).toBeTruthy();
-        // expect(component.tiles[0].value).toEqual(tiles[0].value);
     });
 });
