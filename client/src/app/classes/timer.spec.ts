7c674863aed4467e36fd4f9892df97ce2e60203b
Thomas <thomtrepanier@gmail.com>
2022-02-09 12:41:51 -0500

    Timer tests done

---
+++ client/src/app/classes/timer.spec.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
+import { ILLEGAL_TIMER_PARAMETERS, Timer } from './timer';
+
+interface TimerParameter {
+    minutes: number;
+    seconds: number;
+}
+
+describe('Timer', () => {
+    const timerParameters: Map<TimerParameter, boolean> = new Map([
+        [{ minutes: 0, seconds: 0 }, true],
+        [{ minutes: 0, seconds: 1 }, true],
+        [{ minutes: 1, seconds: 0 }, true],
+        [{ minutes: -1, seconds: 0 }, false],
+        [{ minutes: 0, seconds: -1 }, false],
+    ]);
+
+    const timerDecrementTestCases: { initialTimer: TimerParameter; finalTimer: TimerParameter }[] = [
+        { initialTimer: { minutes: 0, seconds: 0 }, finalTimer: { minutes: 0, seconds: 0 } },
+        { initialTimer: { minutes: 0, seconds: 1 }, finalTimer: { minutes: 0, seconds: 0 } },
+        { initialTimer: { minutes: 0, seconds: 2 }, finalTimer: { minutes: 0, seconds: 1 } },
+        { initialTimer: { minutes: 1, seconds: 0 }, finalTimer: { minutes: 0, seconds: 59 } },
+        { initialTimer: { minutes: 1, seconds: 1 }, finalTimer: { minutes: 1, seconds: 0 } },
+        { initialTimer: { minutes: 2, seconds: 0 }, finalTimer: { minutes: 1, seconds: 59 } },
+    ];
+
+    timerParameters.forEach((isValid: boolean, timerParams: TimerParameter) => {
+        const isValidText = isValid ? 'a valid' : 'an invalid';
+        const afterShouldText = isValid ? 'assign values to properties' : 'throw an error';
+        it('Creating ' + isValidText + ' Timer should ' + afterShouldText, () => {
+            if (isValid) {
+                const timer = new Timer(timerParams.minutes, timerParams.seconds);
+                expect(timer.minutes).toEqual(timerParams.minutes);
+                expect(timer.seconds).toEqual(timerParams.seconds);
+            } else {
+                expect(() => new Timer(timerParams.minutes, timerParams.seconds)).toThrowError(ILLEGAL_TIMER_PARAMETERS);
+            }
+        });
+    });
+
+    timerDecrementTestCases.forEach((value: { initialTimer: TimerParameter; finalTimer: TimerParameter }) => {
+        it(`Decrementing timer at ${value.initialTimer.minutes}:${value.initialTimer.seconds} should set timer 
+            to ${value.finalTimer.minutes}:${value.finalTimer.seconds}`, () => {
+            const timer = new Timer(value.initialTimer.minutes, value.initialTimer.seconds);
+            timer.decrement();
+            expect(timer.minutes).toEqual(value.finalTimer.minutes);
+            expect(timer.seconds).toEqual(value.finalTimer.seconds);
+        });
+    });
+
+    it('getTimerSecondsPadded should always return string with 2 digits', () => {
+        const timer: Timer = new Timer(0, 10);
+        expect(timer.getTimerSecondsPadded().length).toEqual(2);
+    });
+
+    it('getTimerSecondsPadded should always return string with 2 digits', () => {
+        const timer: Timer = new Timer(0, 1);
+        expect(timer.getTimerSecondsPadded().length).toEqual(2);
+    });
+});
--- client/src/app/classes/timer.ts
+++ client/src/app/classes/timer.ts
@@ -3,15 +3,16 @@ export class Timer {
     seconds: number;
 
     constructor(minutes: number, seconds: number) {
+        if (minutes < 0 || seconds < 0) throw new Error(ILLEGAL_TIMER_PARAMETERS);
         this.minutes = minutes;
         this.seconds = seconds;
     }
     decrement(): void {
         if (this.seconds > 0) {
             this.seconds--;
-        } else {
+        } else if (this.minutes > 0) {
             this.minutes--;
-            this.seconds = 60;
+            this.seconds = 59;
         }
     }
 
@@ -19,3 +20,5 @@ export class Timer {
         return this.seconds.toString().padStart(2, '0');
     }
 }
+
+export const ILLEGAL_TIMER_PARAMETERS = 'The arguments passed to create the timer are not valid (minute < 0 or seconds < 0)';
