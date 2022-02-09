fd62a5eb25d6e7685480c15041d56a95f0c2351d
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-08 19:22:09 -0500

    action hint

---
+++ server/app/classes/actions/action-utils/action-utils.spec.ts
@@ -0,0 +1,97 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { Tile } from '@app/classes/tile';
+import Player from '@app/classes/player/player';
+import { ActionUtils } from './action-utils';
+import { expect } from 'chai';
+import { stub } from 'sinon';
+import { ERROR_PLAYER_DOESNT_HAVE_TILE } from '@app/classes/actions/action-error';
+
+const DEFAULT_PLAYER_NAME = 'player1';
+const DEFAULT_PLAYER_ID = '1';
+
+describe('ActionUtils', () => {
+    describe('getTilesFromPlayer', () => {
+        let player: Player;
+
+        beforeEach(() => {
+            player = new Player(DEFAULT_PLAYER_ID, DEFAULT_PLAYER_NAME);
+        });
+
+        it('should return tiles to place and unplayed tiles', () => {
+            const playerTiles: Tile[] = [
+                { letter: 'a', value: 0 },
+                { letter: 'b', value: 0 },
+            ];
+            const tilesToPlay: Tile[] = [playerTiles[0]];
+            const unplayedTilesExpected: Tile[] = [playerTiles[1]];
+
+            player.tiles = playerTiles;
+
+            const [resTilesToPlace, resUnplayedTiles] = ActionUtils.getTilesFromPlayer(tilesToPlay, player);
+
+            expect(tilesToPlay.every((t) => resTilesToPlace.some((t2) => t.letter === t2.letter && t.value === t2.value))).to.be.true;
+            expect(unplayedTilesExpected.every((t) => resUnplayedTiles.some((t2) => t.letter === t2.letter && t.value === t2.value))).to.be.true;
+        });
+
+        it("should throw if playing letter the player doesn't have", () => {
+            const playerTiles: Tile[] = [
+                { letter: 'a', value: 0 },
+                { letter: 'b', value: 0 },
+            ];
+            const tilesToPlay: Tile[] = [{ letter: 'c', value: 0 }];
+
+            player.tiles = playerTiles;
+
+            const INVALID_INDEX = -1;
+            const s = stub(ActionUtils, 'getIndexOfTile').onFirstCall().returns(INVALID_INDEX);
+
+            expect(() => ActionUtils.getTilesFromPlayer(tilesToPlay, player)).to.throw(ERROR_PLAYER_DOESNT_HAVE_TILE);
+
+            s.restore();
+        });
+    });
+
+    describe('getIndexOfTile', () => {
+        it('should return index of tile in array', () => {
+            const tiles: Tile[] = [
+                { letter: 'a', value: 0 },
+                { letter: 'b', value: 0 },
+                { letter: 'c', value: 0 },
+            ];
+
+            const expected = 1;
+            const result = ActionUtils.getIndexOfTile(tiles, tiles[expected]);
+
+            expect(result).to.equal(expected);
+        });
+
+        it('should return index of wildcard when tile not in array', () => {
+            const tiles: Tile[] = [
+                { letter: 'a', value: 0 },
+                { letter: 'b', value: 0 },
+                { letter: '*', value: 0 },
+            ];
+            const tile: Tile = { letter: 'z', value: 0 };
+
+            const expected = 2;
+            const result = ActionUtils.getIndexOfTile(tiles, tile);
+
+            expect(result).to.equal(expected);
+        });
+
+        it('should return -1 when tile not in array', () => {
+            const tiles: Tile[] = [
+                { letter: 'a', value: 0 },
+                { letter: 'b', value: 0 },
+                { letter: 'c', value: 0 },
+            ];
+            const tile: Tile = { letter: 'z', value: 0 };
+
+            const expected = -1;
+            const result = ActionUtils.getIndexOfTile(tiles, tile);
+
+            expect(result).to.equal(expected);
+        });
+    });
+});
