541cca9115f0726beff9536343199738dc575510
Thomas <thomtrepanier@gmail.com>
2022-02-09 23:53:08 -0500

    Changed timer display in lobby info

--- client/src/app/components/lobby-info/lobby-info.component.ts
+++ client/src/app/components/lobby-info/lobby-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { LobbyInfo } from '@app/classes/communication/lobby-info';
 import { GameType } from '@app/classes/game-type';
 import { Timer } from '@app/classes/timer';
@@ -8,20 +8,22 @@ import { Timer } from '@app/classes/timer';
     templateUrl: './lobby-info.component.html',
     styleUrls: ['./lobby-info.component.scss'],
 })
-export class LobbyInfoComponent {
+export class LobbyInfoComponent implements OnInit {
     @Input() lobby: LobbyInfo;
     @Output() joinLobbyId = new EventEmitter<string>();
+    roundTime: Timer;
     // dictionnaries: Dictionnaries[]
 
     constructor() {
         this.lobby = { lobbyId: '0', dictionary: '', playerName: '', gameType: GameType.Classic, maxRoundTime: 0, canJoin: false };
+        this.roundTime = Timer.convertTime(this.lobby.maxRoundTime);
+    }
+
+    ngOnInit() {
+        this.roundTime = Timer.convertTime(this.lobby.maxRoundTime);
     }
 
     joinLobby() {
         this.joinLobbyId.emit(this.lobby.lobbyId);
-    }
-
-    convertTime(): Timer {
-        return Timer.convertTime(this.lobby.maxRoundTime);
     }
 }
