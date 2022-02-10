c15030531210fd31df593c3c5e598c06975f0005
matbro-inextenso <matbro@inextenso.net>
2022-02-09 23:03:03 -0500

    Deleted message service (not useful after all)

--- server/app/services/message/message-error.ts
+++
@@ -1,3 +0,0 @@
-// export const INVALID_COMMAND = 'The command is not one of the recognised commands. type !help for a list of possible commands';
-// export const INVALID_PAYLOAD = 'Invalid payload for command type';
-// export const NOT_PLAYER_TURN = 'It is not the turn of requesting player';
--- server/app/services/message/message.service.ts
+++
@@ -1,7 +0,0 @@
-import { ActiveGameService } from '@app/services/active-game-service/active-game.service';
-import { Service } from 'typedi';
-
-@Service()
-export class MessageService {
-    constructor(private readonly activeGameService: ActiveGameService) {}
-}
