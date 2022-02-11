9dac241b7efa29b6fc52b03b5e347c1eee7fc6ab
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-11 17:39:27 -0500

    add tile reserve and tile reserve total

--- server/app/classes/communication/game-update-data.ts
+++ server/app/classes/communication/game-update-data.ts
@@ -1,5 +1,6 @@
 import { Round } from '@app/classes/round/round';
 import { Square } from '@app/classes/square';
+import { LetterValue } from '@app/classes/tile';
 import { PlayerData } from './player-data';
 
 export interface GameUpdateData {
@@ -8,4 +9,6 @@ export interface GameUpdateData {
     isGameOver?: boolean;
     board?: (Square | undefined)[];
     round?: Round;
+    tileReserve?: { letter: LetterValue; amount: number }[];
+    tileReserveTotal?: number;
 }
