e3d0d5781b8ed80876bed5967233ce4389a0fe3e
Thomas <thomtrepanier@gmail.com>
2022-02-11 17:33:03 -0500

    Added reset service data tests

--- client/src/app/services/round-manager/round-manager.service.spec.ts
+++ client/src/app/services/round-manager/round-manager.service.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Round } from '@app/classes/round';
+import { DEFAULT_PLAYER } from '@app/constants/game';
 import { GameDispatcherController } from '@app/controllers/game-dispatcher-controller/game-dispatcher.controller';
 import RoundManagerService from '@app/services/round-manager/round-manager.service';
 import SpyObj = jasmine.SpyObj;
@@ -8,6 +10,13 @@ import SpyObj = jasmine.SpyObj;
 describe('RoundManagerService', () => {
     let service: RoundManagerService;
     let gameDispatcherControllerSpy: SpyObj<GameDispatcherController>;
+
+    const currentRound: Round = {
+        player: DEFAULT_PLAYER,
+        startTime: new Date(Date.now() - 1000),
+        limitTime: new Date(Date.now()),
+        completedTime: null,
+    };
 
     beforeEach(() => {
         gameDispatcherControllerSpy = jasmine.createSpyObj('GameDispatcherController', ['']);
@@ -23,5 +32,40 @@ describe('RoundManagerService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('ResetServiceData', () => {
+        let timeSourceSpy: unknown;
+
+        beforeEach(() => {
+            timeSourceSpy = spyOn(service['timerSource'], 'complete').and.callFake(() => {
+                return;
+            });
+            service.resetServiceData();
+        });
+
+        it('resetServiceData should reset the gameId', () => {
+            expect(service.gameId).toEqual('');
+        });
+
+        it('resetServiceData should reset the localPlayerId', () => {
+            expect(service.localPlayerId).toEqual('');
+        });
+
+        it('resetServiceData should reset the completed rounds', () => {
+            expect(service.completedRounds).toEqual([]);
+        });
+
+        it('resetServiceData should reset the max round time', () => {
+            expect(service.maxRoundTime).toEqual(0);
+        });
+
+        it('resetServiceData should clear the timeout', () => {
+            // SPY ON clearTimeout
+        });
+
+        it('resetServiceData should reset the gameId', () => {
+            expect(timeSourceSpy).toHaveBeenCalled();
+        });
     });
 });
