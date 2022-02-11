395143800f06af93112aa4d893dc8b10dcf4891b
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-10 21:56:39 -0500

    fix build errors

--- server/app/services/score-calculator-service/score-calculator.const.test.ts
+++ server/app/services/score-calculator-service/score-calculator.const.test.ts
@@ -1,3 +1,4 @@
+import { Position } from '@app/classes/board';
 import { Square } from '@app/classes/square';
 import { MultiplierEffect } from '@app/classes/square/score-multiplier';
 import { Tile } from '@app/classes/tile';
@@ -13,7 +14,7 @@ export const GENERIC_LETTER_1: [Tile, Square] = [
     { letter: 'X', value: 10 },
     {
         tile: null,
-        position: { column: 0, row: 0 },
+        position: new Position(0, 0),
         scoreMultiplier: { multiplier: DEFAULT_WORD_MULTIPLIER, multiplierEffect: MultiplierEffect.WORD },
         wasMultiplierUsed: false,
         isCenter: false,
@@ -24,7 +25,7 @@ export const GENERIC_LETTER_2: [Tile, Square] = [
     { letter: 'N', value: 1 },
     {
         tile: null,
-        position: { column: 0, row: 0 },
+        position: new Position(0, 0),
         scoreMultiplier: { multiplier: DEFAULT_LETTER_MULTIPLIER, multiplierEffect: MultiplierEffect.LETTER },
         wasMultiplierUsed: false,
         isCenter: false,
@@ -35,7 +36,7 @@ export const GENERIC_LETTER_3: [Tile, Square] = [
     { letter: 'C', value: 3 },
     {
         tile: null,
-        position: { column: 0, row: 0 },
+        position: new Position(0, 0),
         scoreMultiplier: null,
         wasMultiplierUsed: false,
         isCenter: false,
