a18d1779ab1060dd49999addebc064bc16dc1d03
matbro-inextenso <matbro@inextenso.net>
2022-02-13 00:33:27 -0500

    fixed errors after merge

--- server/app/services/words-verification-service/words-verification.service.spec.ts
+++ server/app/services/words-verification-service/words-verification.service.spec.ts
@@ -2,8 +2,8 @@
 /* eslint-disable dot-notation */
 // Lint no unused expression must be disabled to use chai syntax
 /* eslint-disable @typescript-eslint/no-unused-expressions, no-unused-expressions */
-import { INVALID_WORD, WORD_CONTAINS_APOSTROPHE, WORD_CONTAINS_ASTERISK, WORD_CONTAINS_HYPHEN, WORD_TOO_SHORT } from '@app/constants/errors';
 import { DICTIONARY_NAME, DICTIONARY_RELATIVE_PATH } from '@app/constants/services-constants/words-verification.service.const';
+import { INVALID_WORD, WORD_CONTAINS_APOSTROPHE, WORD_CONTAINS_ASTERISK, WORD_CONTAINS_HYPHEN, WORD_TOO_SHORT } from '@app/constants/services-errors';
 import * as chai from 'chai';
 import { expect, spy } from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
