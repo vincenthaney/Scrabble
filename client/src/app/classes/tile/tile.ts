ef3c355a77c6c1f7ce633856fba411dec1cdb4d2
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-13 17:11:37 -0500

    add isBlank to tile class client

--- client/src/app/classes/tile/tile.ts
+++ client/src/app/classes/tile/tile.ts
@@ -3,9 +3,11 @@ import { LetterValue } from './tile.types';
 export default class Tile {
     letter: LetterValue;
     value: number;
+    isBlank: boolean;
 
-    constructor(letter: LetterValue, value: number) {
+    constructor(letter: LetterValue, value: number, isBlank?: boolean) {
         this.letter = letter;
         this.value = value;
+        this.isBlank = isBlank ?? false;
     }
 }
