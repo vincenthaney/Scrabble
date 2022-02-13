e56af4ee53490dea84db0ce35a46bafa418c9045
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-12 23:32:45 -0500

    fix word validator to lowercase word

--- server/app/services/words-verification-service/words-verification.service.ts
+++ server/app/services/words-verification-service/words-verification.service.ts
@@ -25,6 +25,7 @@ export class WordsVerificationService {
     // Will be removed during sprint 3
     fetchDictionary(dictionaryName: string, filePath: string): string[] {
         const dataBuffer = fs.readFileSync(join(filePath, dictionaryName));
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
         const data: DictionaryData = JSON.parse(dataBuffer.toString());
         return data.words;
     }
@@ -39,9 +40,10 @@ export class WordsVerificationService {
     }
 
     verifyWords(words: string[], dictionary: string) {
-        for (const word of words) {
+        for (let word of words) {
             if (word.length > 0) {
-                this.removeAccents(word);
+                word = this.removeAccents(word);
+                word = word.toLowerCase();
                 if (word.length < MINIMUM_WORD_LENGTH) throw new Error(word + WORD_TOO_SHORT);
                 if (word.includes('*')) throw new Error(word + WORD_CONTAINS_ASTERISK);
                 if (word.includes('-')) throw new Error(word + WORD_CONTAINS_HYPHEN);
