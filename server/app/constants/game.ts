839db5c83149ff63093e44ca1b7608ea2dce26e0
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-13 20:21:48 -0500

    create GameException

--- server/app/constants/game.ts
+++ server/app/constants/game.ts
@@ -36,3 +36,5 @@ export const UNDEFINED_BOARD_SIZE: Vec2 = { x: -1, y: -1 };
 export const VALID_MULTIPLIERS: number[] = [2, 3];
 
 export const SYSTEM_ID = 'system';
+
+export type GameExceptionType = 'Commande invalide' | 'Mot invalide';
