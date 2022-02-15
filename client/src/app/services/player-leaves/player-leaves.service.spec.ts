1b7323a10298c2d13b0d77a5d27d49fdc5006e97
vincenthaney <71298395+vincenthaney@users.noreply.github.com>
2022-02-14 22:30:30 -0500

    playerLeaves tests

--- client/src/app/services/player-leaves/player-leaves.service.spec.ts
+++ client/src/app/services/player-leaves/player-leaves.service.spec.ts
@@ -1,11 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PlayerLeavesController } from '@app/controllers/player-leaves-controller/player-leaves.controller';
+import { SocketService } from '..';
 import { PlayerLeavesService } from './player-leaves.service';
 
 describe('PlayerLeavesService', () => {
     let service: PlayerLeavesService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientModule, RouterTestingModule],
+            providers: [PlayerLeavesController, SocketService],
+        });
         service = TestBed.inject(PlayerLeavesService);
     });
 
