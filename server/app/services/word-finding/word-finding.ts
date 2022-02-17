57cd7fc0453b2d249bb531ee0d03d3cdab4ed768
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-02-17 16:44:26 -0500

    RS: comment unused code

--- server/app/services/word-finding/word-finding.ts
+++ server/app/services/word-finding/word-finding.ts
@@ -1,18 +1,18 @@
 import { Board } from '@app/classes/board';
 import { Tile } from '@app/classes/tile';
-import { WordExtraction } from '@app/classes/word-extraction/word-extraction';
 import { WordFindingQuery, WordPlacement } from '@app/classes/word-finding';
 import { Service } from 'typedi';
-import { ScoreCalculatorService } from '@app/services/score-calculator-service/score-calculator.service';
-import { WordsVerificationService } from '@app/services/words-verification-service/words-verification.service';
+// import { WordExtraction } from '@app/classes/word-extraction/word-extraction';
+// import { ScoreCalculatorService } from '@app/services/score-calculator-service/score-calculator.service';
+// import { WordsVerificationService } from '@app/services/words-verification-service/words-verification.service';
 
 @Service()
 export default class WordFindingService {
-    constructor(
-        private wordExtraction: WordExtraction,
-        private wordVerification: WordsVerificationService,
-        private scoreCalculator: ScoreCalculatorService,
-    ) {}
+    // constructor(
+    //     private wordExtraction: WordExtraction,
+    //     private wordVerification: WordsVerificationService,
+    //     private scoreCalculator: ScoreCalculatorService,
+    // ) {}
 
     // eslint-disable-next-line no-unused-vars
     findWords(board: Board, tiles: Tile[], query: WordFindingQuery): WordPlacement[] {
