3823c03254fe7db5726f5bd547f085d8949edd36
thomtrep <thomtrepanier@gmail.com>
2022-02-18 14:20:41 -0500

    Added buttons

--- client/src/app/components/timer-selection/timer-selection.component.ts
+++ client/src/app/components/timer-selection/timer-selection.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { MAXIMUM_TIMER_VALUE, MINIMUM_TIMER_VALUE } from '@app/constants/pages-constants';
+import { MAXIMUM_TIMER_VALUE, MINIMUM_TIMER_VALUE, TIMER_VALUE_INCREMENTS } from '@app/constants/pages-constants';
 
 @Component({
     selector: 'app-timer-selection',
@@ -11,7 +11,18 @@ export class TimerSelectionComponent {
     @Input() parentForm: FormGroup;
     timerValue: number;
 
+    incrementTimerValue(): void {
+        this.changeTimerValue(TIMER_VALUE_INCREMENTS);
+    }
+
+    decrementTimerValue(): void {
+        this.changeTimerValue(-TIMER_VALUE_INCREMENTS);
+    }
+
     changeTimerValue(delta: number): void {
         this.timerValue = Math.min(MAXIMUM_TIMER_VALUE, Math.max(MINIMUM_TIMER_VALUE, this.timerValue + delta));
+        this.parentForm.setValue({
+            timer: this.timerValue,
+        });
     }
 }
