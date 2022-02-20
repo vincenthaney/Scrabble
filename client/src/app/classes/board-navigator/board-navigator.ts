97eaa2f5f86e94aa0b7a0139caa77b610c2d36d0
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-02-20 13:08:53 -0500

    create board navigator

---
+++ client/src/app/classes/board-navigator/board-navigator.ts
@@ -0,0 +1,64 @@
+import { SquareView } from '@app/classes/square';
+import { Orientation } from '@app/classes/orientation';
+import { Position } from '@app/classes/position';
+import Direction from './direction';
+
+export class BoardNavigator {
+    private squareGrid: SquareView[][];
+    private position: Position;
+    private orientation: Orientation;
+
+    constructor(squareGrid: SquareView[][], position: Position, orientation: Orientation) {
+        this.squareGrid = squareGrid;
+        this.position = { ...position };
+        this.orientation = orientation;
+    }
+
+    get squareView() {
+        return this.squareGrid[this.position.row][this.position.column];
+    }
+
+    move(direction: Direction, distance: number = 1): BoardNavigator {
+        if (this.orientation === Orientation.Horizontal) {
+            this.position.column += direction * distance;
+        } else {
+            this.position.row += direction * distance;
+        }
+        return this;
+    }
+
+    forward(distance: number = 1): BoardNavigator {
+        return this.move(Direction.Forward, distance);
+    }
+
+    backward(distance: number = 1): BoardNavigator {
+        return this.move(Direction.Backward, distance);
+    }
+
+    nextEmpty(): SquareView | undefined {
+        const navigator = this.clone();
+
+        do {
+            navigator.forward();
+        } while (navigator.isWithinBounds() && !navigator.isEmpty());
+
+        return navigator.isWithinBounds() ? navigator.squareView : undefined;
+    }
+
+    isWithinBounds(): boolean {
+        return this.position.row < this.squareGrid.length && this.position.column < this.squareGrid[this.position.row].length;
+    }
+
+    switchOrientation(): BoardNavigator {
+        this.orientation = this.orientation === Orientation.Horizontal ? Orientation.Vertical : Orientation.Horizontal;
+        return this;
+    }
+
+    isEmpty(): boolean {
+        return this.squareView.square.tile === null;
+    }
+
+    clone(): BoardNavigator {
+        return new BoardNavigator(this.squareGrid, this.position, this.orientation);
+    }
+}
