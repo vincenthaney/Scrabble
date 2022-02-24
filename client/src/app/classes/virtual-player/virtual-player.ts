7441597158d1293640ebe137b5c7bb60ad03db16
Vincent Haney <vincent.7aney@gmail.com>
2022-02-24 10:56:08 -0500

    squelette virtual player

---
+++ client/src/app/classes/virtual-player/virtual-player.ts
@@ -0,0 +1,19 @@
+import { AbstractPlayer } from '@app/classes/player';
+
+export default class VirtualPlayer extends AbstractPlayer {
+
+    constructor(){
+        // subscribe
+    }
+
+    decideAction();
+
+    sendPayload();
+
+    generateExchangeAction();
+
+    generatePassAction();
+
+    generatePlaceAction();
+
+}
