24f73c9f5ee4fad0cf192cbb5df4342115c9ccb9
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-02 00:39:22 -0500

    RS: Add Hint constant

--- server/app/constants/services-constants/word-finding.const.ts
+++ server/app/constants/services-constants/word-finding.const.ts
@@ -1,4 +1,5 @@
 export const INITIAL_TILE = 1;
-// export const QUICK_MOVE_TIME = 3000;
+export const QUICK_MOVE_TIME = 3000;
 export const LONG_MOVE_TIME = 20000;
 export const FINAL_COMPUTE_TIME = 50;
+export const HINT_AMOUNT_OF_WORDS = 3;
