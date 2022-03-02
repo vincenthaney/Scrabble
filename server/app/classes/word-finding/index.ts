b53eb77080a9e538e855c8d62a24008690a65252
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-02 00:35:25 -0500

    RS: Add to index

--- server/app/classes/word-finding/index.ts
+++ server/app/classes/word-finding/index.ts
@@ -1,3 +1,6 @@
 export { default as WordFindingRequest } from './word-finding-request';
 export { default as PointRange } from './point-range';
-export { default as WordPlacement } from './word-placement';
+export { default as SquareProperties } from './square-properties';
+export { default as MovePossibilities } from './move-possibilities';
+export { default as WordFindingUsage } from './word-finding-usage';
+export { default as SearchState } from './search-state';
