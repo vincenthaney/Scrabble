6b9bf9e0200d4d1f266a6f77af85f14989ee82e3
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-02 00:36:35 -0500

    RS: Remove default export

--- server/app/classes/word-finding/word-placement.ts
+++ server/app/classes/word-finding/word-placement.ts
@@ -1,7 +1,7 @@
 import { Orientation, Position } from '@app/classes/board';
 import { Tile } from '@app/classes/tile';
 
-export default interface WordPlacement {
+export interface WordPlacement {
     tilesToPlace: Tile[];
     orientation: Orientation;
     startPosition: Position;
