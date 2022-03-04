0b442065f13bf0514c0343558e62766013ad2795
vincenthaney <71298395+vincenthaney@users.noreply.github.com>
2022-03-04 00:20:17 -0500

    comment out expert

--- server/app/classes/virtual-player/expert-virtual-player/expert-virtual-player.ts
+++ server/app/classes/virtual-player/expert-virtual-player/expert-virtual-player.ts
@@ -1,15 +1,15 @@
-import { AbstractVirtualPlayer } from '@app/classes/virtual-player/abstract-virtual-player';
-import { WordFindingRequest, PointRange } from '@app/classes/word-finding';
+// import { AbstractVirtualPlayer } from '@app/classes/virtual-player/abstract-virtual-player';
+// import { WordFindingRequest, PointRange } from '@app/classes/word-finding';
 
-export class ExpertVirtualPlayer extends AbstractVirtualPlayer {
-    generateWordRequest(): WordFindingRequest {
-        throw new Error('Method not implemented.');
-    }
-    findPointRange(): PointRange {
-        throw new Error('Method not implemented.');
-    }
+// export class ExpertVirtualPlayer extends AbstractVirtualPlayer {
+//     generateWordRequest(): WordFindingRequest {
+//         throw new Error('Method not implemented.');
+//     }
+//     findPointRange(): PointRange {
+//         throw new Error('Method not implemented.');
+//     }
 
-    findAction(): void {
-        throw new Error('Method not implemented.');
-    }
-}
+//     findAction(): void {
+//         throw new Error('Method not implemented.');
+//     }
+// }
