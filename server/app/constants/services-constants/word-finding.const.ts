5d5f862a88b18b5f6945ee4c4e71542549913836
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-10 01:18:56 -0500

    Revert "RS: Add const instead of magic"
    This reverts commit 2c966e5e1c0d4e1f4663b2dcd0f912a942ac9461.

--- server/app/constants/services-constants/word-finding.const.ts
+++ server/app/constants/services-constants/word-finding.const.ts
@@ -3,5 +3,3 @@ export const QUICK_MOVE_TIME = 3000;
 export const LONG_MOVE_TIME = 20000;
 export const FINAL_COMPUTE_TIME = 50;
 export const HINT_AMOUNT_OF_WORDS = 3;
-export const BLANK_TILE_VALUE = 0;
-export const BLANK_TILE_REPLACEMENT = 'E';
