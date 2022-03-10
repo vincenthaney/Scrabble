34d0aa4dca0a5ce367ee1c320fd604bc9339e506
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-10 17:54:45 -0500

    RS: Use all accents

--- server/app/utils/validate-name.ts
+++ server/app/utils/validate-name.ts
@@ -1,3 +1,3 @@
-const VALIDATION_RULE = "^([a-zA-Z0-9éèêàâëï]+[ '\\-_]{0,1})*$";
+const VALIDATION_RULE = "^([a-zA-Z0-9ŠšŽžÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìiíiîiïiñòóôõöùúûýÿ]+[ '\\-_]{0,1})*$";
 
 export const validateName = (name: string) => RegExp(VALIDATION_RULE).test(name);
