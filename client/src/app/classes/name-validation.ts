8d9bd721a73c58af57462b4a85b7ec7f6ce68d51
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-10 18:06:27 -0500

    RS: Use accents client side aswell

--- client/src/app/classes/name-validation.ts
+++ client/src/app/classes/name-validation.ts
@@ -1,6 +1,6 @@
 const MIN_LENGTH = 2;
 const MAX_LENGTH = 20;
-const VALIDATION_RULE = "^([a-zA-Z0-9éèêàâëï]+[ '\\-_]{0,1})*$";
+const VALIDATION_RULE = "^([a-zA-Z0-9ŠšŽžÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìiíiîiïiñòóôõöùúûýÿ]+[ '\\-_]{0,1})*$";
 
 export const NAME_VALIDATION = {
     minLength: MIN_LENGTH,
