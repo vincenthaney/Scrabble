bb24f586ed4a5f2621d0dd3cc56d52252665a8d7
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-12 02:03:09 -0500

    RS: Comment HighScore draft

--- server/app/services/high-scores-service/high-scores-service.spec.ts
+++ server/app/services/high-scores-service/high-scores-service.spec.ts
@@ -1,24 +1,24 @@
-/* eslint-disable @typescript-eslint/no-magic-numbers */
-/* eslint-disable dot-notation */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-import * as chai from 'chai';
-import * as chaiAsPromised from 'chai-as-promised';
-import * as spies from 'chai-spies';
-import HighScoresService from './high-scores-service';
-import MongoDbService from './high-scores-service';
+// /* eslint-disable @typescript-eslint/no-magic-numbers */
+// /* eslint-disable dot-notation */
+// /* eslint-disable no-unused-expressions */
+// /* eslint-disable @typescript-eslint/no-unused-expressions */
+// import * as chai from 'chai';
+// import * as chaiAsPromised from 'chai-as-promised';
+// import * as spies from 'chai-spies';
+// import HighScoresService from './high-scores-service';
+// import MongoDbService from './high-scores-service';
 
-const expect = chai.expect;
-chai.use(spies);
-chai.use(chaiAsPromised);
+// const expect = chai.expect;
+// chai.use(spies);
+// chai.use(chaiAsPromised);
 
-describe('HighScoresService', () => {
-    let service: HighScoresService;
-    beforeEach(() => {
-        service = new HighScoresService();
-    });
+// describe('HighScoresService', () => {
+//     let service: HighScoresService;
+//     beforeEach(() => {
+//         service = new HighScoresService();
+//     });
 
-    it('should be created', () => {
-        expect(service).to.exist;
-    });
-});
+//     it('should be created', () => {
+//         expect(service).to.exist;
+//     });
+// });
--- server/app/services/high-scores-service/high-scores-service.ts
+++ server/app/services/high-scores-service/high-scores-service.ts
@@ -1,42 +1,42 @@
-import { GameType } from '@app/classes/game/game.type';
-import { promises } from 'fs';
-import 'mock-fs'; // required when running test. Otherwise compiler cannot resolve fs, path and __dirname
-import { join } from 'path';
-import { DbHighScore, DbHighScores } from '@app/classes/mongo-db/db-high-scores';
-import { Collection, Document } from 'mongodb';
-import { Service } from 'typedi';
-import MongoDbService from '@app/services/mongo-db-service/mongo-db-service';
-import { DEFAULT_HIGH_SCORES_RELATIVE_PATH } from '@app/constants/services-constants/mongo-db.const';
+// import { GameType } from '@app/classes/game/game.type';
+// import { promises } from 'fs';
+// import 'mock-fs'; // required when running test. Otherwise compiler cannot resolve fs, path and __dirname
+// import { join } from 'path';
+// import { DbHighScore, DbHighScores } from '@app/classes/mongo-db/db-high-scores';
+// import { Collection } from 'mongodb';
+// import { Service } from 'typedi';
+// import MongoDbService from '@app/services/database-service/database-service';
+// import { DEFAULT_HIGH_SCORES_RELATIVE_PATH } from '@app/constants/services-constants/mongo-db.const';
 
-const HIGH_SCORES_MONGO_COLLECTION = 'HighScores';
+// const HIGH_SCORES_MONGO_COLLECTION = 'HighScores';
 
-@Service()
-export default class HighScoresService {
-    constructor(private mongoDbService: MongoDbService) {}
+// @Service()
+// export default class HighScoresService {
+//     constructor(private mongoDbService: MongoDbService) {}
 
-    get collection(): Collection<DbHighScore> {
-        return this.mongoDbService.database.collection(HIGH_SCORES_MONGO_COLLECTION);
-    }
+//     get collection(): Collection<DbHighScore> {
+//         return this.mongoDbService.database.collection(HIGH_SCORES_MONGO_COLLECTION);
+//     }
 
-    static async fetchDefaultHighScores(): Promise<DbHighScore[]> {
-        const filePath = join(__dirname, DEFAULT_HIGH_SCORES_RELATIVE_PATH);
-        const dataBuffer = await promises.readFile(filePath, 'utf-8');
-        const defaultHighScores = JSON.parse(dataBuffer);
-        return defaultHighScores.highScores;
-    }
+//     static async fetchDefaultHighScores(): Promise<DbHighScore[]> {
+//         const filePath = join(__dirname, DEFAULT_HIGH_SCORES_RELATIVE_PATH);
+//         const dataBuffer = await promises.readFile(filePath, 'utf-8');
+//         const defaultHighScores: DbHighScores = JSON.parse(dataBuffer);
+//         return defaultHighScores.highScores;
+//     }
 
-    addHighScore(score: number, name: string, gameType: GameType): void {}
+//     addHighScore(score: number, name: string, gameType: GameType): void {}
 
-    getHighScores(gameType: GameType): DbHighScore[] {
-        return [];
-    }
+//     getHighScores(gameType: GameType): DbHighScores {
+//         return {};
+//     }
 
-    async resetHighScores(): Promise<void> {
-        await this.collection.deleteMany({});
-        this.populateDb();
-    }
+//     async resetHighScores(): Promise<void> {
+//         await this.collection.deleteMany({});
+//         this.populateDb();
+//     }
 
-    async populateDb() {
-        await this.mongoDbService.populateDb(HIGH_SCORES_MONGO_COLLECTION, await HighScoresService.fetchDefaultHighScores());
-    }
-}
+//     async populateDb() {
+//         await this.mongoDbService.populateDb(HIGH_SCORES_MONGO_COLLECTION, await HighScoresService.fetchDefaultHighScores());
+//     }
+// }
