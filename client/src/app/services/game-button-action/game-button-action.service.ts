434f730f95254d1cd7d692ab85b32bc9bfa04a5f
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-12 15:37:36 -0500

    add action exchange to game-button-action-service

--- client/src/app/services/game-button-action/game-button-action.service.ts
+++ client/src/app/services/game-button-action/game-button-action.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActionData, ActionType } from '@app/classes/actions/action-data';
+import { Tile } from '@app/classes/tile';
+import { CommandExceptionMessages } from '@app/constants/command-exception-messages';
+import { SYSTEM_ERROR_ID } from '@app/constants/game';
 import { NO_LOCAL_PLAYER } from '@app/constants/services-errors';
 import { GamePlayController } from '@app/controllers/game-play-controller/game-play.controller';
 import GameService from '@app/services/game/game.service';
@@ -19,5 +22,30 @@ export class GameButtonActionService {
         const localPlayerId = this.gameService.getLocalPlayerId();
         if (!localPlayerId) throw new Error(NO_LOCAL_PLAYER);
         this.gamePlayController.sendAction(this.gameService.gameId, localPlayerId, actionPass);
+    }
+
+    createExchangeAction(tiles: Tile[]): void {
+        const player = this.gameService.getLocalPlayer();
+        const gameId = this.gameService.gameId;
+
+        if (!player) throw new Error(NO_LOCAL_PLAYER);
+        if (tiles.some((tile) => !player?.getTiles().includes(tile))) {
+            return this.sendError(CommandExceptionMessages.DontHaveTiles, gameId, player.id);
+        }
+
+        const actionExchange: ActionData = {
+            type: ActionType.EXCHANGE,
+            input: '',
+            payload: { tiles },
+        };
+
+        this.gamePlayController.sendAction(gameId, player.id, actionExchange);
+    }
+
+    sendError(message: string, gameId: string, playerId: string): void {
+        this.gamePlayController.sendError(gameId, playerId, {
+            content: message,
+            senderId: SYSTEM_ERROR_ID,
+        });
     }
 }
