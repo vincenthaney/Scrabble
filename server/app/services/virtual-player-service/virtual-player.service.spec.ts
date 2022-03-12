626f7bfe2f2134aa72a0b8a923ee7d15463fb7ba
Vincent Haney <vincent.7aney@gmail.com>
2022-03-12 16:15:18 -0500

    first try on tests vp service

--- server/app/services/virtual-player-service/virtual-player.service.spec.ts
+++ server/app/services/virtual-player-service/virtual-player.service.spec.ts
@@ -1,9 +1,19 @@
-import { expect } from 'chai';
+import { ActionData, ActionType } from '@app/classes/communication/action-data';
+// import { expect } from 'chai';
+import { VirtualPlayerService } from './virtual-player.service';
+// import fetch from 'node-fetch';
 
-describe('sendAction should', () => {
+describe.only('sendAction should', () => {
+    let virtualPlayerService: VirtualPlayerService;
+    beforeEach(() => {
+        virtualPlayerService = new VirtualPlayerService();
+    });
+
     it('should call fetch', () => {
-        // const fetchSpy = spy.on(fetch, 'fetch', () => {}).
-        // beginnerVirtualPlayer.sendAction()
-        expect(true).to.equal(false);
+        const TEST_GAME_ID = 'coocookachoo';
+        const TEST_PLAYER_ID = 'I am the walrus';
+        const TEST_ACTION: ActionData = { type: ActionType.PLACE, input: '', payload: {} };
+        virtualPlayerService.sendAction(TEST_GAME_ID, TEST_PLAYER_ID, TEST_ACTION);
+        // expect(stub).to.have.been.called();
     });
 });
