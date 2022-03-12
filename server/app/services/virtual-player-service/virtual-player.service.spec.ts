5c228c8ae33b84b4f8df9ac93055bce415384122
Vincent Haney <vincent.7aney@gmail.com>
2022-03-12 14:06:12 -0500

    modif virutal player service test

--- server/app/services/virtual-player-service/virtual-player.service.spec.ts
+++ server/app/services/virtual-player-service/virtual-player.service.spec.ts
@@ -1,6 +1,9 @@
+import { expect } from 'chai';
+
 describe('sendAction should', () => {
     it('should call fetch', () => {
         // const fetchSpy = spy.on(fetch, 'fetch', () => {}).
         // beginnerVirtualPlayer.sendAction()
+        expect(true).to.equal(false);
     });
 });
