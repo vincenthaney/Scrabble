ea1bacca2e140b401339be4bfff09437942bb702
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-12 00:37:24 -0500

    RS: Constates pour db

---
+++ server/app/constants/services-constants/mongo-db.const.ts
@@ -0,0 +1,6 @@
+export const HIGH_SCORES_MONGO_COLLECTION = 'HighScores';
+export const MONGO_USERNAME = 'Equipe212Scrabble';
+export const MONGO_PASSWORD = 'uva5IyneIE6Tu1tB';
+export const MONGO_URL = `mongodb+srv:/${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.pzdht.mongodb.net/LOG2990-equipe212?retryWrites=true&w=majority`;
+export const MONGO_DATABASE = 'LOG2990-equipe212';
+export const DEFAULT_HIGH_SCORES_RELATIVE_PATH = '../../../assets/letter-distribution.json';
