fabe2334c6ff9e8556128dae82099398fef40d6e
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-12 02:07:22 -0500

    RS: Fix typo

--- server/app/constants/services-constants/mongo-db.const.ts
+++ server/app/constants/services-constants/mongo-db.const.ts
@@ -3,4 +3,5 @@ export const MONGO_USERNAME = 'Equipe212Scrabble';
 export const MONGO_PASSWORD = 'uva5IyneIE6Tu1tB';
 export const MONGO_URL = `mongodb+srv:/${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.pzdht.mongodb.net/LOG2990-equipe212?retryWrites=true&w=majority`;
 export const MONGO_DATABASE = 'LOG2990-equipe212';
-export const DEFAULT_HIGH_SCORES_RELATIVE_PATH = '../../../assets/letter-distribution.json';
+export const DEFAULT_HIGH_SCORES_RELATIVE_PATH = '../../../assets/default-high-scores.json';
+export const DEFAULT_VIRTUAL_PLAYERS_RELATIVE_PATH = '../../../assets/default-virtual-players.json';
