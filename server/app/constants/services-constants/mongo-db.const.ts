52bfc08929d89390d74d8304b8457645c80148ab
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-12 14:00:14 -0500

    RS: Rename const for more precision

--- server/app/constants/services-constants/mongo-db.const.ts
+++ server/app/constants/services-constants/mongo-db.const.ts
@@ -1,7 +1,8 @@
-export const HIGH_SCORES_MONGO_COLLECTION = 'HighScores';
-export const MONGO_USERNAME = 'Equipe212Scrabble';
-export const MONGO_PASSWORD = 'uva5IyneIE6Tu1tB';
-export const MONGO_URL = `mongodb+srv:/${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.pzdht.mongodb.net/LOG2990-equipe212?retryWrites=true&w=majority`;
-export const MONGO_DATABASE = 'LOG2990-equipe212';
+export const HIGH_SCORES_MONGO_COLLECTION_NAME = 'HighScores';
+export const MONGO_DB_USERNAME = 'Equipe212Scrabble';
+export const MONGO_DB_PASSWORD = 'uva5IyneIE6Tu1tB';
+export const MONGO_DB_URL = `mongodb+srv:/${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}
+@cluster0.pzdht.mongodb.net/LOG2990-equipe212?retryWrites=true&w=majority`;
+export const MONGO_DATABASE_NAME = 'LOG2990-equipe212';
 export const DEFAULT_HIGH_SCORES_RELATIVE_PATH = '../../../assets/default-high-scores.json';
 export const DEFAULT_VIRTUAL_PLAYERS_RELATIVE_PATH = '../../../assets/default-virtual-players.json';
