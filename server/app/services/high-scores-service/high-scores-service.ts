028c3817f51eda5a32af6f7135d648344478fe7c
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-12 22:42:51 -0500

    RS: Add HighScore service

--- server/app/services/high-scores-service/high-scores-service.ts
+++ server/app/services/high-scores-service/high-scores-service.ts
@@ -1,42 +1,65 @@
-// import { GameType } from '@app/classes/game/game.type';
-// import { promises } from 'fs';
-// import 'mock-fs'; // required when running test. Otherwise compiler cannot resolve fs, path and __dirname
-// import { join } from 'path';
-// import { DbHighScore, DbHighScores } from '@app/classes/mongo-db/db-high-scores';
-// import { Collection } from 'mongodb';
-// import { Service } from 'typedi';
-// import MongoDbService from '@app/services/database-service/database-service';
-// import { DEFAULT_HIGH_SCORES_RELATIVE_PATH } from '@app/constants/services-constants/mongo-db.const';
+import 'mock-fs'; // required when running test. Otherwise compiler cannot resolve fs, path and __dirname
+import { promises } from 'fs';
+import { join } from 'path';
+import { Collection } from 'mongodb';
+import { Service } from 'typedi';
+import DatabaseService from '@app/services/database-service/database-service';
+import { DEFAULT_HIGH_SCORES_RELATIVE_PATH, HIGH_SCORES_MONGO_COLLECTION_NAME } from '@app/constants/services-constants/mongo-db.const';
+import { HighScoresData, HighScore } from '@app/classes/database/high-score';
+import { GameType } from '@app/classes/game/game-type';
 
-// const HIGH_SCORES_MONGO_COLLECTION = 'HighScores';
+@Service()
+export default class HighScoresService {
+    constructor(private databaseService: DatabaseService) {}
 
-// @Service()
-// export default class HighScoresService {
-//     constructor(private mongoDbService: MongoDbService) {}
+    private static async fetchDefaultHighScores(): Promise<HighScore[]> {
+        const filePath = join(__dirname, DEFAULT_HIGH_SCORES_RELATIVE_PATH);
+        const dataBuffer = await promises.readFile(filePath, 'utf-8');
+        const defaultHighScores: HighScoresData = JSON.parse(dataBuffer);
+        return defaultHighScores.highScores;
+    }
+    async getHighScores(gameType: GameType): Promise<HighScore[]> {
+        return this.collection.find({ gameType }).toArray();
+    }
 
-//     get collection(): Collection<DbHighScore> {
-//         return this.mongoDbService.database.collection(HIGH_SCORES_MONGO_COLLECTION);
-//     }
+    async addHighScore(name: string, score: number, gameType: GameType): Promise<boolean> {
+        const sortedHighScores = await this.getSortedHighScores(gameType);
+        if (sortedHighScores[0].score > score) return false;
 
-//     static async fetchDefaultHighScores(): Promise<DbHighScore[]> {
-//         const filePath = join(__dirname, DEFAULT_HIGH_SCORES_RELATIVE_PATH);
-//         const dataBuffer = await promises.readFile(filePath, 'utf-8');
-//         const defaultHighScores: DbHighScores = JSON.parse(dataBuffer);
-//         return defaultHighScores.highScores;
-//     }
+        const presentHighScore = sortedHighScores.find((highScore) => highScore.score === score);
+        if (presentHighScore) return this.updateHighScore(name, presentHighScore);
 
-//     addHighScore(score: number, name: string, gameType: GameType): void {}
+        return this.replaceHighScore(name, score, sortedHighScores[0]);
+    }
 
-//     getHighScores(gameType: GameType): DbHighScores {
-//         return {};
-//     }
+    async updateHighScore(name: string, highScore: HighScore): Promise<boolean> {
+        if (highScore.names.find((currentName) => currentName === name)) return false;
+        await this.collection.updateOne({ score: highScore.score, gameType: highScore.gameType }, { $push: { names: name } });
+        return true;
+    }
 
-//     async resetHighScores(): Promise<void> {
-//         await this.collection.deleteMany({});
-//         this.populateDb();
-//     }
+    async replaceHighScore(name: string, score: number, highScore: HighScore): Promise<boolean> {
+        await this.collection.replaceOne(
+            { score: highScore.score, gameType: highScore.gameType },
+            { gameType: highScore.gameType, score, names: [name] },
+        );
+        return true;
+    }
 
-//     async populateDb() {
-//         await this.mongoDbService.populateDb(HIGH_SCORES_MONGO_COLLECTION, await HighScoresService.fetchDefaultHighScores());
-//     }
-// }
+    async resetHighScores(): Promise<void> {
+        await this.collection.deleteMany({});
+        this.populateDb();
+    }
+
+    private get collection(): Collection<HighScore> {
+        return this.databaseService.database.collection(HIGH_SCORES_MONGO_COLLECTION_NAME);
+    }
+
+    private async getSortedHighScores(gameType: GameType): Promise<HighScore[]> {
+        return (await this.getHighScores(gameType)).sort((previous, current) => previous.score - current.score);
+    }
+
+    private async populateDb(): Promise<void> {
+        await this.databaseService.populateDb(HIGH_SCORES_MONGO_COLLECTION_NAME, await HighScoresService.fetchDefaultHighScores());
+    }
+}
