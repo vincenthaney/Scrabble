fcbabe33f927a3f03fe1f2624dc33b69775dba0c
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-13 01:00:17 -0400

    add arrows const

--- client/src/app/constants/components-constants.ts
+++ client/src/app/constants/components-constants.ts
@@ -4,4 +4,6 @@ export const LOCAL_PLAYER_ICON: IconName[] = ['user-astronaut', 'user-cowboy', '
 
 export const BACKSPACE = 'Backspace';
 export const ESCAPE = 'Escape';
+export const ARROW_LEFT = 'ArrowLeft';
+export const ARROW_RIGHT = 'ArrowRight';
 export const KEYDOWN = 'keydown';
