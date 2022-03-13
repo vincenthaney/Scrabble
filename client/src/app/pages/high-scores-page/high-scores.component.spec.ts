0dd933ffef8202751b8af8aa7a306760996f90e2
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-13 17:56:48 -0400

    HighScoresPage tests

--- client/src/app/pages/high-scores-page/high-scores.component.spec.ts
+++ client/src/app/pages/high-scores-page/high-scores.component.spec.ts
@@ -1,25 +1,104 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable dot-notation */
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Injectable } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SingleHighScore } from '@app/classes/admin/high-score';
+import { GameType } from '@app/classes/game-type';
+import { HighScoreBoxComponent } from '@app/components/high-score-box/high-score-box.component';
+import { IconComponent } from '@app/components/icon/icon.component';
+import { PageHeaderComponent } from '@app/components/page-header/page-header.component';
+import HighScoresService from '@app/services/high-scores/high-scores.service';
+import { HighScoresComponent } from './high-scores.component';
 
-// import { HighScoresComponent } from './high-scores.component';
+const DEFAULT_MAP: Map<GameType, SingleHighScore[]> = new Map([
+    [GameType.Classic, []],
+    [GameType.LOG2990, []],
+]);
 
-// describe('HighScoresComponent', () => {
-//   let component: HighScoresComponent;
-//   let fixture: ComponentFixture<HighScoresComponent>;
+@Injectable()
+export class HighScoresServiceSpy extends HighScoresService {
+    // eslint-disable-next-line no-unused-vars
+    getHighScores(gameType: GameType): SingleHighScore[] {
+        return [];
+    }
+    handleHighScoresRequest(): void {
+        return;
+    }
+}
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ HighScoresComponent ]
-//     })
-//     .compileComponents();
-//   });
+describe('HighScoresComponent', () => {
+    let component: HighScoresComponent;
+    let highScoresServiceMock: HighScoresService;
+    let fixture: ComponentFixture<HighScoresComponent>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HighScoresComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    beforeEach(async () => {
+        highScoresServiceMock = jasmine.createSpyObj(['getHighScores']);
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+        await TestBed.configureTestingModule({
+            declarations: [HighScoresComponent, HighScoreBoxComponent, PageHeaderComponent, IconComponent],
+            imports: [
+                MatInputModule,
+                MatFormFieldModule,
+                MatDividerModule,
+                HttpClientTestingModule,
+                MatFormFieldModule,
+                MatSelectModule,
+                MatCardModule,
+                BrowserAnimationsModule,
+                FormsModule,
+                ReactiveFormsModule,
+                MatButtonToggleModule,
+                RouterModule,
+                RouterTestingModule,
+            ],
+            providers: [{ provide: HighScoresService, useClass: HighScoresServiceSpy }],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        highScoresServiceMock = TestBed.inject(HighScoresService);
+        highScoresServiceMock['highScoresMap'] = DEFAULT_MAP;
+        fixture = TestBed.createComponent(HighScoresComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('ngOnInit should call handleHighScoresRequest ', () => {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const spyHandleHighScoresRequest = spyOn(highScoresServiceMock, 'handleHighScoresRequest').and.callFake(() => {});
+        component.ngOnInit();
+        expect(spyHandleHighScoresRequest).toHaveBeenCalled();
+    });
+
+    describe('ngOndestroy', () => {
+        it('should always call next and complete on ngUnsubscribe', () => {
+            const ngUnsubscribeNextSpy = spyOn<any>(component.componentDestroyed$, 'next');
+            const ngUnsubscribeCompleteSpy = spyOn<any>(component.componentDestroyed$, 'complete');
+
+            component.ngOnDestroy();
+            expect(ngUnsubscribeNextSpy).toHaveBeenCalled();
+            expect(ngUnsubscribeCompleteSpy).toHaveBeenCalled();
+        });
+    });
+
+    it('getHighScores should call getHighScores ', () => {
+        const spyGetHighScores = spyOn(highScoresServiceMock, 'getHighScores');
+        component.getHighScores();
+        expect(spyGetHighScores).toHaveBeenCalled();
+    });
+});
