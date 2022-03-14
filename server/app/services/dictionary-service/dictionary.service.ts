51526389a7462866955ffa1c156f59bd258d867a
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-13 20:21:53 -0400

    create dictionary service

---
+++ server/app/services/dictionary-service/dictionary.service.ts
@@ -0,0 +1,32 @@
+import { Dictionary, DictionaryData } from '@app/classes/dictionary';
+import { DICTIONARIES, INVALID_DICTIONARY_NAME } from '@app/constants/dictionary.const';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { Service } from 'typedi';
+
+@Service()
+export default class DictionaryService {
+    private dictionaries: Map<string, Dictionary> = new Map();
+
+    constructor() {
+        this.addAllDictionaries();
+    }
+
+    getDictionary(name: string): Dictionary {
+        const dictionary = this.dictionaries.get(name);
+        if (dictionary) return dictionary;
+        throw new Error(INVALID_DICTIONARY_NAME);
+    }
+
+    private addAllDictionaries(): void {
+        for (const [path, name] of DICTIONARIES) {
+            this.dictionaries.set(name, this.fetchDictionaryWords(path));
+        }
+    }
+
+    private fetchDictionaryWords(path: string): Dictionary {
+        const buffer = readFileSync(join(__dirname, path));
+        const data: DictionaryData = JSON.parse(buffer.toString());
+        return new Dictionary(data.words);
+    }
+}
