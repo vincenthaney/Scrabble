7fae0831cdfb42b09fda1b54276855c9dff06ba6
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-15 12:10:17 -0400

    Rename to parseAndSend

--- client/src/app/services/input-parser/input-parser.service.ts
+++ client/src/app/services/input-parser/input-parser.service.ts
@@ -25,12 +25,12 @@ export default class InputParserService {
         private gameViewEventManagerService: GameViewEventManagerService,
     ) {}
 
-    parseInput(input: string): void {
+    parseAndSendInput(input: string): void {
         const playerId = this.getLocalPlayer().id;
         const gameId = this.gameService.getGameId();
 
         if (this.isAction(input)) {
-            this.parseCommand(input, gameId, playerId);
+            this.parseAndSendCommand(input, gameId, playerId);
         } else {
             this.controller.sendMessage(gameId, playerId, {
                 content: input,
@@ -39,7 +39,7 @@ export default class InputParserService {
         }
     }
 
-    private parseCommand(input: string, gameId: string, playerId: string): void {
+    private parseAndSendCommand(input: string, gameId: string, playerId: string): void {
         try {
             this.controller.sendAction(gameId, playerId, this.createActionData(input));
         } catch (exception) {
