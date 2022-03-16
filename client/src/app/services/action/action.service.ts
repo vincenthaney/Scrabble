80488ae8a479e72b15d142a4c80c56f51cf90ff4
Thomas <thomtrepanier@gmail.com>
2022-03-16 15:32:11 -0400

    Removed unecessary checks

--- client/src/app/services/action/action.service.ts
+++ client/src/app/services/action/action.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActionData, ActionExchangePayload, ActionPlacePayload, ActionType } from '@app/classes/actions/action-data';
-import CommandException from '@app/classes/command-exception';
 import { Orientation } from '@app/classes/orientation';
 import { Position } from '@app/classes/position';
 import { Tile } from '@app/classes/tile';
-import { CommandExceptionMessages } from '@app/constants/command-exception-messages';
-import { SYSTEM_ERROR_ID } from '@app/constants/game';
 import { GamePlayController } from '@app/controllers/game-play-controller/game-play.controller';
 
 @Injectable({
@@ -46,15 +43,9 @@ export class ActionService {
     sendAction(gameId: string, playerId: string | undefined, actionData: ActionData): void {
         if (!playerId) return;
 
-        try {
-            const callbacks = this.preSendActionCallbacksMap.get(actionData.type);
-            if (callbacks) {
-                callbacks.forEach((callback) => callback(actionData, gameId, playerId));
-            }
-        } catch (exception) {
-            if (exception instanceof CommandException) {
-                this.handleCommandException(gameId, playerId, actionData.input, exception);
-            }
+        const callbacks = this.preSendActionCallbacksMap.get(actionData.type);
+        if (callbacks) {
+            callbacks.forEach((callback) => callback(actionData, gameId, playerId));
         }
 
         this.gamePlayController.sendAction(gameId, playerId, actionData);
@@ -63,21 +54,6 @@ export class ActionService {
     private convertBlankTilesLetter(actionPlacePayload: ActionPlacePayload): void {
         actionPlacePayload.tiles.forEach((tile) => {
             if (tile.isBlank && tile.playedLetter) tile.letter = tile.playedLetter;
-        });
-    }
-
-    private handleCommandException(gameId: string, playerId: string, input: string, exception: CommandException): void {
-        this.sendError(this.generateErrorMessageContent(exception.message, input), gameId, playerId);
-    }
-
-    private generateErrorMessageContent(message: string, input: string): string {
-        return message === CommandExceptionMessages.NotYourTurn ? message : `La commande **${input}** est invalide :<br />${message}`;
-    }
-
-    private sendError(message: string, gameId: string, playerId: string): void {
-        this.gamePlayController.sendError(gameId, playerId, {
-            content: message,
-            senderId: SYSTEM_ERROR_ID,
         });
     }
 
