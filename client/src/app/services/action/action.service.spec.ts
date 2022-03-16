841d49952a30d2e0b74c1296b641b81a8354bd39
Thomas <thomtrepanier@gmail.com>
2022-03-16 15:33:07 -0400

    Added tests to ActionService

--- client/src/app/services/action/action.service.spec.ts
+++ client/src/app/services/action/action.service.spec.ts
@@ -3,11 +3,25 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActionData, ActionExchangePayload, ActionPlacePayload, ActionType } from '@app/classes/actions/action-data';
+import { Orientation } from '@app/classes/orientation';
+import { Tile } from '@app/classes/tile';
 import { DEFAULT_PLAYER } from '@app/constants/game';
 import { ActionService } from './action.service';
 
 const DEFAULT_PLAYER_ID = DEFAULT_PLAYER.id;
 const DEFAULT_GAME_ID = 'some id';
+
+const DEFAULT_TILES: Tile[] = [new Tile('A', 1), new Tile('A', 2, true)];
+const PLACE_PAYLOAD: ActionPlacePayload = {
+    tiles: DEFAULT_TILES,
+    startPosition: { row: 0, column: 0 },
+    orientation: Orientation.Horizontal,
+};
+
+const EXCHANGE_PAYLOAD: ActionExchangePayload = {
+    tiles: DEFAULT_TILES,
+};
 
 describe('ActionService', () => {
     let service: ActionService;
@@ -22,5 +36,158 @@ describe('ActionService', () => {
     it('should be created', () => {
         expect(service).toBeTruthy();
         expect(service['preSendActionCallbacksMap']).toBeTruthy();
+    });
+
+    it('createPlaceActionPayload should return ActionPlacePayload with the correct attributes', () => {
+        const expectedPayload: ActionPlacePayload = PLACE_PAYLOAD;
+        const actualPayload = service.createPlaceActionPayload(PLACE_PAYLOAD.tiles, PLACE_PAYLOAD.startPosition, PLACE_PAYLOAD.orientation);
+        expect(actualPayload).toEqual(expectedPayload);
+        expect(typeof actualPayload).toBe(typeof expectedPayload);
+    });
+
+    it('createExchangeActionPayload should return ActionExchangePayload with the correct attributes', () => {
+        const expectedPayload: ActionExchangePayload = EXCHANGE_PAYLOAD;
+        const actualPayload = service.createExchangeActionPayload(DEFAULT_TILES);
+        expect(actualPayload).toEqual(expectedPayload);
+        expect(typeof actualPayload).toBe(typeof expectedPayload);
+    });
+
+    describe('createActionData', () => {
+        let inputSpy: jasmine.Spy;
+        let actualData: ActionData;
+
+        beforeEach(() => {
+            inputSpy = spyOn<any>(service, 'createInputFromPayload').and.returnValue('spyInput');
+        });
+
+        it('if input is defined, should not call createInputFromPayload', () => {
+            actualData = service.createActionData(ActionType.PASS, {}, 'input');
+            expect(inputSpy).not.toHaveBeenCalled();
+            expect(actualData.input).toEqual('input');
+        });
+
+        it('if input is NOT defined, should call createInputFromPayload', () => {
+            actualData = service.createActionData(ActionType.PASS, {}, '');
+            expect(inputSpy).toHaveBeenCalledWith(ActionType.PASS, {});
+            expect(actualData.input).toEqual('spyInput');
+        });
+
+        it('if payload is defined, action data should have provided payload', () => {
+            actualData = service.createActionData(ActionType.EXCHANGE, EXCHANGE_PAYLOAD, 'input');
+            expect(actualData.payload).toEqual(EXCHANGE_PAYLOAD);
+        });
+
+        it('if input is NOT defined, should call createInputFromPayload', () => {
+            actualData = service.createActionData(ActionType.PASS, undefined, '');
+            expect(actualData.payload).toEqual({});
+        });
+
+        it('should return ActionData with correct values', () => {
+            actualData = service.createActionData(ActionType.EXCHANGE, EXCHANGE_PAYLOAD, 'input');
+            const expectedData: ActionData<ActionExchangePayload> = {
+                type: ActionType.EXCHANGE,
+                payload: EXCHANGE_PAYLOAD,
+                input: 'input',
+            };
+            expect(actualData).toEqual(expectedData);
+        });
+    });
+
+    describe('sendAction', () => {
+        let sendActionSpy: jasmine.Spy;
+        let convertBlankTilesLetterSpy: jasmine.Spy;
+        let actionData: ActionData<ActionExchangePayload>;
+
+        const mockObject = {
+            fakeCallBack: (data: ActionData): ActionData => data,
+        };
+
+        beforeEach(() => {
+            sendActionSpy = spyOn(service['gamePlayController'], 'sendAction').and.callFake(() => {
+                return;
+            });
+            convertBlankTilesLetterSpy = spyOn<any>(service, 'convertBlankTilesLetter').and.callFake(() => {
+                return;
+            });
+            actionData = {
+                type: ActionType.EXCHANGE,
+                payload: EXCHANGE_PAYLOAD,
+                input: 'input',
+            };
+        });
+
+        it('if playerId is undefined, should not sendAction', () => {
+            service.sendAction(DEFAULT_GAME_ID, undefined, actionData);
+            expect(sendActionSpy).not.toHaveBeenCalled();
+        });
+
+        it('should call the callbacks of the action if it has any', () => {
+            const fakeSpy = spyOn(mockObject, 'fakeCallBack');
+            service['preSendActionCallbacksMap'] = new Map();
+            service['preSendActionCallbacksMap'].set(ActionType.HELP, [(data: ActionData) => mockObject.fakeCallBack(data)]);
+
+            actionData.type = ActionType.HELP;
+            service.sendAction(DEFAULT_GAME_ID, DEFAULT_PLAYER_ID, actionData);
+
+            expect(fakeSpy).toHaveBeenCalledWith(actionData);
+        });
+
+        it('should convert blank tiles before PLACE action is sent', () => {
+            actionData.type = ActionType.PLACE;
+            actionData.payload = PLACE_PAYLOAD;
+            service.sendAction(DEFAULT_GAME_ID, DEFAULT_PLAYER_ID, actionData);
+
+            expect(convertBlankTilesLetterSpy).toHaveBeenCalledWith(actionData.payload);
+        });
+
+        it('should call gamePlayController.sendAction with provided data', () => {
+            service.sendAction(DEFAULT_GAME_ID, DEFAULT_PLAYER_ID, actionData);
+            expect(sendActionSpy).toHaveBeenCalledWith(DEFAULT_GAME_ID, DEFAULT_PLAYER_ID, actionData);
+        });
+    });
+
+    it('convertBlankTilesLetter should set letter to playedLetter if tile is blank', () => {
+        const originalTiles: Tile[] = [
+            {
+                letter: '*',
+                value: 0,
+                playedLetter: 'A',
+                isBlank: true,
+            },
+            {
+                letter: '*',
+                value: 0,
+                isBlank: true,
+            },
+            {
+                letter: '*',
+                value: 0,
+                playedLetter: 'A',
+            },
+        ];
+
+        const payload = { ...PLACE_PAYLOAD, tiles: originalTiles };
+        const expectedTiles: Tile[] = [
+            {
+                letter: 'A',
+                value: 0,
+                playedLetter: 'A',
+                isBlank: true,
+            },
+            {
+                letter: '*',
+                value: 0,
+                isBlank: true,
+            },
+            {
+                letter: '*',
+                value: 0,
+                playedLetter: 'A',
+            },
+        ];
+        const expectedResult = { ...PLACE_PAYLOAD, tiles: expectedTiles };
+
+        service['convertBlankTilesLetter'](payload);
+        expect(payload).toEqual(expectedResult);
     });
 });
