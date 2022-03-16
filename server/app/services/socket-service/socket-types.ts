034fd1f64c65dc799fe4cb11a5986aae241a3916
vincenthaney <71298395+vincenthaney@users.noreply.github.com>
2022-03-16 12:12:47 -0400

    change to new type

--- server/app/services/socket-service/socket-types.ts
+++ server/app/services/socket-service/socket-types.ts
@@ -2,7 +2,7 @@ import { GameUpdateData } from '@app/classes/communication/game-update-data';
 import { LobbyData } from '@app/classes/communication/lobby-data';
 import { Message } from '@app/classes/communication/message';
 import { PlayerName } from '@app/classes/communication/player-name';
-import { StartMultiplayerGameData } from '@app/classes/game/game-config';
+import { StartGameData } from '@app/classes/game/game-config';
 
 export type SocketEmitEvents =
     | 'gameUpdate'
@@ -19,7 +19,7 @@ export type SocketEmitEvents =
 
 export type GameUpdateEmitArgs = GameUpdateData;
 export type JoinRequestEmitArgs = PlayerName;
-export type StartGameEmitArgs = StartMultiplayerGameData;
+export type StartGameEmitArgs = StartGameData;
 export type RejectEmitArgs = PlayerName;
 export type CanceledGameEmitArgs = PlayerName;
 export type JoinerLeaveGameEmitArgs = PlayerName;
