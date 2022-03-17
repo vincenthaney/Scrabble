df027bce4b7521b526050df72bc03b8be1a0985c
Vincent Haney <vincent.7aney@gmail.com>
2022-03-17 17:41:38 -0400

    inject service

--- server/app/classes/virtual-player/abstract-virtual-player.ts
+++ server/app/classes/virtual-player/abstract-virtual-player.ts
@@ -2,6 +2,7 @@ import Player from '@app/classes/player/player';
 import { PointRange } from '@app/classes/word-finding';
 import { VIRTUAL_PLAYER_ID_PREFIX } from '@app/constants/virtual-player-constants';
 import { ActiveGameService } from '@app/services/active-game-service/active-game.service';
+import { VirtualPlayerService } from '@app/services/virtual-player-service/virtual-player.service';
 import WordFindingService from '@app/services/word-finding/word-finding';
 import { Container } from 'typedi';
 import { v4 as uuidv4 } from 'uuid';
@@ -12,12 +13,18 @@ export abstract class AbstractVirtualPlayer extends Player {
 
     private wordFindingService: WordFindingService;
     private activeGameService: ActiveGameService;
+    private virtualPlayerService: VirtualPlayerService;
     constructor(gameId: string, name: string) {
         super(VIRTUAL_PLAYER_ID_PREFIX + uuidv4(), name);
         this.pointHistory = new Map<number, number>();
         this.gameId = gameId;
         this.wordFindingService = Container.get(WordFindingService);
         this.activeGameService = Container.get(ActiveGameService);
+        this.virtualPlayerService = Container.get(VirtualPlayerService);
+    }
+
+    getVirtualPlayerService(): VirtualPlayerService {
+        return this.virtualPlayerService;
     }
 
     getWordFindingService(): WordFindingService {
