82dd3ab05d95f7dacedef75426ea310c51fc4aa8
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-18 16:20:23 -0400

    update word finding service to pass score calculator

--- server/app/services/word-finding/word-finding-v2.ts
+++ server/app/services/word-finding/word-finding-v2.ts
@@ -5,14 +5,15 @@ import { ScoredWordPlacement } from '@app/classes/word-finding/word-placement';
 import { Service } from 'typedi';
 import DictionaryService from '@app/services/dictionary-service/dictionary.service';
 import WordFinding from '@app/classes/word-finding-v2/word-finding';
+import { ScoreCalculatorService } from '@app/services/score-calculator-service/score-calculator.service';
 
 @Service()
 export default class WordFindingServiceV2 {
-    constructor(private readonly dictionaryService: DictionaryService) {}
+    constructor(private readonly dictionaryService: DictionaryService, private readonly scoreCalculatorService: ScoreCalculatorService) {}
 
     findWords(board: Board, tiles: Tile[], request: WordFindingRequest): ScoredWordPlacement[] {
         const dictionary = this.dictionaryService.getDefaultDictionary();
-        const wordFinding = new WordFinding(board, tiles, request, dictionary);
+        const wordFinding = new WordFinding(board, tiles, request, dictionary, this.scoreCalculatorService);
 
         return wordFinding.findWords();
     }
