b764ddeb9fc82acbd44154ace34b20237b706031
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-18 16:31:35 -0400

    update player to use word finding

--- server/app/classes/virtual-player/abstract-virtual-player.ts
+++ server/app/classes/virtual-player/abstract-virtual-player.ts
@@ -3,7 +3,7 @@ import { PointRange } from '@app/classes/word-finding';
 import { VIRTUAL_PLAYER_ID_PREFIX } from '@app/constants/virtual-player-constants';
 import { ActiveGameService } from '@app/services/active-game-service/active-game.service';
 import { VirtualPlayerService } from '@app/services/virtual-player-service/virtual-player.service';
-import WordFindingService from '@app/services/word-finding/word-finding';
+import WordFindingServiceV2 from '@app/services/word-finding/word-finding-v2';
 import { Container } from 'typedi';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -11,14 +11,14 @@ export abstract class AbstractVirtualPlayer extends Player {
     gameId: string;
     pointHistory: Map<number, number>;
 
-    private wordFindingService: WordFindingService;
+    private wordFindingService: WordFindingServiceV2;
     private activeGameService: ActiveGameService;
     private virtualPlayerService: VirtualPlayerService;
     constructor(gameId: string, name: string) {
         super(VIRTUAL_PLAYER_ID_PREFIX + uuidv4(), name);
         this.pointHistory = new Map<number, number>();
         this.gameId = gameId;
-        this.wordFindingService = Container.get(WordFindingService);
+        this.wordFindingService = Container.get(WordFindingServiceV2);
         this.activeGameService = Container.get(ActiveGameService);
         this.virtualPlayerService = Container.get(VirtualPlayerService);
     }
@@ -27,7 +27,7 @@ export abstract class AbstractVirtualPlayer extends Player {
         return this.virtualPlayerService;
     }
 
-    getWordFindingService(): WordFindingService {
+    getWordFindingService(): WordFindingServiceV2 {
         return this.wordFindingService;
     }
 
