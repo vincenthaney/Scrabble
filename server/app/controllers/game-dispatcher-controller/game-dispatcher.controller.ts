b0b243096da251504f8c595b3765b2ef278ba47c
vincenthaney <71298395+vincenthaney@users.noreply.github.com>
2022-03-18 17:33:20 -0400

    change lines to sliceVP method

--- server/app/controllers/game-dispatcher-controller/game-dispatcher.controller.ts
+++ server/app/controllers/game-dispatcher-controller/game-dispatcher.controller.ts
@@ -3,7 +3,6 @@ import { CreateGameRequest, GameRequest, LobbiesRequest } from '@app/classes/com
 import { GameConfigData } from '@app/classes/game/game-config';
 import { GameMode } from '@app/classes/game/game-mode';
 import { HttpException } from '@app/classes/http.exception';
-import Player from '@app/classes/player/player';
 import { SECONDS_TO_MILLISECONDS, TIME_TO_RECONNECT } from '@app/constants/controllers-constants';
 import {
     DICTIONARY_REQUIRED,
@@ -232,14 +231,13 @@ export class GameDispatcherController {
             gameId = this.gameDispatcherService.createSoloGame(config);
             const gameConfig = this.gameDispatcherService.getSoloGameFromId(gameId).getReadyConfig();
             const startGameData = await this.activeGameService.beginGame(gameId, gameConfig);
+
             this.socketService.addToRoom(config.playerId, gameId);
 
-            startGameData.player2 = startGameData.player2 as Player;
-            const testP1 = new Player(startGameData.player2.id, startGameData.player2.name);
-            testP1.tiles = startGameData.player2.tiles;
-            testP1.isConnected = startGameData.player2.isConnected;
-            startGameData.player2 = testP1;
+            startGameData.player2 = this.virtualPlayerService.sliceVirtualPlayerToPlayer(startGameData.player2);
+
             this.socketService.emitToSocket(config.playerId, 'startGame', startGameData);
+
             if (IS_ID_VIRTUAL_PLAYER(startGameData.round.playerData.id)) {
                 this.virtualPlayerService.triggerVirtualPlayerTurn(
                     startGameData,
--- server/app/services/virtual-player-service/virtual-player.service.ts
+++ server/app/services/virtual-player-service/virtual-player.service.ts
@@ -7,14 +7,15 @@ import { GameUpdateData } from '@app/classes/communication/game-update-data';
 import Game from '@app/classes/game/game';
 import { IS_REQUESTING } from '@app/constants/game';
 import { AbstractVirtualPlayer } from '@app/classes/virtual-player/abstract-virtual-player';
-import { GAME_SHOULD_CONTAIN_ROUND } from '@app/constants/virtual-player-constants';
+import { CONTENT_TYPE, GAME_SHOULD_CONTAIN_ROUND } from '@app/constants/virtual-player-constants';
+import Player from '@app/classes/player/player';
 
 @Service()
 export class VirtualPlayerService {
     sendAction(gameId: string, playerId: string, action: ActionData): void {
         fetch(`${environment.serverUrl}/games/${gameId}/players/${playerId}/action`, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { [CONTENT_TYPE]: 'application/json' },
             body: JSON.stringify({ type: action.type, payload: action.payload, input: action.input }),
         });
     }
@@ -23,5 +24,12 @@ export class VirtualPlayerService {
         if (!data.round) throw new Error(GAME_SHOULD_CONTAIN_ROUND);
         const virtualPlayer = game.getPlayer(data.round.playerData.id, IS_REQUESTING) as AbstractVirtualPlayer;
         virtualPlayer.playTurn();
+    }
+
+    sliceVirtualPlayerToPlayer(virtualPlayer: Player): Player {
+        const player = new Player(virtualPlayer.id, virtualPlayer.name);
+        player.tiles = virtualPlayer.tiles;
+        player.isConnected = virtualPlayer.isConnected;
+        return player;
     }
 }
