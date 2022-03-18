f61025905b749b2a718016437595d377cd9aee8b
Vincent Haney <vincent.7aney@gmail.com>
2022-03-18 14:05:44 -0400

    add headers

--- server/app/services/virtual-player-service/virtual-player.service.ts
+++ server/app/services/virtual-player-service/virtual-player.service.ts
@@ -6,8 +6,10 @@ import { environment } from '@app/environments/environment';
 @Service()
 export class VirtualPlayerService {
     sendAction(gameId: string, playerId: string, action: ActionData): void {
+        console.log('VIRTUAL PLAYER POSTING');
         fetch(`${environment.serverUrl}/games/${gameId}/players/${playerId}/action`, {
             method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ type: action.type, payload: action.payload, input: action.input }),
         });
     }
