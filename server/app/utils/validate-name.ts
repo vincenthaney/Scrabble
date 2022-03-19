b74ca639198476e30dbe4064c622801c56363bed
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-03-19 17:48:09 -0400

    RS: Use new cleaner regex

--- server/app/utils/validate-name.ts
+++ server/app/utils/validate-name.ts
@@ -1,3 +1,3 @@
-const VALIDATION_RULE = "^([a-zA-Z0-9ŠšŽžÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìiíiîiïiñòóôõöùúûýÿ]+[ '\\-_]{0,1})*$";
+const VALIDATION_RULE = "^([A-Za-zÀ-ÖØ-öø-ÿ]+[ '\\-_]{0,1})*$";
 
 export const validateName = (name: string) => RegExp(VALIDATION_RULE).test(name);
