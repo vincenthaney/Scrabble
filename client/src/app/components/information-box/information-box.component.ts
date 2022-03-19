5df08259ca1e49f9f3f5c0dabfba67c7a8906aa0
matbro-inextenso <matbro@inextenso.net>
2022-03-19 14:20:22 -0400

    put back things on one line

--- client/src/app/components/information-box/information-box.component.ts
+++ client/src/app/components/information-box/information-box.component.ts
@@ -64,9 +64,7 @@ export class InformationBoxComponent implements OnInit, OnDestroy, AfterViewInit
                 this.updateActivePlayerBorder(activePlayer);
             });
         }
-        this.roundManager.subscribeToEndRoundEvent(this.componentDestroyed$, () => {
-            this.endRound();
-        });
+        this.roundManager.subscribeToEndRoundEvent(this.componentDestroyed$, () => this.endRound());
         this.isPlayer1 = this.checkIfIsPlayer1();
         this.localPlayerIcon = this.getLocalPlayerIcon();
     }
