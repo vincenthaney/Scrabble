07d038a0835b57ec4b3bf49611ea3af110e89b4d
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-19 15:37:08 -0400

    implement word finding beginner decision

--- server/app/classes/word-finding-v2/word-finding-beginner.ts
+++ server/app/classes/word-finding-v2/word-finding-beginner.ts
@@ -1,11 +1,63 @@
 import { ScoredWordPlacement } from '@app/classes/word-finding/word-placement';
+import { NO_REQUEST_POINT_HISTORY, NO_REQUEST_POINT_RANGE } from '@app/constants/services-errors';
+import { ScoreCalculatorService } from '@app/services/score-calculator-service/score-calculator.service';
+import { Board } from '@app/classes/board';
+import { Dictionary } from '@app/classes/dictionary';
+import { Tile } from '@app/classes/tile';
+import WordFindingRequest from '@app/classes/word-finding/word-finding-request';
 import WordFinding from './word-finding';
+import { WORD_FINDING_BEGINNER_ACCEPTANCE_THRESHOLD } from '@app/constants/classes-constants';
 
 export default class WordFindingBeginner extends WordFinding {
+    private placementFoundAcceptance: number = Number.MIN_VALUE;
+    private acceptanceProbabilities: Map<number, number>;
+
+    constructor(board: Board, tiles: Tile[], request: WordFindingRequest, dictionary: Dictionary, scoreCalculatorService: ScoreCalculatorService) {
+        super(board, tiles, request, dictionary, scoreCalculatorService);
+        this.calculateAcceptanceProbabilities();
+    }
+
     handleWordPlacement(wordPlacement: ScoredWordPlacement): void {
-        if (this.request.pointRange && this.isWithinPointRange(wordPlacement.score, this.request.pointRange)) this.wordPlacements.push(wordPlacement);
+        if (this.isWithinPointRange(wordPlacement.score)) {
+            const acceptanceProbability = this.acceptanceProbabilities.get(wordPlacement.score) ?? 0;
+            if (acceptanceProbability > this.placementFoundAcceptance) {
+                this.wordPlacements = [wordPlacement];
+                this.placementFoundAcceptance = acceptanceProbability;
+            }
+        }
     }
     isSearchCompleted(): boolean {
-        return this.wordPlacements.length > 0;
+        return this.wordPlacements.length > 0 && this.placementFoundAcceptance >= WORD_FINDING_BEGINNER_ACCEPTANCE_THRESHOLD;
+    }
+
+    private calculateAcceptanceProbabilities(): void {
+        if (!this.request.pointRange) throw new Error(NO_REQUEST_POINT_RANGE);
+        if (!this.request.pointHistory) throw new Error(NO_REQUEST_POINT_HISTORY);
+
+        const minimumFrequency = this.findScoreMinimumFrequency();
+        this.acceptanceProbabilities = new Map<number, number>();
+
+        for (const score of this.request.pointRange) {
+            const scoreFrequency = this.request.pointHistory.get(score);
+            if (scoreFrequency) {
+                this.acceptanceProbabilities.set(score, 1 / (scoreFrequency - minimumFrequency + 1));
+            } else {
+                this.acceptanceProbabilities.set(score, 1);
+            }
+        }
+    }
+
+    private findScoreMinimumFrequency(): number {
+        if (!this.request.pointRange) throw new Error(NO_REQUEST_POINT_RANGE);
+        if (!this.request.pointHistory) throw new Error(NO_REQUEST_POINT_HISTORY);
+
+        let minimumFrequency = Number.MAX_VALUE;
+
+        for (const score of this.request.pointRange) {
+            const scoreFrequency = this.request.pointHistory.get(score) ?? 0;
+            if (scoreFrequency < minimumFrequency) minimumFrequency = scoreFrequency;
+        }
+
+        return minimumFrequency;
     }
 }
