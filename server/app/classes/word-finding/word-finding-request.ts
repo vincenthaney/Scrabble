232bc813a65e35e231c4a0cb852054924dc7aeea
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-19 15:36:34 -0400

    update word finding request to use Range

--- server/app/classes/word-finding/word-finding-request.ts
+++ server/app/classes/word-finding/word-finding-request.ts
@@ -1,6 +1,7 @@
-import { PointRange, WordFindingUseCase } from '.';
+import { WordFindingUseCase } from '.';
+import Range from '@app/classes/range/range';
 export default interface WordFindingRequest {
     useCase: WordFindingUseCase;
-    pointRange?: PointRange;
+    pointRange?: Range;
     pointHistory?: Map<number, number>;
 }
