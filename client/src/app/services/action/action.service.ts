8d02fe2530cfe5830109f5882d0121ed2e7c270c
Thomas <thomtrepanier@gmail.com>
2022-03-18 23:13:22 -0400

    Disabled action send when waiting for action confirmation

--- client/src/app/services/action/action.service.ts
+++ client/src/app/services/action/action.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@angular/core';
 import { ActionData, ActionPayload, ActionType, ExchangeActionPayload, PlaceActionPayload } from '@app/classes/actions/action-data';
+import { Message } from '@app/classes/communication/message';
 import { Orientation } from '@app/classes/orientation';
 import { Position } from '@app/classes/position';
 import { Tile } from '@app/classes/tile';
+import { SYSTEM_ERROR_ID } from '@app/constants/game';
 import { GamePlayController } from '@app/controllers/game-play-controller/game-play.controller';
 import { ActionPayloadToString } from '@app/utils/action-payload-to-string';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ActionService {
-    constructor(private gamePlayController: GamePlayController) {}
+    hasActionBeenPlayed: boolean;
+    serviceDestroyed$: Subject<boolean>;
+
+    constructor(private gamePlayController: GamePlayController) {
+        this.hasActionBeenPlayed = false;
+        this.serviceDestroyed$ = new Subject();
+        this.gamePlayController.gameUpdateValue.pipe(takeUntil(this.serviceDestroyed$)).subscribe(() => this.resetHasActionBeenSent());
+        this.gamePlayController.newMessageValue.pipe(takeUntil(this.serviceDestroyed$)).subscribe((newMessage: Message | null) => {
+            // Je ne suis pas sûr si on veut juste checker si c'est une erreur, on s'il faudrait checker le message parce que
+            // certaines erreurs ne doivent pas permettre de rejouer (aucune ne me vient en tête live, mais c'est possible qu'il y en ait)
+            if (newMessage && newMessage.senderId === SYSTEM_ERROR_ID) {
+                this.resetHasActionBeenSent();
+            }
+        });
+    }
 
     createPlaceActionPayload(tiles: Tile[], startPosition: Position, orientation: Orientation): PlaceActionPayload {
         return {
@@ -34,11 +52,12 @@ export class ActionService {
     }
 
     sendAction(gameId: string, playerId: string | undefined, actionData: ActionData): void {
-        if (!playerId) return;
+        if (!playerId || this.hasActionBeenPlayed) return;
 
         if (actionData.type === ActionType.PLACE) this.convertBlankTilesLetter((actionData.payload as PlaceActionPayload).tiles);
 
         this.gamePlayController.sendAction(gameId, playerId, actionData);
+        this.hasActionBeenPlayed = true;
     }
 
     private createInputFromPayload(actionType: ActionType, payload: ActionPayload): string {
@@ -56,5 +75,9 @@ export class ActionService {
         tiles.forEach((tile) => {
             if (tile.isBlank && tile.playedLetter) tile.letter = tile.playedLetter;
         });
+    }
+
+    private resetHasActionBeenSent(): void {
+        this.hasActionBeenPlayed = false;
     }
 }
