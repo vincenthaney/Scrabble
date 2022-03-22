144681ca4d67fae2daec53af4df4408794b9bcf6
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-21 22:20:29 -0400

    add comment

--- client/src/app/services/game-service/game.service.ts
+++ client/src/app/services/game-service/game.service.ts
@@ -174,6 +174,11 @@ export default class GameService implements OnDestroy, IResetServiceData {
     }
 
     wake(): void {
+        // Sometimes the game service needs to listen to an event without being used before.
+        // This causes problem because the class is not being initialized before being used,
+        // which means it never subscribes to the events. When calling the wake() method, we
+        // make sure that the service is initialized before doing an action that might create
+        // an event.
         return;
     }
 
