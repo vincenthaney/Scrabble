c30721efdd72f43929932b2b5b60c510e8be41d7
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-22 13:38:02 -0400

    Make hint  command a bit better

--- server/app/classes/word-finding/word-finding-hint/word-finding-hint.ts
+++ server/app/classes/word-finding/word-finding-hint/word-finding-hint.ts
@@ -1,11 +1,23 @@
-import { HINT_ACTION_NUMBER_OF_WORDS } from '@app/constants/classes-constants';
+import { HINT_ACTION_NUMBER_OF_WORDS, HINT_ACTION_TRIES_NUMBER } from '@app/constants/classes-constants';
 import { AbstractWordFinding, ScoredWordPlacement } from '@app/classes/word-finding';
 
 export default class WordFindingHint extends AbstractWordFinding {
+    private findingTries = 0;
+
     handleWordPlacement(wordPlacement: ScoredWordPlacement): void {
-        this.wordPlacements.push(wordPlacement);
+        this.findingTries++;
+        if (this.wordPlacements.length < HINT_ACTION_NUMBER_OF_WORDS) {
+            this.wordPlacements.push(wordPlacement);
+        } else {
+            for (let i = 0; i < this.wordPlacements.length; ++i) {
+                if (this.wordPlacements[i].score < wordPlacement.score) {
+                    this.wordPlacements.splice(i, 1, wordPlacement);
+                    return;
+                }
+            }
+        }
     }
     isSearchCompleted(): boolean {
-        return this.wordPlacements.length >= HINT_ACTION_NUMBER_OF_WORDS;
+        return this.wordPlacements.length >= HINT_ACTION_NUMBER_OF_WORDS && this.findingTries >= HINT_ACTION_TRIES_NUMBER;
     }
 }
