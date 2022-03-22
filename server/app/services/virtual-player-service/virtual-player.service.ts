cf48b657f1a6f4a9afee77c9eee5c16f6e42feeb
vincenthaney <71298395+vincenthaney@users.noreply.github.com>
2022-03-22 18:22:49 -0400

    refactor sendAction

--- server/app/services/virtual-player-service/virtual-player.service.ts
+++ server/app/services/virtual-player-service/virtual-player.service.ts
@@ -15,9 +15,10 @@ import { StatusCodes } from 'http-status-codes';
 @Service()
 export class VirtualPlayerService {
     async sendAction(gameId: string, playerId: string, action: ActionData): Promise<Response> {
-        let response = this.sendFetchRequest(gameId, playerId, action);
-        if ((await response).status !== StatusCodes.NO_CONTENT) {
-            response = this.sendFetchRequest(gameId, playerId, ActionPass.createActionData());
+        let response = await this.sendFetchRequest(gameId, playerId, action);
+        console.log(action);
+        if (response.status !== StatusCodes.NO_CONTENT) {
+            response = await this.sendFetchRequest(gameId, playerId, ActionPass.createActionData());
         }
         return response;
     }
