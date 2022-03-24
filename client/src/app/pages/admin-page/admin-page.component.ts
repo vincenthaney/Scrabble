087984afb7f11080f035b249b05ea8a47addff2b
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-24 16:46:18 -0400

    Add content to admin page

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { LobbyInfoComponent } from '@app/components/lobby-info/lobby-info.component';
+
+interface Tab {
+    link: string;
+    component: unknown;
+}
 
 @Component({
     selector: 'app-admin-page',
     templateUrl: './admin-page.component.html',
     styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent {}
+export class AdminPageComponent {
+    tabs: Tab[];
+    currentTab: Tab;
+
+    constructor() {
+        this.tabs = [
+            { link: 'Link 1', component: LobbyInfoComponent },
+            { link: 'Link 2', component: LobbyInfoComponent },
+            { link: 'Link 3', component: LobbyInfoComponent },
+        ];
+        this.currentTab = this.tabs[0];
+    }
+}
