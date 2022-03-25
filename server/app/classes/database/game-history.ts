327dda09135681911c933a8fcddaa4ef58d7d9e6
matbro-inextenso <matbro@inextenso.net>
2022-03-25 18:10:21 -0400

    Created GameHistory interface

---
+++ server/app/classes/database/game-history.ts
@@ -0,0 +1,20 @@
+import { GameMode } from '@app/classes/game/game-mode';
+import { GameType } from '@app/classes/game/game-type';
+
+export interface GameHistory {
+    startTime: Date;
+    endTime: Date;
+    player1Data: PlayerHistoryData;
+    player2Data: PlayerHistoryData;
+    replacingPlayer: PlayerHistoryData;
+    gameType: GameType;
+    gameMode: GameMode;
+    isOver: boolean;
+    hasBeenAbandonned: boolean;
+}
+
+export interface PlayerHistoryData {
+    name: string;
+    score: number;
+    isVirtualPlayer: boolean;
+}
