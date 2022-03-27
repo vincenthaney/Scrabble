ebd9b60a20f981d5bf6baaba0f3c5844d543e3e8
Thomas <thomtrepanier@gmail.com>
2022-03-27 13:48:09 -0400

    Squelette de la méthode pour valider les objectifs

--- server/app/services/objectives-service/objectives.service.ts
+++ server/app/services/objectives-service/objectives.service.ts
@@ -3,8 +3,10 @@ import { GameObjectives } from '@app/classes/objectives/game-objectives';
 import { ValidationParameters } from '@app/classes/objectives/validation-parameters';
 import { Service } from 'typedi';
 
+// Laisser les commentaires pour pouvoir se repérer lors de l'utilisation des objectifs
 @Service()
 export default class ObjectivesService {
+    // Actually créer les objectifs
     async createObjectivesForGame(): Promise<GameObjectives> {
         const publicObjectives: Set<AbstractObjective> = new Set();
         const player1Objective: AbstractObjective = undefined as unknown as AbstractObjective;
@@ -17,6 +19,9 @@ export default class ObjectivesService {
     // Voir comment envoyer du feedback lors de la complétion
 
     validateGameObjectives(gameObjectives: GameObjectives, validationParameters: ValidationParameters): void {
+        gameObjectives.getAllObjectives().forEach((objective: AbstractObjective) => {
+            objective.isValid(validationParameters);
+        });
         return;
     }
 }
