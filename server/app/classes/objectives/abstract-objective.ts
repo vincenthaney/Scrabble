737429b01dcf6c0e8f25e78f506b9244b5b63e54
Thomas <thomtrepanier@gmail.com>
2022-03-28 10:38:12 -0400

    Changed completion predicate

--- server/app/classes/objectives/abstract-objective.ts
+++ server/app/classes/objectives/abstract-objective.ts
@@ -4,7 +4,7 @@ export abstract class AbstractObjective {
     constructor(public name: string, public bonusPoints: number, public progress: number, private readonly maxProgress: number) {}
 
     isCompleted(): boolean {
-        return this.progress === this.maxProgress;
+        return this.progress >= this.maxProgress;
     }
 
     abstract isValid(validationParameters: ValidationParameters): boolean;
