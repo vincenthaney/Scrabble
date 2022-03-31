d25b13744bae8466009647c7b94afb6a96abb1ba
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-03-31 10:05:19 -0400

    add name to objective

--- client/src/app/classes/communication/objective-data.ts
+++ client/src/app/classes/communication/objective-data.ts
@@ -1,6 +1,7 @@
 import { ObjectiveState } from '@app/classes/objectives/objective-state';
 
 export interface ObjectiveData {
+    name: string;
     state: ObjectiveState;
     progress: number;
     maxProgress: number;
