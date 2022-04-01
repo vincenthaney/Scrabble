8e3bcdfdd434d6cbb84d777e7cbc4c9eaca18e13
Vincent Haney <vincent.7aney@gmail.com>
2022-04-01 12:36:01 -0400

    add angular modules

--- client/src/app/modules/material.module.ts
+++ client/src/app/modules/material.module.ts
@@ -15,7 +15,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 // import { MatMenuModule } from '@angular/material/menu';
-// import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
@@ -26,7 +26,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 // import { MatSortModule } from '@angular/material/sort';
 import { MatStepperModule } from '@angular/material/stepper';
-// import { MatTableModule } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -48,7 +48,7 @@ const modules = [
     MatInputModule,
     MatListModule,
     // MatMenuModule,
-    // MatPaginatorModule,
+    MatPaginatorModule,
     MatProgressBarModule,
     MatProgressSpinnerModule,
     MatRadioModule,
@@ -59,7 +59,7 @@ const modules = [
     MatSnackBarModule,
     // MatSortModule,
     MatStepperModule,
-    // MatTableModule,
+    MatTableModule,
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
