13491a9eaec108a88cfc254ef135d8c4a8b46a3a
vincenthaney <71298395+vincenthaney@users.noreply.github.com>
2022-04-01 12:41:14 -0400

    fix admin page

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -5,20 +5,4 @@ import { Component } from '@angular/core';
     templateUrl: './admin-page.component.html',
     styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent {
-    dictionaries = this.getDictionaries();
-
-    constructor(private dictionaryService: DictionaryService) {}
-
-    getDictionaries(): DictionarySummary[] {
-        return this.dictionaryService.getDictionaries();
-    }
-
-    getDictionariesNames(): string[] {
-        const dictionariesNames: string[];
-        for (const dictionary of this.dictionaries) {
-            dictionariesNames.push(dictionary.name);
-        }
-        return dictionariesNames;
-    }
-}
+export class AdminPageComponent {}
