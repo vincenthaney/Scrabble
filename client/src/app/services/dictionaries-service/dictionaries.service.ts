400d835a0a3b07313505b62faa5bb7bf71d2bea3
vincenthaney <71298395+vincenthaney@users.noreply.github.com>
2022-04-03 15:01:33 -0400

    dictionaries service

--- client/src/app/services/dictionaries-service/dictionaries.service.ts
+++ client/src/app/services/dictionaries-service/dictionaries.service.ts
@@ -1,35 +1,108 @@
 import { Injectable } from '@angular/core';
+import { DictionarySummary } from '@app/classes/communication/dictionary';
+import Dictionary from '@app/classes/dictionary/dictionary';
+import { DictionaryData } from '@app/classes/dictionary/dictionary-data';
+import {
+    DICTIONARIES_ADDED,
+    DICTIONARIES_NOT_ADDED,
+    DICTIONARY_ADDED,
+    DICTIONARY_DELETED,
+    DICTIONARY_DOWNLOADING,
+    DICTIONARY_NOT_DELETED,
+    DICTIONARY_NOT_DOWNLOADED,
+    DICTIONARY_NOT_UPDATED,
+    DICTIONARY_UPDATED,
+} from '@app/constants/dictionary-service-constants';
+import { DictionariesController } from '@app/controllers/dictionaries-controller/dictionaries-controller';
+import { Subject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class DictionariesService {
-    async updateDictionary(id: string, name: string, description: string): Promise<void> {
-        // envoyer requête au serveur pour modifier les informations du dictionaire spécifié
-        return;
-    }
-    async downloadDictionary(id: string): Promise<void> {
-        // const data2 = this.getDictionary(id);
-        const data = { test: 'test' };
-        // Initialize Blob
-        const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
-        const name = 'testDictionary.json';
-        // Make the download process
-        const a = window.document.createElement('a');
-        a.href = window.URL.createObjectURL(blob);
-        a.download = name;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        return;
-    }
-    async deleteDictionary(id: string): Promise<void> {
-        // envoyer requete au service du serveur pour supprimer le dictionaire spécifié
-        return;
+    dictionaryData: DictionaryData;
+    dictionaries: Map<string, Dictionary>;
+    updateRequestResponse: string;
+    deleteRequestResponse: string;
+    uploadRequestResponse: string;
+
+    constructor(private dictionariesController: DictionariesController, private serviceDestroyed$: Subject<boolean> = new Subject()) {
+        this.dictionariesController.subscribeToDictionaryUpdateEvent(this.serviceDestroyed$, (dictionarySummary) => {
+            this.updateRequestResponse = dictionarySummary;
+        });
+
+        this.dictionariesController.subscribeToDictionaryDownloadEvent(this.serviceDestroyed$, (dictionaryData) => {
+            this.dictionaryData = dictionaryData;
+        });
+
+        this.dictionariesController.subscribeToDictionaryDeleteEvent(this.serviceDestroyed$, (response) => {
+            this.deleteRequestResponse = response;
+        });
+        this.dictionariesController.subscribeToDictionaryUploadEvent(this.serviceDestroyed$, (response) => {
+            this.uploadRequestResponse = response;
+        });
+        this.dictionariesController.subscribeToGetAllDictionariesEvent(this.serviceDestroyed$, (dictionaries) => {
+            this.dictionaries = dictionaries;
+        });
     }
 
-    // private async getDictionary(id: string): Promise<void> {
-    //     // envoyer ewuqtes au server pour obtenir une copie du dictionaire spécifié
-    //     return;
-    // }
+    async updateDictionary(id: string, name: string, description: string): Promise<DictionarySummary | string> {
+        this.dictionariesController.handleUpdateDictionary({ title: name, description, id });
+        if (this.updateRequestResponse) {
+            // send signal to component to update it's data
+            return DICTIONARY_UPDATED;
+        }
+        return DICTIONARY_NOT_UPDATED;
+    }
+
+    async downloadDictionary(id: string): Promise<void | string> {
+        this.dictionariesController.handleDownloadDictionary(id);
+        if (this.dictionaryData) {
+            this.startDownload();
+            return DICTIONARY_DOWNLOADING;
+        }
+        return DICTIONARY_NOT_DOWNLOADED;
+    }
+
+    async deleteDictionary(id: string): Promise<void | string> {
+        this.dictionariesController.handleDeleteDictionary(id);
+        if (this.deleteRequestResponse) {
+            // send signal to component to update it's data
+            return DICTIONARY_DELETED;
+        }
+        return DICTIONARY_NOT_DELETED;
+    }
+
+    async uploadDictionary(): Promise<string> {
+        // envoyer requete au service du serveur pour supprimer le dictionaire spécifié
+        // const dictionaryData = methodToGetDictFromUser()
+        // this.dictionariesController.handleUploadDictionary(id);
+        // si bonne réponse, supprimer dict et actualiser
+        // changer ceci pour le real thang
+        const dictionaryData = {} as unknown as DictionaryData;
+        this.dictionariesController.handleUploadDictionary(dictionaryData);
+        if (this.deleteRequestResponse) {
+            // send signal to component to update it's data
+            return DICTIONARY_ADDED;
+        }
+        return DICTIONARY_NOT_DELETED;
+    }
+
+    async getDictionaries(): Promise<string> {
+        this.dictionariesController.handleGetAllDictionariesEvent();
+        if (this.dictionaries) {
+            return DICTIONARIES_ADDED;
+        }
+        return DICTIONARIES_NOT_ADDED;
+    }
+
+    private startDownload(): void {
+        const title = this.dictionaryData.title;
+        const downloadProcess = window.document.createElement('a');
+        downloadProcess.href = window.URL.createObjectURL(new Blob([JSON.stringify(this.dictionaryData)], { type: 'application/json' }));
+        downloadProcess.download = title;
+        document.body.appendChild(downloadProcess);
+        downloadProcess.click();
+        document.body.removeChild(downloadProcess);
+    }
 }
