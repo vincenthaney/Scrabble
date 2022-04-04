d882c8728c7c12c28df4405341eecb772f7fffdd
Vincent Haney <vincent.7aney@gmail.com>
2022-04-04 15:26:29 -0400

    upload dictionary json verif

--- client/src/app/components/upload-dictionary/upload-dictionary.component.ts
+++ client/src/app/components/upload-dictionary/upload-dictionary.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -9,16 +10,29 @@ import { environment } from 'src/environments/environment';
 })
 export class UploadDictionaryComponent {
     fileName = '';
-
-    constructor(private http: HttpClient) {}
+    isUploadableFile: boolean = false;
+    constructor(private http: HttpClient, private dialogRef: MatDialogRef<UploadDictionaryComponent>) {}
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    onFileSelected(event: any) {
+    onFileSelected(event: any): void {
         const file: File = event.target.files[0];
-        if (file) {
+        const fileJSON = JSON.stringify(file);
+        if (this.isJSONDictionaryFile(fileJSON)) {
+            this.isUploadableFile = true;
             const endpoint = `${environment.serverUrl}/dictionaries`;
             const upload$ = this.http.post(endpoint, JSON.stringify(file));
             upload$.subscribe();
+            return;
         }
+        this.isUploadableFile = false;
+    }
+    closeDialog(): void {
+        this.dialogRef.close();
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    isJSONDictionaryFile(fileJSON: any): boolean {
+        console.log(fileJSON);
+        return fileJSON.title && fileJSON.description && fileJSON.words;
     }
 }
