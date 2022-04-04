ecb5d1dbcb22dfe2bd8a1fbc596313ea09477feb
Vincent Haney <vincent.7aney@gmail.com>
2022-04-04 15:37:48 -0400

    new ocmpoenent for delete

---
+++ client/src/app/components/delete-dictionary-dialog/delete-dictionary-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-delete-dictionary-dialog',
+    templateUrl: 'delete-dictionary-dialog.component.html',
+    styleUrls: ['delete-dictionary-dialog.component.scss'],
+})
+export class DeleteDictionaryDialogComponent {}
