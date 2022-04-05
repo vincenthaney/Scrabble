e3ee1f7e55e3dbb810bd5e388ceffc2dabb2a8b9
vincenthaney <71298395+vincenthaney@users.noreply.github.com>
2022-04-05 14:12:17 -0400

    dictionary upload

--- client/src/app/components/upload-dictionary/upload-dictionary.component.ts
+++ client/src/app/components/upload-dictionary/upload-dictionary.component.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { environment } from 'src/environments/environment';
+import { DictionaryData } from '@app/classes/dictionary';
+import { FILE_NOT_DICTIONARY, WRONG_FILE_TYPE } from '@app/constants/dictionaries-components';
 
 @Component({
     selector: 'app-upload-dictionary',
@@ -9,22 +9,36 @@ import { environment } from 'src/environments/environment';
     styleUrls: ['upload-dictionary.component.scss'],
 })
 export class UploadDictionaryComponent {
-    fileName = '';
+    errorMessage: string = '';
     isUploadableFile: boolean = false;
-    constructor(private http: HttpClient, private dialogRef: MatDialogRef<UploadDictionaryComponent>) {}
+    selectedFile: File;
+    jsonFile: DictionaryData;
+    constructor(private dialogRef: MatDialogRef<UploadDictionaryComponent>) {}
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    onFileSelected(event: any): void {
-        const file: File = event.target.files[0];
-        const fileJSON = JSON.stringify(file);
-        if (this.isJSONDictionaryFile(fileJSON)) {
-            this.isUploadableFile = true;
-            const endpoint = `${environment.serverUrl}/dictionaries`;
-            const upload$ = this.http.post(endpoint, JSON.stringify(file));
-            upload$.subscribe();
-            return;
-        }
-        this.isUploadableFile = false;
+    onFileChanged(event: any) {
+        this.selectedFile = event.target.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsText(this.selectedFile, 'UTF-8');
+        fileReader.onload = () => {
+            try {
+                const readFile: DictionaryData = JSON.parse(fileReader.result as string);
+                if (this.isDictionaryDataInterface(readFile)) {
+                    this.jsonFile = readFile;
+                } else {
+                    this.errorMessage = FILE_NOT_DICTIONARY;
+                }
+            } catch (error) {
+                const newError = error as SyntaxError;
+                this.errorMessage = newError.message;
+            }
+        };
+        fileReader.onerror = () => {
+            this.errorMessage = WRONG_FILE_TYPE;
+        };
+    }
+    onUpload() {
+        // upload code goes here
     }
     closeDialog(): void {
         this.dialogRef.close();
@@ -34,5 +48,11 @@ export class UploadDictionaryComponent {
     isJSONDictionaryFile(fileJSON: any): boolean {
         console.log(fileJSON);
         return fileJSON.title && fileJSON.description && fileJSON.words;
+    }
+
+    isDictionaryDataInterface(data: DictionaryData): boolean {
+        return (
+            data instanceof Object && 'title' in data && data instanceof Object && 'description' in data && data instanceof Object && 'words' in data
+        );
     }
 }
--- client/src/app/constants/dictionaries-components.ts
+++ client/src/app/constants/dictionaries-components.ts
@@ -1,1 +1,3 @@
 export const DELETE_COMPONENT_TITLE = 'Supprimer un dictionnaire';
+export const FILE_NOT_DICTIONARY = "Le fichier ne respecte pas le format {'title', 'description','words'}.";
+export const WRONG_FILE_TYPE = "Le fichier n'est pas un fichier JSON.";
