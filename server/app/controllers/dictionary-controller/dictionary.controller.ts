585a62d69467326acefbea5a9b73b87c231c4f91
Vincent Haney <vincent.7aney@gmail.com>
2022-04-05 15:50:13 -0400

    fix routes

--- server/app/controllers/dictionary-controller/dictionary.controller.ts
+++ server/app/controllers/dictionary-controller/dictionary.controller.ts
@@ -19,7 +19,7 @@ export class DictionaryController {
         this.router = Router();
 
         this.router.post('/dictionaries', async (req: DictionaryRequest, res: Response) => {
-            const dictionaryData: DictionaryData = req.body;
+            const dictionaryData: DictionaryData = req.body.dictionaryData;
             try {
                 await this.dictionaryService.addNewDictionary(dictionaryData);
                 res.status(StatusCodes.CREATED).send();
@@ -29,7 +29,7 @@ export class DictionaryController {
         });
 
         this.router.patch('/dictionaries', async (req: DictionaryRequest, res: Response) => {
-            const dictionaryUpdateInfo: DictionaryUpdateInfo = req.body;
+            const dictionaryUpdateInfo: DictionaryUpdateInfo = req.body.dictionaryUpdateInfo;
 
             try {
                 await this.dictionaryService.updateDictionary(dictionaryUpdateInfo);
