706118d9d02e8e5ea4c8238c0573401928313d72
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-06 16:20:58 -0400

    test controller

--- client/src/app/controllers/game-history-controller/game-history.controller.spec.ts
+++ client/src/app/controllers/game-history-controller/game-history.controller.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
@@ -15,5 +16,25 @@ describe('GameHistoryControllerService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('getGameHistories', () => {
+        it('should call get with endpoint', () => {
+            const spy = spyOn(service['http'], 'get');
+
+            service.getGameHistories();
+
+            expect(spy).toHaveBeenCalledOnceWith(service['endpoint']);
+        });
+    });
+
+    describe('resetGameHistories', () => {
+        it('should call delete with endpoint', () => {
+            const spy = spyOn(service['http'], 'delete');
+
+            service.resetGameHistories();
+
+            expect(spy).toHaveBeenCalledOnceWith(service['endpoint']);
+        });
     });
 });
