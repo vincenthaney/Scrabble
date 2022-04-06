aa29aa3dc4f801473d02fe23874ac08105d5b159
Thomas <thomtrepanier@gmail.com>
2022-04-06 18:40:05 -0400

    Added test

--- client/src/app/services/game-dispatcher-service/game-dispatcher.service.spec.ts
+++ client/src/app/services/game-dispatcher-service/game-dispatcher.service.spec.ts
@@ -378,10 +378,17 @@ describe('GameDispatcherService', () => {
             expect(service.currentLobby).toEqual(TEST_LOBBY_DATA);
         });
 
-        it('should set currentLobby is Multiplayer, should route to waiting-room', () => {
+        it('should if is Multiplayer, should route to waiting-room', () => {
             TEST_LOBBY_DATA.gameMode = GameMode.Multiplayer;
             postObservable.next({ lobbyData: TEST_LOBBY_DATA });
             expect(routerSpy).toHaveBeenCalledWith('waiting-room');
+        });
+
+        it('should if is Solo, should NOT route to waiting-room', () => {
+            TEST_LOBBY_DATA.gameMode = GameMode.Solo;
+            postObservable.next({ lobbyData: TEST_LOBBY_DATA });
+            expect(routerSpy).not.toHaveBeenCalledWith('waiting-room');
+            TEST_LOBBY_DATA.gameMode = GameMode.Multiplayer;
         });
 
         it('on error, should send gameCreationFailed$ event', () => {
