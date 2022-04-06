bfeebb84cd4869c1eb4c52d3a89cd99e798e1603
RaphaelSalvas <71101775+RaphaelSalvas@users.noreply.github.com>
2022-04-06 12:28:08 -0400

    RS: Add test for alternativeMove

--- server/app/classes/virtual-player/expert-virtual-player/expert-virtual-player.spec.ts
+++ server/app/classes/virtual-player/expert-virtual-player/expert-virtual-player.spec.ts
@@ -4,12 +4,11 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import { ActionExchange, ActionPass, ActionPlace } from '@app/classes/actions';
-import { ScoredWordPlacement, WordFindingUseCase } from '@app/classes/word-finding';
+import { AbstractWordFinding, ScoredWordPlacement, WordFindingUseCase } from '@app/classes/word-finding';
 import { PLAYER_ID, PLAYER_NAME, TEST_POINT_RANGE } from '@app/constants/virtual-player-tests-constants';
-import WordFindingService from '@app/services/word-finding-service/word-finding.service';
 import * as chai from 'chai';
 import { expect, spy } from 'chai';
-import { createStubInstance, SinonStubbedInstance } from 'sinon';
+import { createStubInstance } from 'sinon';
 import { ExpertVirtualPlayer } from './expert-virtual-player';
 
 describe('ExpertVirtualPlayer', () => {
@@ -32,6 +31,54 @@ describe('ExpertVirtualPlayer', () => {
             const testPointRange = expertVirtualPlayer['findPointRange']();
             expect(testPointRange.min).to.equal(0);
             expect(testPointRange.max).to.equal(Number.MAX_SAFE_INTEGER);
+        });
+    });
+
+    describe('alternativeMove', () => {
+        it('should call ActionExchange if no moves are found and exchange is possible', () => {
+            spy.on(expertVirtualPlayer, 'isExchangePossible', () => {
+                return true;
+            });
+            const createActionDataSpy = spy.on(ActionExchange, 'createActionData', () => {});
+
+            expertVirtualPlayer['alternativeMove']();
+            expect(createActionDataSpy).to.have.been.called();
+        });
+
+        it('should call ActionPass if no moves are found and exchange is not possible', () => {
+            spy.on(expertVirtualPlayer, 'isExchangePossible', () => {
+                return false;
+            });
+            const createActionDataSpy = spy.on(ActionPass, 'createActionData', () => {});
+
+            expertVirtualPlayer['alternativeMove']();
+            expect(createActionDataSpy).to.have.been.called();
+        });
+
+        it('should call ActionPass if no moves are found and exchange is not possible', () => {
+            const wordFindingInstanceStub = createStubInstance(AbstractWordFinding);
+            wordFindingInstanceStub['wordPlacements'] = [];
+            expertVirtualPlayer['wordFindingInstance'] = wordFindingInstanceStub as unknown as AbstractWordFinding;
+            spy.on(expertVirtualPlayer, 'isExchangePossible', () => {
+                return false;
+            });
+            const createActionDataSpy = spy.on(ActionPass, 'createActionData', () => {});
+
+            expertVirtualPlayer['alternativeMove']();
+            expect(createActionDataSpy).to.have.been.called();
+        });
+
+        it('should call ActionPlace if a moves is found', () => {
+            const wordFindingInstanceStub = createStubInstance(AbstractWordFinding);
+            wordFindingInstanceStub['wordPlacements'] = [{} as unknown as ScoredWordPlacement];
+            expertVirtualPlayer['wordFindingInstance'] = wordFindingInstanceStub as unknown as AbstractWordFinding;
+            spy.on(expertVirtualPlayer, 'isExchangePossible', () => {
+                return false;
+            });
+            const createActionDataSpy = spy.on(ActionPlace, 'createActionData', () => {});
+
+            expertVirtualPlayer['alternativeMove']();
+            expect(createActionDataSpy).to.have.been.called();
         });
     });
 
@@ -94,47 +141,5 @@ describe('ExpertVirtualPlayer', () => {
         expect(testWordFindingRequest.useCase).to.equal(WordFindingUseCase.Expert);
         expect(testWordFindingRequest.pointHistory).to.deep.equal(expertVirtualPlayer.pointHistory);
         expect(testWordFindingRequest.pointRange).to.deep.equal(TEST_POINT_RANGE);
-    });
-
-    describe('computeWordPlacement', () => {
-        let wordFindingServiceStub: SinonStubbedInstance<WordFindingService>;
-
-        beforeEach(async () => {
-            wordFindingServiceStub = createStubInstance(WordFindingService);
-            expertVirtualPlayer['wordFindingService'] = wordFindingServiceStub as unknown as WordFindingService;
-        });
-
-        afterEach(() => {
-            chai.spy.restore();
-        });
-
-        it('should call findWords', () => {
-            const findWordsSpy = spy.on(expertVirtualPlayer['wordFindingService'], 'findWords', () => {
-                return [];
-            });
-            spy.on(expertVirtualPlayer, 'getGameBoard', () => {
-                return;
-            });
-            spy.on(expertVirtualPlayer, 'generateWordFindingRequest', () => {
-                return;
-            });
-            expertVirtualPlayer['computeWordPlacement']();
-            expect(findWordsSpy).to.have.been.called();
-        });
-
-        it('should call getGameBoard and generateWord', () => {
-            chai.spy.on(expertVirtualPlayer['wordFindingService'], 'findWords', () => {
-                return [];
-            });
-            const getGameBoardSpy = spy.on(expertVirtualPlayer, 'getGameBoard', () => {
-                return;
-            });
-            const generateWordSpy = spy.on(expertVirtualPlayer, 'generateWordFindingRequest', () => {
-                return;
-            });
-            expertVirtualPlayer['computeWordPlacement']();
-            expect(getGameBoardSpy).to.have.been.called();
-            expect(generateWordSpy).to.have.been.called();
-        });
     });
 });
--- server/app/classes/virtual-player/expert-virtual-player/expert-virtual-player.ts
+++ server/app/classes/virtual-player/expert-virtual-player/expert-virtual-player.ts
@@ -2,11 +2,9 @@ import { AbstractVirtualPlayer } from '@app/classes/virtual-player/abstract-virt
 import { ActionExchange, ActionPass, ActionPlace } from '@app/classes/actions';
 import { ActionData } from '@app/classes/communication/action-data';
 import Range from '@app/classes/range/range';
-import { AbstractWordFinding, ScoredWordPlacement, WordFindingRequest, WordFindingUseCase } from '@app/classes/word-finding';
+import { WordFindingRequest, WordFindingUseCase } from '@app/classes/word-finding';
 
 export class ExpertVirtualPlayer extends AbstractVirtualPlayer {
-    private wordFindingInstance: AbstractWordFinding;
-
     protected async findAction(): Promise<ActionData> {
         const scoredWordPlacement = this.computeWordPlacement();
         return scoredWordPlacement ? ActionPlace.createActionData(scoredWordPlacement) : this.alternativeMove();
@@ -24,17 +22,17 @@ export class ExpertVirtualPlayer extends AbstractVirtualPlayer {
         return this.isExchangePossible() ? ActionExchange.createActionData(this.tiles) : ActionPass.createActionData();
     }
 
-    private computeWordPlacement(): ScoredWordPlacement | undefined {
-        const request = this.generateWordFindingRequest();
-        this.wordFindingInstance = this.getWordFindingService().getWordFindingInstance(request.useCase, [
-            this.getGameBoard(this.gameId, this.id),
-            this.tiles,
-            request,
-        ]);
-        return this.wordFindingInstance.findWords().pop();
-    }
+    // private computeWordPlacement(): ScoredWordPlacement | undefined {
+    //     const request = this.generateWordFindingRequest();
+    //     this.wordFindingInstance = this.getWordFindingService().getWordFindingInstance(request.useCase, [
+    //         this.getGameBoard(this.gameId, this.id),
+    //         this.tiles,
+    //         request,
+    //     ]);
+    //     return this.wordFindingInstance.findWords().pop();
+    // }
 
-    private generateWordFindingRequest(): WordFindingRequest {
+    protected generateWordFindingRequest(): WordFindingRequest {
         return {
             pointRange: this.findPointRange(),
             useCase: WordFindingUseCase.Expert,
