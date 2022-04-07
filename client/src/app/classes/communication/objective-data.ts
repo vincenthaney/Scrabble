94ec984886d494b39bec70dcc1acd75acf4721d9
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-06 21:54:39 -0400

    update objective interface to match server

--- client/src/app/classes/communication/objective-data.ts
+++ client/src/app/classes/communication/objective-data.ts
@@ -3,9 +3,9 @@ import { ObjectiveState } from '@app/classes/objectives/objective-state';
 export interface ObjectiveData {
     name: string;
     description: string;
+    bonusPoints: number;
     state: ObjectiveState;
+    isPublic: boolean;
     progress: number;
     maxProgress: number;
-    isPublic: boolean;
-    bonusPoints: number;
 }
