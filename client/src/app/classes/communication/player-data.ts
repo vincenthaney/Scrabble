b81d73933722cbfbe3ca4207bdad2f9c139e8b3f
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-06 21:54:47 -0400

    add objectives to player data

--- client/src/app/classes/communication/player-data.ts
+++ client/src/app/classes/communication/player-data.ts
@@ -1,8 +1,10 @@
 import { Tile } from '@app/classes/tile';
+import { ObjectiveData } from './objective-data';
 
 export default interface PlayerData {
     id: string;
     name?: string;
     score?: number;
     tiles?: Tile[];
+    objectives?: ObjectiveData[];
 }
