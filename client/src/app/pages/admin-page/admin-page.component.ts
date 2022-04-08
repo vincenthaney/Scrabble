0e7e0baf96697c3d93341500506cf7d5fe736126
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-07 22:23:53 -0400

    make tabs stay after refresh

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { ActivatedRoute, Router } from '@angular/router';
+
+enum AdminTabs {
+    GameHistory = 0,
+    Dictionary = 1,
+    VirtualPlayer = 2,
+    HighScores = 3,
+}
+
+export const DEFAULT_ADMIN_TAB = AdminTabs.GameHistory;
 
 @Component({
     selector: 'app-admin-page',
     templateUrl: './admin-page.component.html',
     styleUrls: ['./admin-page.component.scss'],
 })
-export class AdminPageComponent {}
+export class AdminPageComponent implements OnInit {
+    selectedTab: AdminTabs = DEFAULT_ADMIN_TAB;
+
+    constructor(private route: ActivatedRoute, private router: Router) {}
+
+    ngOnInit(): void {
+        this.route.queryParams.subscribe((params) => {
+            const tab: string | undefined = params.tab;
+
+            if (tab) {
+                const tabIndex = Number.parseInt(tab, 10);
+                if (!Number.isNaN(tabIndex)) this.selectedTab = tabIndex as AdminTabs;
+            }
+        });
+    }
+
+    selectedTabChange(event: MatTabChangeEvent) {
+        this.router.navigate([], {
+            relativeTo: this.route,
+            queryParams: {
+                tab: event.index,
+            },
+            queryParamsHandling: 'merge',
+            skipLocationChange: false,
+        });
+    }
+}
