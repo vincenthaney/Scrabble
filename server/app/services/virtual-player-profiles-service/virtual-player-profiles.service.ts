f922a17e50aaec80a2d2011b147fd3f83d741c37
matbro-inextenso <matbro@inextenso.net>
2022-04-10 13:44:34 -0400

    used ids to identify profiles instead of names

--- server/app/services/virtual-player-profiles-service/virtual-player-profiles.service.ts
+++ server/app/services/virtual-player-profiles-service/virtual-player-profiles.service.ts
@@ -3,15 +3,14 @@ import { HttpException } from '@app/classes/http-exception/http-exception';
 import { VirtualPlayerLevel } from '@app/classes/player/virtual-player-level';
 import {
     DEFAULT_VIRTUAL_PLAYER_PROFILES_RELATIVE_PATH,
-    HIGH_SCORES_MONGO_COLLECTION_NAME,
     VIRTUAL_PLAYER_PROFILES_MONGO_COLLECTION_NAME,
 } from '@app/constants/services-constants/mongo-db.const';
-import { CANNOT_ADD_DEFAULT_PROFILE, CANNOT_MODIFY_DEFAULT_PROFILE, NAME_ALREADY_USED } from '@app/constants/services-errors';
+import { CANNOT_ADD_DEFAULT_PROFILE, NAME_ALREADY_USED } from '@app/constants/services-errors';
 import DatabaseService from '@app/services/database-service/database.service';
 import { promises } from 'fs';
 import { StatusCodes } from 'http-status-codes';
 import 'mock-fs'; // required when running test. Otherwise compiler cannot resolve fs, path and __dirname
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
 import { join } from 'path';
 import { Service } from 'typedi';
 
@@ -41,24 +40,14 @@ export default class VirtualPlayerProfilesService {
         await this.collection.insertOne(newProfile);
     }
 
-    async updateVirtualPlayerProfile(newName: string, virtualPlayerProfile: VirtualPlayerProfile): Promise<void> {
-        if (virtualPlayerProfile.isDefault) throw new HttpException(CANNOT_MODIFY_DEFAULT_PROFILE, StatusCodes.BAD_REQUEST);
+    async updateVirtualPlayerProfile(newName: string, profileId: string): Promise<void> {
         if (await this.isNameAlreadyUsed(newName)) throw new HttpException(NAME_ALREADY_USED(newName), StatusCodes.BAD_REQUEST);
 
-        await this.collection.updateOne(
-            { name: virtualPlayerProfile.name, level: virtualPlayerProfile.level, isDefault: virtualPlayerProfile.isDefault },
-            { $set: { name: newName } },
-        );
+        await this.collection.updateOne({ _id: new ObjectId(profileId), isDefault: false }, { $set: { name: newName } });
     }
 
-    async deleteVirtualPlayerProfile(virtualPlayerProfile: VirtualPlayerProfile): Promise<void> {
-        if (virtualPlayerProfile.isDefault) throw new HttpException(CANNOT_MODIFY_DEFAULT_PROFILE, StatusCodes.BAD_REQUEST);
-
-        await this.collection.deleteOne({
-            name: virtualPlayerProfile.name,
-            level: virtualPlayerProfile.level,
-            isDefault: virtualPlayerProfile.isDefault,
-        });
+    async deleteVirtualPlayerProfile(profileId: string): Promise<void> {
+        await this.collection.deleteOne({ _id: new ObjectId(profileId), isDefault: false });
     }
 
     async resetVirtualPlayerProfiles(): Promise<void> {
@@ -67,7 +56,7 @@ export default class VirtualPlayerProfilesService {
     }
 
     private get collection(): Collection<VirtualPlayerProfile> {
-        return this.databaseService.database.collection(HIGH_SCORES_MONGO_COLLECTION_NAME);
+        return this.databaseService.database.collection(VIRTUAL_PLAYER_PROFILES_MONGO_COLLECTION_NAME);
     }
 
     private async populateDb(): Promise<void> {
