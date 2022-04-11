16e242219422d292a4e74023fffdda09cf5620e7
Vincent Haney <vincent.7aney@gmail.com>
2022-04-10 22:34:31 -0400

    fix tests error

--- client/src/app/pages/admin-page/admin-page.component.spec.ts
+++ client/src/app/pages/admin-page/admin-page.component.spec.ts
@@ -3,6 +3,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -43,6 +45,8 @@ describe('AdminPageComponent', () => {
                 BrowserAnimationsModule,
                 RouterTestingModule.withRoutes([{ path: 'admin', component: AdminPageComponent }]),
                 HttpClientTestingModule,
+                MatSnackBarModule,
+                MatDialogModule,
             ],
             providers: [{ provide: HighScoresService, useValue: highScoreServiceSpy }],
         }).compileComponents();
