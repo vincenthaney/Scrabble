4631ccdc53f6e62ca029d2eae453bac9198d0fc3
Thomas <thomtrepanier@gmail.com>
2022-04-11 14:02:39 -0400

    fix lint

--- client/src/app/pages/admin-page/admin-page.component.spec.ts
+++ client/src/app/pages/admin-page/admin-page.component.spec.ts
@@ -5,7 +5,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Params } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
