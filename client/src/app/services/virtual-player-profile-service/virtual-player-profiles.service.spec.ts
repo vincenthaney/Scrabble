3a3eeb9d57809c476ae2c2eb78e4a8c1df82e1fd
Thomas <thomtrepanier@gmail.com>
2022-04-11 17:24:37 -0400

    Added tests to virtual profile service

--- client/src/app/services/virtual-player-profile-service/virtual-player-profiles.service.spec.ts
+++ client/src/app/services/virtual-player-profile-service/virtual-player-profiles.service.spec.ts
@@ -1,130 +1,99 @@
-// import { HttpClientTestingModule } from '@angular/common/http/testing';
-// import { TestBed } from '@angular/core/testing';
-// import { GameHistoriesData } from '@app/classes/communication/game-histories';
-// import { GameHistoriesConverter } from '@app/classes/game-history/game-histories-converter';
-// import { GameHistory } from '@app/classes/game-history/game-history';
-// import { GameHistoryController } from '@app/controllers/game-history-controller/game-history.controller';
-// import { Observable, Subject, throwError } from 'rxjs';
+/* eslint-disable dot-notation */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { VirtualPlayerProfileData } from '@app/classes/communication/virtual-player-profiles';
+import { ERROR_SNACK_BAR_CONFIG } from '@app/constants/components-constants';
+import { MOCK_PLAYER_PROFILES } from '@app/constants/service-test-constants';
+import { VirtualPlayerProfilesController } from '@app/controllers/virtual-player-profiles-controller/virtual-player-profiles.controller';
+import { Observable, Subject, throwError } from 'rxjs';
+import { VirtualPlayerProfilesService } from './virtual-player-profiles.service';
 
-// import { VirtualPlayerProfilesService } from './virtual-player-profiles.service';
+fdescribe('VirtualPlayerProfilesService', () => {
+    let service: VirtualPlayerProfilesService;
+    let controllerSpy: jasmine.SpyObj<VirtualPlayerProfilesController>;
 
-// describe('GameHistoryService', () => {
-//     let service: VirtualPlayerProfilesService;
-//     let controllerSpy: jasmine.SpyObj<GameHistoryController>;
+    beforeEach(() => {
+        controllerSpy = jasmine.createSpyObj(VirtualPlayerProfilesController, {
+            getVirtualPlayerProfiles: new Observable<VirtualPlayerProfileData>(),
+        });
 
-//     beforeEach(() => {
-//         controllerSpy = jasmine.createSpyObj(GameHistoryController, {
-//             getGameHistories: new Observable<GameHistoriesData>(),
-//             resetGameHistories: new Observable<void>(),
-//         });
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, MatSnackBarModule],
+            providers: [{ provide: VirtualPlayerProfilesController, useValue: controllerSpy }, MatSnackBar],
+        });
+        service = TestBed.inject(VirtualPlayerProfilesService);
+    });
 
-//         TestBed.configureTestingModule({
-//             imports: [HttpClientTestingModule],
-//             providers: [{ provide: GameHistoryController, useValue: controllerSpy }],
-//         });
-//         service = TestBed.inject(VirtualPlayerProfilesService);
-//     });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 
-//     it('should be created', () => {
-//         expect(service).toBeTruthy();
-//     });
+    describe('getVirtualPlayerProfiles', () => {
+        let handleErrorSpy: jasmine.Spy;
 
-//     describe('getGameHistories', () => {
-//         it('should call getGameHistories', () => {
-//             service.getGameHistories();
-//             expect(controllerSpy.getGameHistories).toHaveBeenCalled();
-//         });
+        beforeEach(() => {
+            handleErrorSpy = spyOn<any>(service, 'handleError').and.callFake(() => {});
+        });
 
-//         it('should call convert with result', (done) => {
-//             const data: GameHistoriesData = { gameHistories: [] };
-//             const subject = new Subject<GameHistoriesData>();
-//             const spy = spyOn(GameHistoriesConverter, 'convert');
-//             controllerSpy.getGameHistories.and.returnValue(subject);
+        it('should call getVirtualPlayerProfiles', () => {
+            service.getVirtualPlayerProfiles();
+            expect(controllerSpy.getVirtualPlayerProfiles).toHaveBeenCalled();
+        });
 
-//             service.getGameHistories().finally(() => {
-//                 expect(spy).toHaveBeenCalled();
-//                 spy.and.callThrough();
-//                 done();
-//             });
+        it('should resolve profiles in result', (done) => {
+            const expected: VirtualPlayerProfileData = { virtualPlayerProfiles: MOCK_PLAYER_PROFILES };
+            const subject = new Subject<VirtualPlayerProfileData>();
+            controllerSpy.getVirtualPlayerProfiles.and.returnValue(subject);
 
-//             subject.next(data);
-//         });
+            service
+                .getVirtualPlayerProfiles()
+                .then((result) => {
+                    expect(result).toEqual(expected.virtualPlayerProfiles);
+                })
+                .catch(() => {
+                    expect(false).toBeTrue();
+                })
+                .finally(() => {
+                    done();
+                });
 
-//         it('should resolve with conversion result', (done) => {
-//             const expected = new Array<GameHistory>();
-//             const subject = new Subject<GameHistoriesData>();
-//             const spy = spyOn(GameHistoriesConverter, 'convert').and.returnValue(expected);
-//             controllerSpy.getGameHistories.and.returnValue(subject);
+            subject.next(expected);
+        });
 
-//             service
-//                 .getGameHistories()
-//                 .then((result) => {
-//                     expect(result).toEqual(expected);
-//                 })
-//                 .catch(() => {
-//                     expect(false).toBeTrue();
-//                 })
-//                 .finally(() => {
-//                     spy.and.callThrough();
-//                     done();
-//                 });
+        it('should call handleError on error', (done) => {
+            controllerSpy.getVirtualPlayerProfiles.and.returnValue(throwError('error'));
 
-//             subject.next();
-//         });
+            service.getVirtualPlayerProfiles().finally(() => {
+                expect(handleErrorSpy).toHaveBeenCalledWith('error');
+                done();
+            });
+        });
 
-//         it('should reject on error', (done) => {
-//             controllerSpy.getGameHistories.and.returnValue(throwError('error'));
+        it('should reject on error', (done) => {
+            controllerSpy.getVirtualPlayerProfiles.and.returnValue(throwError('error'));
 
-//             let success = false;
-//             service
-//                 .getGameHistories()
-//                 .catch(() => {
-//                     success = true;
-//                 })
-//                 .finally(() => {
-//                     expect(success).toBeTrue();
-//                     done();
-//                 });
-//         });
-//     });
+            let success = false;
+            service
+                .getVirtualPlayerProfiles()
+                .catch(() => {
+                    success = true;
+                })
+                .finally(() => {
+                    expect(success).toBeTrue();
+                    done();
+                });
+        });
+    });
 
-//     describe('resetGameHistories', () => {
-//         it('should call resetGameHistories', () => {
-//             service.resetGameHistories();
-//             expect(controllerSpy.resetGameHistories).toHaveBeenCalled();
-//         });
+    it('handleError should open Snack Bar with right config', () => {
+        const snackBarSpy = spyOn(service['snackBar'], 'open');
+        const error: HttpErrorResponse = { error: { message: 'error' } } as HttpErrorResponse;
 
-//         it('should resolve on next', (done) => {
-//             const subject = new Subject<void>();
-//             controllerSpy.resetGameHistories.and.returnValue(subject);
-
-//             let success = false;
-//             service
-//                 .resetGameHistories()
-//                 .then(() => {
-//                     success = true;
-//                 })
-//                 .finally(() => {
-//                     expect(success).toBeTrue();
-//                     done();
-//                 });
-
-//             subject.next();
-//         });
-
-//         it('should reject on error', (done) => {
-//             controllerSpy.resetGameHistories.and.returnValue(throwError('error'));
-
-//             let success = false;
-//             service
-//                 .resetGameHistories()
-//                 .catch(() => {
-//                     success = true;
-//                 })
-//                 .finally(() => {
-//                     expect(success).toBeTrue();
-//                     done();
-//                 });
-//         });
-//     });
-// });
+        service['handleError'](error);
+        expect(snackBarSpy).toHaveBeenCalledWith(error.error.message, 'OK', ERROR_SNACK_BAR_CONFIG);
+    });
+});
