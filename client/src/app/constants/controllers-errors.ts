2bb2c573c73933d5cdd1d63a638bf69b7a2079e6
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-12 16:37:22 -0400

    cannot create game with deleted dictionary

--- client/src/app/constants/controllers-errors.ts
+++ client/src/app/constants/controllers-errors.ts
@@ -1,2 +1,2 @@
 export const HTTP_ABORT_ERROR = 0;
-export const INVALID_DICTIONARY_ID = "L'identifiant (ID) du dictionanaire est invalide";
+export const INVALID_DICTIONARY_ID = "L'identificateur (ID) du dictionanaire est invalide";
