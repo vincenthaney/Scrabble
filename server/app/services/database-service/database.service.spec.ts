ee34acd09448230905c50d5f2a18b10b91864fa6
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-12 18:42:33 -0400

    stub dictionary service

--- server/app/services/database-service/database.service.spec.ts
+++ server/app/services/database-service/database.service.spec.ts
@@ -20,13 +20,16 @@ const TEST_DOCUMENT_BIG_ARRAY: Document[] = [{ score: 1 }, { score: 1 }, { score
 describe('Database service', () => {
     let databaseService: DatabaseService;
     let mongoServer: MongoMemoryServer;
+    let testingUnit: ServicesTestingUnit;
 
     beforeEach(async () => {
+        testingUnit = new ServicesTestingUnit().withStubbedDictionaryService();
         databaseService = Container.get(DatabaseService);
         mongoServer = await ServicesTestingUnit.getMongoServer();
     });
 
     afterEach(async () => {
+        testingUnit.restore();
         if (databaseService['mongoClient']) {
             await databaseService['mongoClient'].close();
         }
