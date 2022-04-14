2bbe9b4729f5b32fd4b4a3fac15b06dd73450b31
Vincent Haney <vincent.7aney@gmail.com>
2022-04-14 00:36:05 -0400

    virtual player admin component

---
+++ client/src/app/components/admin-virtual-players/admin-virtual-players.types.ts
@@ -0,0 +1,28 @@
+import { VirtualPlayerLevel } from '@app/classes/player/virtual-player-level';
+
+export type DisplayVirtualPlayersKeys = keyof { name: string; level: string } | 'name' | 'level' | 'actions';
+
+export type DisplayVirtualPlayersColumns = {
+    [Property in DisplayVirtualPlayersKeys]: string;
+};
+
+export type DisplayVirtualPlayersColumnsIteratorItem = { key: DisplayVirtualPlayersKeys; label: string };
+
+export enum VirtualPlayersComponentState {
+    Ready = 'ready',
+    Loading = 'loading',
+    Error = 'error',
+}
+
+export interface UpdateVirtualPlayersDialogParameters {
+    name: string;
+    level: VirtualPlayerLevel;
+    id: string;
+}
+
+export interface DeleteVirtualPlayerDialogParameters {
+    name: string;
+    level: VirtualPlayerLevel;
+    id: string;
+    onClose: () => void;
+}
