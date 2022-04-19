ac154e0809ed41737c93931565e4933ae0bc8639
Thomas <thomtrepanier@gmail.com>
2022-04-19 15:18:39 -0400

    Show other player objective when completed

--- client/src/app/services/objectives-manager-service/objectives-manager.service.ts
+++ client/src/app/services/objectives-manager-service/objectives-manager.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { StartGameData } from '@app/classes/communication/game-config';
 import { GameObjectivesData } from '@app/classes/communication/game-objectives-data';
 import { ObjectiveData } from '@app/classes/communication/objective-data';
+import { ObjectiveState } from '@app/classes/objectives/objective-state';
 import { IResetServiceData } from '@app/utils/i-reset-service-data/i-reset-service-data';
 
 @Injectable({
@@ -24,18 +25,28 @@ export class ObjectivesManagerService implements IResetServiceData {
     }
 
     getPublicObjectives(): ObjectiveData[] {
-        return this.getObjectives().filter((objective) => objective.isPublic);
+        return this.getObjectives(this.isLocalPlayerPlayer1).filter((objective) => objective.isPublic);
     }
 
     getPrivateObjectives(): ObjectiveData[] {
-        return this.getObjectives().filter((objective) => !objective.isPublic);
+        return this.getObjectives(this.isLocalPlayerPlayer1)
+            .filter((objective) => !objective.isPublic)
+            .concat(this.getOpponentPrivateObjectiveIfCompleted());
     }
 
     resetServiceData(): void {
         this.objectives = undefined;
     }
 
-    private getObjectives(): ObjectiveData[] {
-        return (this.isLocalPlayerPlayer1 ? this.objectives?.player1Objectives : this.objectives?.player2Objectives) || [];
+    private getObjectives(requestingPlayerIsPlaye1: boolean): ObjectiveData[] {
+        return (requestingPlayerIsPlaye1 ? this.objectives?.player1Objectives : this.objectives?.player2Objectives) || [];
+    }
+
+    private getOpponentPrivateObjectiveIfCompleted(): ObjectiveData[] {
+        const opponentPrivateObjectives: ObjectiveData[] = this.getObjectives(!this.isLocalPlayerPlayer1).filter(
+            (objective: ObjectiveData) => !objective.isPublic && objective.state !== ObjectiveState.NotCompleted,
+        );
+        opponentPrivateObjectives.forEach((objective: ObjectiveData) => (objective.state = ObjectiveState.CompletedByOpponent));
+        return opponentPrivateObjectives;
     }
 }
