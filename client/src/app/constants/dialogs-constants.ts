b22c730548bc936c93ba7cc091a2741429d94cfe
Vincent Haney <vincent.7aney@gmail.com>
2022-04-18 21:30:14 -0400

    change component size

--- client/src/app/constants/dialogs-constants.ts
+++ client/src/app/constants/dialogs-constants.ts
@@ -6,5 +6,5 @@ export interface PositiveFeedbackResponse {
 export const UPDATE_VIRTUAL_PLAYER_DIALOG_HEIGHT = '300px';
 export const UPDATE_VIRTUAL_PLAYER_DIALOG_WIDTH = '450px';
 
-export const CREATE_VIRTUAL_PLAYER_DIALOG_HEIGHT = '335px';
+export const CREATE_VIRTUAL_PLAYER_DIALOG_HEIGHT = '315px';
 export const CREATE_VIRTUAL_PLAYER_DIALOG_WIDTH = '450px';
