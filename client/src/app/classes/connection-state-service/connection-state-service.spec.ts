6474b1bba6ca408b52d1d986abcefee1f4411d4b
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-19 19:00:46 -0400

    test connection state service

---
+++ client/src/app/classes/connection-state-service/connection-state-service.spec.ts
@@ -0,0 +1,37 @@
+/* eslint-disable dot-notation */
+import { Observable } from 'rxjs';
+import ConnectionStateService, { ConnectionState } from './connection-state-service';
+
+class TestClass extends ConnectionStateService {}
+
+describe('ConnectionStateService', () => {
+    let connectionStateService: ConnectionStateService;
+    let destroy$: Observable<boolean>;
+
+    beforeEach(() => {
+        connectionStateService = new TestClass();
+        destroy$ = new Observable();
+    });
+
+    describe('subscribe', () => {
+        it('should call subscribe on state$', () => {
+            const spy = spyOn(connectionStateService['state$'], 'subscribe');
+            connectionStateService.subscribe(destroy$, () => undefined);
+            expect(spy).toHaveBeenCalled();
+        });
+    });
+
+    describe('getState', () => {
+        it('should return state value', () => {
+            expect(connectionStateService.getState()).toEqual(connectionStateService['state$'].value);
+        });
+    });
+
+    describe('nextState', () => {
+        it('should call next', () => {
+            const spy = spyOn(connectionStateService['state$'], 'next');
+            connectionStateService['nextState'](ConnectionState.Loading);
+            expect(spy).toHaveBeenCalled();
+        });
+    });
+});
