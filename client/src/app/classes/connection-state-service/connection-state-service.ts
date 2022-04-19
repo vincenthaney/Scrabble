508b8674e78a38b3aeba23ea48f03f6b789c5b5f
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-19 19:00:35 -0400

    create connection state service

---
+++ client/src/app/classes/connection-state-service/connection-state-service.ts
@@ -0,0 +1,24 @@
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+export enum ConnectionState {
+    Loading = 'Loading',
+    Connected = 'Connected',
+    Error = 'Error',
+}
+
+export default abstract class ConnectionStateService {
+    private state$: BehaviorSubject<ConnectionState> = new BehaviorSubject<ConnectionState>(ConnectionState.Loading);
+
+    subscribe(destroy$: Observable<boolean>, next: (state: ConnectionState) => void): Subscription {
+        return this.state$.pipe(takeUntil(destroy$)).subscribe(next);
+    }
+
+    getState(): ConnectionState {
+        return this.state$.value;
+    }
+
+    protected nextState(state: ConnectionState) {
+        this.state$.next(state);
+    }
+}
