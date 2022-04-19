9cc6f3e99b1a04415270408e3c0ef6650cbd53a2
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-04-19 19:02:26 -0400

    update app component to display loading

--- client/src/app/pages/app/app.component.ts
+++ client/src/app/pages/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { InitializerService, InitializeState } from '@app/services/initializer-service/initializer.service';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnDestroy {
+    states = InitializeState;
+    state: InitializeState = InitializeState.Loading;
+    private componentDestroyed$: Subject<boolean> = new Subject();
+
+    constructor(private readonly initializer: InitializerService) {
+        this.initializer.subscribe(this.componentDestroyed$, this.handleNewState.bind(this));
+    }
+
+    ngOnDestroy(): void {
+        this.componentDestroyed$.next(true);
+        this.componentDestroyed$.complete();
+    }
+
+    handleNewState(state: InitializeState) {
+        this.state = state;
+    }
+}
